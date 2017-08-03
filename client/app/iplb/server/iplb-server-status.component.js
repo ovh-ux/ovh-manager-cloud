9ad930edb2699da74f33cb2a5dce89e151f629eb
Kevin Manson <kevin.manson@corp.ovh.com>
2017-08-03 13:58:46 -0400

    feat(iplb): add status tile

--- client/app/iplb/server/iplb-server-status.component.js
+++ client/app/iplb/server/iplb-server-status.component.js
@@ -1,32 +1,20 @@
 class IpblServerStatusController {
+    constructor (IpblServerStatusService) {
+        this.IpblServerStatusService = IpblServerStatusService;
+    }
+
     $onInit () {
         if (!this.server) {
             this.server = {};
         }
 
-        if (this.hasIssue()) {
-            this.iconType = "error";
-        } else if (this.hasNoInfo()) {
-            this.iconType = "help";
-        } else {
-            this.iconType = "warning";
-        }
-    }
-
-    hasIssue () {
-        return _.some(this.server.serverState || [], serverState => serverState.status === "DOWN");
-    }
-
-    hasNoInfo () {
-        return !this.server.serverState ||
-            this.server.serverState === [] ||
-            _.chain(this.server.serverState).map(state => state.status !== "no check").some(Boolean);
+        this.iconType = this.IpblServerStatusService.getStatusIcon(this.server);
     }
 }
 
 angular.module("managerApp")
     .component("iplbServerStatus", {
-        template: `<cui-status-icon data-type="{{$ctrl.iconType}}"></cui-status-icon>`,
+        template: '<cui-status-icon data-type="{{$ctrl.iconType}}"></cui-status-icon>',
         controller: IpblServerStatusController,
         bindings: {
             server: "<"
