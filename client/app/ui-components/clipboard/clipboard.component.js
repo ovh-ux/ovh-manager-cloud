27d32849398377043a57360bf934d7c3b7077577
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-08-16 16:05:42 -0400

    implement copy handler, missing icons

--- client/app/ui-components/clipboard/clipboard.component.js
+++ client/app/ui-components/clipboard/clipboard.component.js
@@ -2,9 +2,42 @@
     "use strict";
 
     class CuiClipboardController {
+        constructor ($timeout) {
+            this.$timeout = $timeout;
+
+            this.options = {
+                action: "copy",
+                status: "initial"
+            };
+        }
+
         onTextFocus ($event) {
             $event.target.select();
+            this.copyText();
         }
+
+        copyText () {
+            let succeeded;
+
+            try {
+                succeeded = document.execCommand(this.options.action);
+            } catch (err) {
+                this.error = err;
+                succeeded = false;
+            }
+
+            this.handleResult(succeeded);
+            this.$timeout(() => this.reset(), 2000);
+        }
+
+        handleResult (succeeded) {
+            this.options.status = succeeded ? "success" : "error";
+        }
+
+        reset () {
+            this.options.status = "initial";
+        }
+
     }
 
     angular.module("managerApp")
@@ -16,6 +49,9 @@
                         data-ng-focus="$ctrl.onTextFocus($event)"
                         data-ng-value="$ctrl.text"
                         readonly>
+                    <span class="cui-clipboard__icon" data-ng-if="$ctrl.options.status === 'initial'"><i class="oui-icon oui-icon-help_circle aria-hidden="true"></i></span>
+                    <span class="cui-clipboard__icon" data-ng-if="$ctrl.options.status === 'success'"><i class="oui-icon oui-icon-success_circle aria-hidden="true"></i></span>
+                    <span class="cui-clipboard__icon" data-ng-if="$ctrl.options.status === 'error'"><i class="oui-icon oui-icon-error_circle aria-hidden="true"></i></span>
                 </div>
                 `,
             controller: CuiClipboardController,
