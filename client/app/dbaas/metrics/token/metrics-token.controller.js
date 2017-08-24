e19f0534c618acda8ea97230766f5107f1088894
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-08-24 16:50:05 -0400

    list view on token and platforms page

--- client/app/dbaas/metrics/token/metrics-token.controller.js
+++ client/app/dbaas/metrics/token/metrics-token.controller.js
@@ -28,6 +28,68 @@
             return `+${number}`;
         }
 
+        labelsTemplate () {
+            return `
+                <span class="token-labels" data-ng-repeat="label in $row.labels | orderBy : 'key'" data-ng-if="$index < 2">
+                    <span data-ng-bind="label.key"></span>:<span data-ng-bind="label.value"></span>
+                </span>
+                <cui-dropdown-menu data-ng-if="$row.labels.length > 2">
+                    <cui-dropdown-menu-button>
+                        <div class="token-labels__button-container">
+                            <span class="token-labels token-labels__button" data-ng-bind="MetricsTokenCtrl.displayRemainingLabels($row.labels.length - 2)"></span>
+                        </div>
+                    </cui-dropdown-menu-button>
+                    <cui-dropdown-menu-body class="token-labels-container">
+                        <span class="token-labels" data-ng-repeat="label in $row.labels | orderBy : 'key'" data-ng-if="$index >= 2">
+                            <span data-ng-bind="label.key"></span>:<span data-ng-bind="label.value"></span>
+                        </span>
+                    </cui-dropdown-menu-body>
+                </cui-dropdown-menu>
+            `;
+        }
+        actionTemplate () {
+            return `
+                <cui-dropdown-menu>
+                    <cui-dropdown-menu-button>
+                        <ng-include src="'app/ui-components/icons/button-action.html'"></ng-include>
+                    </cui-dropdown-menu-button>
+                    <cui-dropdown-menu-body>
+                        <div class="oui-action-menu">
+                            <div class="oui-action-menu__item oui-action-menu-item">
+                                <div class="oui-action-menu-item__icon">
+                                    <i class="oui-icon oui-icon-eye" aria-hidden="true"></i>
+                                </div>
+                                <button class="oui-button oui-button_link oui-action-menu-item__label"
+                                    type="button"
+                                    data-translate="metrics_token_show_preview"
+                                    data-ng-click="MetricsTokenCtrl.showPreview($row.id)"></button>
+                            </div>
+                        </div>
+                        <div class="oui-action-menu">
+                            <div class="oui-action-menu__item oui-action-menu-item">
+                                <div class="oui-action-menu-item__icon">
+                                    <i class="oui-icon oui-icon-pen_line" aria-hidden="true"></i>
+                                </div>
+                                <button class="oui-button oui-button_link oui-action-menu-item__label"
+                                    type="button"
+                                    data-translate="metrics_token_edit"
+                                    data-ng-click="MetricsTokenCtrl.edit($row.id, $row.description)"></button>
+                            </div>
+                            <div class="oui-action-menu__item oui-action-menu-item">
+                                <div class="oui-action-menu-item__icon">
+                                    <i class="oui-icon oui-icon-trash_line" aria-hidden="true"></i>
+                                </div>
+                                <button class="oui-button oui-button_link oui-action-menu-item__label"
+                                    type="button"
+                                    data-translate="metrics_token_delete"
+                                    data-ng-click="MetricsTokenCtrl.delete($row.id)"></button>
+                            </div>
+                        </div>
+                    </cui-dropdown-menu-body>
+                </cui-dropdown-menu>
+            `;
+        }
+
         showPreview (tokenID) {
             this.ControllerHelper.modal.showModal({
                 modalConfig: {
