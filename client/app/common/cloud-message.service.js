6830a58b3e0711172c52e4e40ec1caa165d3cc73
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-08-25 10:18:21 -0400

    doc for cloud-message (transversal service)

--- client/app/common/cloud-message.service.js
+++ client/app/common/cloud-message.service.js
@@ -21,6 +21,9 @@ class CloudMessage {
         this.logMessage(_.isPlainObject(message) ? message : { text: message }, "info", containerName);
     }
 
+    /*
+     * Handle the message type (error, warning, ...etc) and push the message to the messageHandler
+     */
     logMessage (messageHash, type, containerName) {
         if (!messageHash.text) {
             return;
@@ -36,6 +39,11 @@ class CloudMessage {
         }
     }
 
+    /*
+     * Handle message to dispatch.
+     * @params containerName : custom name for the container
+     * if no params is passed, it will take the current state name as containerName
+     */
     getMessageHandler (containerName) {
         containerName = `${containerName || this.$state.current.name}.`;
         let messageHandler = null;
@@ -46,10 +54,16 @@ class CloudMessage {
         return messageHandler;
     }
 
+    /*
+     * Retrieve messages
+     */
     getMessages (containerName) {
         return this.subscribers[containerName].messages;
     }
 
+    /*
+     * Flush currents messages
+     */
     flushMessages (containerName) {
         const messageHandler = this.getMessageHandler(containerName);
 
@@ -59,6 +73,9 @@ class CloudMessage {
         }
     }
 
+    /*
+     * unsubscribe to the message receiver.
+     */
     unSubscribe (containerName) {
         const subscriber = this.subscribers[containerName];
         if (subscriber) {
@@ -68,6 +85,12 @@ class CloudMessage {
         this.subscribers = _.omit(this.subscribers, containerName);
     }
 
+    /*
+     * subscibe to the message receiver.
+     * @params containerName : container name or the state name to subscribe
+     * @params params actions to do
+     * ex params : { onMessage: () => this.getMessage() }
+     */
     subscribe (containerName, params) {
         this.subscribers[containerName] = _.extend({
             messages: [],
