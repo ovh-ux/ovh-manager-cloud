573ac74ffa80c5de7bc7ea2a83a6c8da2185a1bf
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-08-25 16:44:02 -0400

    assigned event variable when initializing infrastructure

---
+++ client/app/cloud/project/compute/infrastructure/cloud-project-compute-infrastructure.config.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ *  Configuration for at-internet tracking
+ **/
+angular.module("managerApp")
+    .config(atInternetControllerDecoratorsProvider => {
+        atInternetControllerDecoratorsProvider.decorate({
+            CloudProjectComputeInfrastructureCtrl: {
+                initInfra (atInternet, controller) {
+                    controller.Cloud.Project().Lexi().query().$promise
+                        .then(projects => {
+                            atInternet.trackEvent({
+                                event: `[CloudProject-${projects.length}]`,
+                                page: "cloud-project::cloud-project-compute::cloud-project-compute-infrastructure"
+                            });
+                        });
+                }
+            }
+        });
+    });
