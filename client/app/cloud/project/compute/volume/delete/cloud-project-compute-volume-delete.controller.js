f4de9a16cc4cb2b1a0b4ec0db198bca4cf26027d
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-09-28 12:08:48 -0400

    volume delete modal

---
+++ client/app/cloud/project/compute/volume/delete/cloud-project-compute-volume-delete.controller.js
@@ -0,0 +1,37 @@
+class CloudProjectComputeVolumeDeleteCtrl {
+    constructor ($uibModalInstance, OvhApiCloudProjectVolume, serviceName, volume) {
+        this.$uibModalInstance = $uibModalInstance;
+        this.OvhApiCloudProjectVolume = OvhApiCloudProjectVolume;
+
+        this.serviceName = serviceName;
+        this.volume = volume;
+
+        this.loaders = {
+            delete: false
+        }
+    }
+
+    deleteVolume (volumeId) {
+        return this.OvhApiCloudProjectVolume
+            .Lexi()
+            .remove({serviceName : this.serviceName, volumeId: volumeId})
+            .$promise;
+    }
+
+    confirm () {
+        this.loaders.delete = true;
+        return this.deleteVolume(this.volume.id)
+            .then(response => this.$uibModalInstance.close(response))
+            .catch(err => this.$uibModalInstance.dismiss(err))
+            .finally(() => {
+                this.loaders.delete = false
+            });
+    }
+
+    cancel () {
+        this.$uibModalInstance.dismiss();
+    }
+
+}
+
+angular.module("managerApp").controller("CloudProjectComputeVolumeDeleteCtrl", CloudProjectComputeVolumeDeleteCtrl);
