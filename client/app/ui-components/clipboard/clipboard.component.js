6fb9525e1b62efc85a9ce2d274d90d3f405b0eb0
Kevin Manson <kevin.manson@corp.ovh.com>
2017-10-05 16:29:08 -0400

    feat(iplb): fix conflicts

--- client/app/ui-components/clipboard/clipboard.component.js
+++ client/app/ui-components/clipboard/clipboard.component.js
@@ -36,7 +36,7 @@
         }
 
         handleResult (succeeded) {
-            this.options.status = succeeded ? "success" : "error";
+            this.options.status = succeeded ? "success" : "initial";
         }
 
         reset () {
@@ -48,37 +48,23 @@
     angular.module("managerApp")
         .component("cuiClipboard", {
             template: `
-                <div>
-                    <label class="cui-clipboard__label"
-                        data-ng-bind="$ctrl.label"></label>
+                <div class="cui-clipboard__input-container">
                     <input class="cui-clipboard__input"
                         type="text"
+                        id="{{$ctrl.id}}"
                         data-ng-focus="$ctrl.onTextFocus($event)"
                         data-ng-value="$ctrl.text"
                         readonly>
                     <span class="cui-clipboard__icon" data-ng-if="$ctrl.options.status === 'initial'"><i class="oui-icon oui-icon-copy-normal aria-hidden="true"></i></span>
                     <span class="cui-clipboard__icon" data-ng-if="$ctrl.options.status === 'success'"><i class="oui-icon oui-icon-copy-success aria-hidden="true"></i></span>
-                    <span class="cui-clipboard__icon" data-ng-if="$ctrl.options.status === 'error'"><i class="oui-icon oui-icon-copy-error aria-hidden="true"></i></span>
+                    <!--<span class="cui-clipboard__icon" data-ng-if="$ctrl.options.status === 'error'"><i class="oui-icon oui-icon-copy-error aria-hidden="true"></i></span>-->
                 </div>
                 `,
             controller: CuiClipboardController,
             bindings: {
                 text: "<",
-                label: "<?"
-            }
-        })
-        .component("cuiClipboardList", {
-            template: `
-                <div class="cui-clipboard-list">
-                    <div class="cui-clipboard-list__item">
-                        <ng-transclude></ng-transclude>
-                    </div>
-                </div>
-            `,
-            controller: CuiClipboardController,
-            transclude: true,
-            bindings: {
-                text: "<"
+                label: "<?",
+                id: "@?"
             }
         });
 })();
