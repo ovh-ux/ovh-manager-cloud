6c5f99a3520dcc2c485abf53a73d70c488ffa4b4
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-10-06 16:58:21 -0400

    upadte app.js to include decorate and remove it from config

--- client/app/app.js
+++ client/app/app.js
@@ -125,4 +125,26 @@ angular.module("managerApp", [
             };
             off();
         });
+
+    })
+    .run(($cookies, atInternetDecorator, TRACKING, TARGET, OvhApiMe) => {
+        "use strict";
+
+        let config = TRACKING[TARGET].config;
+        const referrerSite = $cookies.get("OrderCloud");
+
+        if (referrerSite) {
+            config.referrerSite = referrerSite;
+        }
+
+        OvhApiMe.Lexi().get().$promise
+            .then(me => {
+                config.countryCode = me.country;
+                config.currencyCode =  me.currency && me.currency.code;
+            })
+            .catch(err => console.log(err))
+            .finally(() => {
+                atInternetDecorator.setConfig(config);
+                atInternetDecorator.decorate();
+            });
     });
--- client/components/at-internet/at-internet.config.js
+++ client/components/at-internet/at-internet.config.js
@@ -14,49 +14,4 @@ angular.module("managerApp")
     });
 
 })
-.config(["$provide", function ($provide) {
-    "use strict";
 
-
-    $provide.decorator("atInternet", function ($delegate, $q, $cookies, OvhApiMe, TRACKING, TARGET) {
-        var delegateTrackPage = $delegate.trackPage;
-        var isDefaultConfigurationSet = false;
-        var trackPageRequestArgumentStack = [];
-
-        //Decorate trackPage to stack requests until At-internet default configuration is set
-        $delegate.trackPage = function () {
-            if (isDefaultConfigurationSet) {
-                delegateTrackPage.apply($delegate, arguments);
-            } else {
-                trackPageRequestArgumentStack.push(arguments);
-            }
-        };
-
-        OvhApiMe.Lexi().get().$promise.then(function (me) {
-            var settings = angular.copy(TRACKING[TARGET].config);
-
-            var referrerSite = $cookies.get("OrderCloud");
-            if (referrerSite) {
-                settings.referrerSite = referrerSite;
-            }
-
-            settings.countryCode = me.country;
-            settings.currencyCode =  me.currency && me.currency.code;
-            $delegate.setDefaults(settings);
-
-            isDefaultConfigurationSet = true;
-
-            _.forEach(trackPageRequestArgumentStack, function (trackPageArguments) {
-                delegateTrackPage.apply($delegate, trackPageArguments);
-            });
-
-        }).catch(function (err) {
-            $delegate.trackPage = function () {};
-            trackPageRequestArgumentStack = [];
-            $delegate.setEnabled(false);
-            return $q.reject(err);
-        });
-
-        return $delegate;
-    });
-}])
