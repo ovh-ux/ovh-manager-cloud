01f5e90469a05273448654e99227307eca63bb91
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-10-23 15:52:52 -0400

    update route name to avoid being blocked by ublock

--- client/app/dbaas/dbaas-metrics/metrics.js
+++ client/app/dbaas/dbaas-metrics/metrics.js
@@ -1,7 +1,7 @@
 angular.module("managerApp").config($stateProvider => {
     const metricsHeader =
         {
-            templateUrl: "app/dbaas/metrics/header/metrics-header.html",
+            templateUrl: "app/dbaas/dbaas-metrics/header/metrics-header.html",
             controller: "MetricsHeaderCtrl",
             controllerAs: "MetricsHeaderCtrl"
         };
@@ -9,67 +9,67 @@ angular.module("managerApp").config($stateProvider => {
     $stateProvider
         .state("dbaas.metrics", {
             url: "/metrics",
-            templateUrl: "app/dbaas/metrics/metrics.html",
+            templateUrl: "app/dbaas/dbaas-metrics/metrics.html",
             "abstract": true,
-            translations: ["common", "dbaas/metrics"]
+            translations: ["common", "dbaas/dbaas-metrics"]
         })
         .state("dbaas.metrics.detail", {
             url: "/{serviceName}",
             views: {
                 metricsContainer: {
-                    templateUrl: "app/dbaas/metrics/metrics-detail.html",
+                    templateUrl: "app/dbaas/dbaas-metrics/metrics-detail.html",
                     controller: "MetricsDetailCtrl",
                     controllerAs: "MetricsDetailCtrl"
                 }
             },
-            translations: ["common", "dbaas/metrics"]
+            translations: ["common", "dbaas/dbaas-metrics"]
         })
         .state("dbaas.metrics.detail.dashboard", {
             url: "/dashboard",
             views: {
                 metricsHeader,
                 metricsContent: {
-                    templateUrl: "app/dbaas/metrics/dashboard/metrics-dashboard.html",
+                    templateUrl: "app/dbaas/dbaas-metrics/dashboard/metrics-dashboard.html",
                     controller: "MetricsDashboardCtrl",
                     controllerAs: "MetricsDashboardCtrl"
                 }
             },
-            translations: ["common", "dbaas/metrics", "dbaas/metrics/dashboard", "dbaas/metrics/token"]
+            translations: ["common", "dbaas/dbaas-metrics", "dbaas/dbaas-metrics/dashboard", "dbaas/dbaas-metrics/token"]
         })
         .state("dbaas.metrics.detail.token", {
             url: "/token",
             views: {
                 metricsHeader,
                 metricsContent: {
-                    templateUrl: "app/dbaas/metrics/token/metrics-token.html",
+                    templateUrl: "app/dbaas/dbaas-metrics/token/metrics-token.html",
                     controller: "MetricsTokenCtrl",
                     controllerAs: "MetricsTokenCtrl"
                 }
             },
-            translations: ["common", "dbaas/metrics", "dbaas/metrics/token"]
+            translations: ["common", "dbaas/dbaas-metrics", "dbaas/dbaas-metrics/token"]
         })
         .state("dbaas.metrics.detail.token.add", {
             url: "/add",
             views: {
                 metricsHeader,
                 metricsSubContent: {
-                    templateUrl: "app/dbaas/metrics/token/add/metrics-token-add.html",
+                    templateUrl: "app/dbaas/dbaas-metrics/token/add/metrics-token-add.html",
                     controller: "MetricsTokenAddCtrl",
                     controllerAs: "MetricsTokenAddCtrl"
                 }
             },
-            translations: ["common", "dbaas/metrics", "dbaas/metrics/token", "dbaas/metrics/token/add"]
+            translations: ["common", "dbaas/dbaas-metrics", "dbaas/dbaas-metrics/token", "dbaas/dbaas-metrics/token/add"]
         })
         .state("dbaas.metrics.detail.platform", {
             url: "/platform",
             views: {
                 metricsHeader,
                 metricsContent: {
-                    templateUrl: "app/dbaas/metrics/platform/metrics-platform.html",
+                    templateUrl: "app/dbaas/dbaas-metrics/platform/metrics-platform.html",
                     controller: "MetricsPlatformCtrl",
                     controllerAs: "MetricsPlatformCtrl"
                 }
             },
-            translations: ["common", "dbaas/metrics", "dbaas/metrics/platform"]
+            translations: ["common", "dbaas/dbaas-metrics", "dbaas/dbaas-metrics/platform"]
         });
 });
