9dad7c33f39ddd2cec7074fa72da8ff85d213f13
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-11-02 13:39:58 -0400

    dasbhoard

--- client/app/vps/dashboard/vps-dashboard.controller.js
+++ client/app/vps/dashboard/vps-dashboard.controller.js
@@ -1,21 +1,40 @@
 class VpsDashboardCtrl {
-    constructor ($filter, $stateParams, CloudMessage, VpsService) {
+    constructor ($filter, $stateParams, CloudMessage, VpsActionService, VpsService) {
         this.$filter = $filter;
         this.$stateParams = $stateParams;
         this.CloudMessage = CloudMessage;
         this.serviceName = $stateParams.serviceName;
+        this.VpsActionService = VpsActionService;
         this.VpsService = VpsService;
 
         this.vps = {};
+        this.vpsPolling = {
+            rebootVm: false,
+            setNetboot: false,
+            setMonitoring: false,
+            changeRootPassword: false,
+            createSnapshot: false,
+            deleteSnapshot: false,
+            reinstallVm: false,
+            revertSnapshot: false,
+            restoreVeeamBackup: false,
+            removeVeeamBackup: false,
+            openConsoleAccess: false,
+            getConsoleUrl: false,
+            deliverVm: false,
+            restoreVm: false
+        };
 
         this.loaders = {
-            init: false
+            init: false,
+            polling: false
         }
     }
 
     $onInit () {
         this.loaders.init = true;
         this.loadData();
+        this.loadIps();
     }
 
     loadData () {
@@ -24,17 +43,29 @@ class VpsDashboardCtrl {
                 this.vps = vps;
                 const expiration = moment.utc(vps.expiration);
                 this.vps.expiration = moment([expiration.year(), expiration.month(), expiration.date()]).toDate();
-                this.vps.ipv6Gateway = _.get($scope, "vps.ipv6Gateway"); //If an old value is present, we preserve it in case loadIPs is not called after.  
                 this.vps.iconDistribution = vps.distribution ? "icon-" + vps.distribution.distribution : "";
-                this.vps.iconLocationCountry = vps.location ? "icon-" + vps.location.country : "";
-
                 if (vps.isExpired) {
                     this.CloudMessage.warning($translate.instant("common_service_expired", [vps.name]));
                 } else if (vps.messages.length > 0) {
                     this.CloudMessage.error($translate.instant("vps_dashboard_loading_error"), vps);
                 }
-                this.loaders.init = false;
-            });
+            })
+            .catch(err => this.CloudMessage.error(err))
+            .finally(() => { this.loaders.init = false });
+    }
+
+    loadIps () {
+        this.VpsService.getIps().then(ips => {
+            this.vps.ips = ips.results;
+            this.vps.ipv6Gateway = _.get(_.find(ips.results, { version: "v6" }), "gateway");
+        });
+    }
+
+    setAction (action) {
+        if (action == "reboot") {
+            this.VpsActionService.reboot();
+        }
+
     }
 
 }
