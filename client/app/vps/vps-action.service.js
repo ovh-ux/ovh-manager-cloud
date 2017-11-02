51ed7e4643daf0d38b34b1b097576c40249acac0
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-11-02 17:14:25 -0400

    modal update + DNSsec

--- client/app/vps/vps-action.service.js
+++ client/app/vps/vps-action.service.js
@@ -13,6 +13,29 @@ class VpsActionService {
         });
     }
 
+    deleteSecondaryDns (domain) {
+        // this.ControllerHelper.modal.showModal({
+        //     modalConfig: {
+        //         templateUrl: "app/vps/modal/reboot/vps-reboot.html",
+        //         controller: "VpsRebootCtrl",
+        //         controllerAs: "$ctrl",
+        //         resolve: {
+        //             domain: () => domain
+        //         }
+        //     }
+        // });
+    }
+
+    addSecondaryDns () {
+        this.ControllerHelper.modal.showModal({
+            modalConfig: {
+                templateUrl: "app/vps/modal/secondary-dns/add-secondary-dns.html",
+                controller: "AddSecondaryDnsCtrl",
+                controllerAs: "$ctrl"
+            }
+        });
+    }
+
 }
 
 angular.module("managerApp").service("VpsActionService", VpsActionService);
--- client/app/vps/vps.js
+++ client/app/vps/vps.js
@@ -33,27 +33,16 @@ angular.module("managerApp").config($stateProvider => {
                     controllerAs: "$ctrl"
                 }
             }
+        })
+        .state("iaas.vps.detail.secondary-dns", {
+            url: "/secondary-dns",
+            views: {
+                vpsHeader,
+                vpsContent: {
+                    templateUrl: "app/vps/secondary-dns/vps-secondary-dns.html",
+                    controller: "VpsSecondaryDnsCtrl",
+                    controllerAs: "$ctrl"
+                }
+            }
         });
-}).constant("additionalDisk.capacities", [
-    {
-        option: "additionalDisk50",
-        size: 50
-    },
-    {
-        option: "additionalDisk100",
-        size: 100
-    },
-    {
-        option: "additionalDisk200",
-        size: 200
-    },
-    {
-        option: "additionalDisk500",
-        size: 500
-    }
-]).constant("additionalDisk.hasNoOption",
-    "Does not have option"
-).constant("STOP_NOTIFICATION_USER_PREF", {
-    autoRenew: "VPS_AUTORENEW_STOP_BOTHER",
-    ipV6: "VPS_IPV6_STOP_NOTIFICATION"
 });
