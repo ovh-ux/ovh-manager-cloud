7cfcd0660c3973e78d280487b014e0139becd45c
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-11-02 17:14:25 -0400

    modal update + DNSsec

--- client/app/vps/modal/reboot/vps-reboot.controller.js
+++ client/app/vps/modal/reboot/vps-reboot.controller.js
@@ -1,7 +1,7 @@
 class VpsRebootCtrl {
     constructor ($translate, $uibModalInstance, CloudMessage, VpsService) {
         this.$translate = $translate;
-        this.$uibModalInstance;
+        this.$uibModalInstance = $uibModalInstance;
         this.CloudMessage = CloudMessage;
         this.VpsService = VpsService;
 
@@ -54,63 +54,3 @@ class VpsRebootCtrl {
 }
 
 angular.module("managerApp").controller("VpsRebootCtrl", VpsRebootCtrl);
-// angular.module("managerApp").controller("VpsRebootCtrl", [
-//     "$scope",
-//     "Module.vps.services.Vps",
-//     "CloudMessage",
-
-//     function ($scope,Vps,CloudMessage){
-//         "use strict";
-
-//         $scope.model = null;
-//         $scope.loader = {
-//             loading: false,
-//             task : false
-//         };
-//         $scope.selected = {
-//             rescue: false
-//         };
-
-//         $scope.loadCheck = function () {
-//             $scope.loader.task = true;
-//             Vps.getTaskInError().then(function (tasks){
-//                 loadVpsRescueMode(tasks);
-//             }, function (tasks){
-//                 loadVpsRescueMode(tasks);
-//             });
-//         };
-
-//         function loadVpsRescueMode(tasks) {
-//             if (!tasks || !tasks.length){
-//                 Vps.getSelected().then(function (data) {
-//                     $scope.model = data;
-//                     $scope.loader.task = false;
-//                 }, function () {
-//                     $scope.resetAction();
-//                     $scope.loader.task = false;
-//                     CloudMessage.error($scope.tr("vps_configuration_reboot_fail"), "ERROR", $scope.alertId);
-//                 });
-//             } else {
-//                 $scope.resetAction();
-//                 $scope.loader.task = false;
-//                 CloudMessage.error($scope.tr("vps_configuration_polling_fail"), "ERROR", $scope.alertId);
-//             }
-//         }
-
-
-//         $scope.rebootVps = function () {
-//             $scope.loader.loading = true;
-//             Vps.reboot($scope.selected.rescue).then(function () {
-//                 $scope.resetAction();
-//                 $scope.loader.loading = false;
-//                 CloudMessage.success($scope.tr("vps_configuration_reboot_success", $scope.vps.name), true, $scope.alertId);
-//             }, function (data) {
-//                 $scope.resetAction();
-//                 $scope.loader.loading = false;
-//                 CloudMessage.error($scope.tr("vps_configuration_reboot_fail"), data, $scope.alertId);
-//             });
-//         };
-
-//     }
-
-// ]);
