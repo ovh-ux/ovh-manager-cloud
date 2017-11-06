db39200e1113562ff312a5cdef0ffcf6c721e11c
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-11-06 18:00:40 -0500

    Chore(style): added big loader

--- client/app/vps/vps-detail.controller.js
+++ client/app/vps/vps-detail.controller.js
@@ -1,20 +1,27 @@
 class VpsDetailCtrl {
-    constructor ($filter, $q, $scope, $stateParams, $timeout, CloudMessage, VpsService) {
-        this.$timeout = $timeout;
+    constructor ($filter, $q, $scope, $stateParams, $translate, CloudMessage, VpsService) {
         this.$filter = $filter;
         this.$q = $q;
         this.$scope = $scope;
         this.$stateParams = $stateParams;
+        this.$translate = $translate;
         this.CloudMessage = CloudMessage;
         this.serviceName = $stateParams.serviceName;
         this.VpsService = VpsService;
 
+        this.loaders = {
+            init: false
+        };
         this.messages = [];
     }
 
     $onInit () {
+        this.loaders.init = true;
         this.loadMessage();
-        this.VpsService.getSelected(true).then(vps => { this.description = vps.displayName});
+        this.VpsService.getSelected(true)
+            .then(vps => { this.description = vps.displayName })
+            .catch(() => this.CloudMessage.error(this.$translate.instant("vps_dashboard_loading_error")))
+            .finally(() => { this.loaders.init = false });
         this.$scope.$on("changeDescription", (event, data) => {
             this.description = data;
         });
