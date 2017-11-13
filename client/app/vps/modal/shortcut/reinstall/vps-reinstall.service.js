d714209e8893120e2eb1f7b518b0424f805e6f4c
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-11-13 11:46:01 -0500

    chore(reinstall): reinstall modal services calls

--- client/app/vps/modal/shortcut/reinstall/vps-reinstall.service.js
+++ client/app/vps/modal/shortcut/reinstall/vps-reinstall.service.js
@@ -1,10 +1,41 @@
 class VpsReinstallService {
-    constructor (OvhApiMe) {
+    constructor ($http, $translate, CloudMessage, OvhApiMe) {
+        this.$http = $http;
+        this.$translate = $translate;
+        this.CloudMessage = CloudMessage;
         this.OvhApiMe = OvhApiMe;
     }
 
     getSshKeys () {
-        return this.OvhApiMe.SshKey().Lexi().get().$promise;
+        return this.OvhApiMe.SshKey().Lexi().query().$promise;
+    }
+
+    getPackages (image) {
+        this.$http.get("/distribution/image/vps/"+image)
+        .then(response => { return this.filterKernel(response.data) })
+        .catch(() => this.CloudMessage.error(this.$translate.instant("vps_configuration_reinstall_loading_summary_error")));
+    }
+
+    filterKernel (data) {
+        const packages = data.packages;
+        return _.filter(packages, pkg => {
+            return !_.includes((pkg.name + pkg.alias).toLowerCase(), "kernel");
+        });
+    }
+
+    getPackagesFiltered (image) {
+        const filteredPackages = [];
+        this.$http.get("/distribution/image/vps/"+image)
+            .then(response => {
+                const packages = response.data.packages;
+                _.filter(packages, pkg => {
+                    if (!_.includes((pkg.name + pkg.alias).toLowerCase(), "kernel")) {
+                        packages.push(pkg);
+                    }
+                });
+                return packages;
+            })
+            .catch(() => this.CloudMessage.error(this.$translate.instant("vps_configuration_reinstall_loading_summary_error")));
     }
 
 }
