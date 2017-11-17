3c5202444fe2839fe024328c873c1ad0913e8f2c
feoche <francoiseoche@gmail.com>
2017-11-17 16:46:06 +0100

    feat(VPS pages - Upgrade section): Create a single-page wizard
    Create a single-page wizard for upgrading VPS. Three steps for this part, need to create a generic
    component based on this sample

---
+++ client/app/vps/upgrade/vps-upgrade.controller.js
@@ -0,0 +1,96 @@
+class VpsUpgradeCtrl {
+    constructor ($filter, $translate, $q, VpsService) {
+        this.$filter = $filter;
+        this.$translate = $translate;
+        this.$q = $q;
+        this.Vps = VpsService;
+
+        this.loaders = {
+            init: false
+        };
+
+        this.step = 0;
+        this.order = null;
+        this.selectedModel = {};
+        this.upgradesList = null;
+        this.agree = {
+            value: false
+        };
+    }
+
+    $onInit () {
+        this.loaders.init = true;
+        this.loadNextStep();
+    }
+
+    canLoadNextStep (condition, step) {
+        if (condition) {
+            this.loadNextStep(step);
+        }
+    }
+
+    loadNextStep (step) {
+        this.step = (step || this.step) + 1;
+        switch (this.step) {
+            case 1:
+                this.loadUpgradesList();
+                break;
+            case 2:
+                this.upgradeVps();
+                this.loadNextStep();
+                break;
+            default:
+                break;
+        }
+    }
+
+    getCurrentModel () {
+        return _.find(this.upgradesList, upgrade => upgrade.isCurrentModel === true);
+    }
+
+    loadUpgradesList () {
+        this.loaders.step1 = true;
+        if (!this.upgradesList) {
+            return this.Vps.upgradesList().then(data => {
+                this.upgradesList = data.results;
+                return data;
+            }).catch(data => {
+                this.resetAction();
+                return this.$q.reject(data);
+            }).finally(() => {
+                this.loaders.step1 = false;
+            });
+        }
+    }
+
+    upgradeVps () {
+        this.loaders.step2 = true;
+        this.order = null;
+        const modelToUpgradeTo = $.grep(this.upgradesList, e => { return e.model === this.selectedModel.model; });
+        if (modelToUpgradeTo.length) {
+            this.selectedModelForUpgrade = modelToUpgradeTo[0];
+            return this.Vps.upgrade(this.selectedModelForUpgrade.model, this.selectedModelForUpgrade.duration.duration).then(data => {
+                this.agree.value = false;
+                this.selectedModelForUpgrade.duration.dateFormatted = this.$filter("date")(this.selectedModelForUpgrade.duration.date, "dd/MM/yyyy");
+                this.order = data;
+                return data;
+            }).catch(data => {
+                this.resetAction();
+                return this.$q.reject(data);
+            }).finally(() => {
+                this.loaders.step2 = false;
+            });
+        }
+    }
+
+    displayBC () {
+        this.resetAction();
+        window.open(
+            this.order.url,
+            "_blank"
+        );
+    }
+
+}
+
+angular.module("managerApp").controller("VpsUpgradeCtrl", VpsUpgradeCtrl);
