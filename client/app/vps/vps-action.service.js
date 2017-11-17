269fd6441c5c3fc88fe5166df23665f83f094e80
feoche <francoiseoche@gmail.com>
2017-11-17 16:46:06 +0100

    feat(VPS pages - Upgrade section): Create a single-page wizard
    Create a single-page wizard for upgrading VPS. Three steps for this part, need to create a generic
    component based on this sample

--- client/app/vps/vps-action.service.js
+++ client/app/vps/vps-action.service.js
@@ -1,6 +1,7 @@
 class VpsActionService {
-    constructor (ControllerHelper) {
+    constructor (ControllerHelper, $state) {
         this.ControllerHelper = ControllerHelper;
+        this.$state = $state;
     }
 
     password (serviceName) {
@@ -81,6 +82,10 @@ class VpsActionService {
                 }
             }
         });
+    }
+
+    upgrade () {
+        this.$state.go("iaas.vps.detail.upgrade");
     }
 
     editName (displayName, serviceName) {
