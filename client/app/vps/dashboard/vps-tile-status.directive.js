00f7ded1523c8106f4f314d6de85cb3f48190229
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-11-22 14:56:23 -0500

    status tile following CX guideline

---
+++ client/app/vps/dashboard/vps-tile-status.directive.js
@@ -0,0 +1,26 @@
+angular.module("managerApp")
+    .directive("vpsTileStatusItem", () => ({
+        template: `
+            <div class="vps-tile-status"
+                 data-ng-show="$ctrl.item"
+                 data-ng-class="{ 'vps-tile-status_grey' : $ctrl.striped}">
+                <i class="oui-icon vps-tile-status__icon"
+                   data-ng-class="{ 'oui-icon-success vps-tile-status__icon_success': $ctrl.item == 'OK',
+                                    'oui-icon-error vps-tile-status__icon_error': $ctrl.item == 'KO'}"></i>
+                <span class="vps-tile-status__text"
+                      data-ng-bind="$ctrl.text"></span>
+
+            </div>
+        `,
+        controller: class VpsTileStatusItemCtrl {},
+        controllerAs: "$ctrl",
+        transclude: true,
+        replace: true,
+        restrict: "E",
+        scope: true,
+        bindToController: {
+            item: "<",
+            text: "<",
+            striped: "<"
+        }
+    }));
