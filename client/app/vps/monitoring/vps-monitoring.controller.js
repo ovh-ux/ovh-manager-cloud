b38f4af0ef884af07b7e0f5d14749eec3152d247
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-12-05 09:25:32 -0500

    CR general review

--- client/app/vps/monitoring/vps-monitoring.controller.js
+++ client/app/vps/monitoring/vps-monitoring.controller.js
@@ -1,22 +1,17 @@
 class VpsMonitoringCtrl {
-    constructor ($q, $stateParams, $translate, CloudMessage, VpsActionService ,VpsService) {
+    constructor ($q, $stateParams, $translate, CloudMessage, VpsActionService, VpsMonitoringConstant, VpsService) {
         this.$q = $q;
         this.$translate = $translate;
         this.CloudMessage = CloudMessage;
         this.serviceName = $stateParams.serviceName;
         this.VpsActionService = VpsActionService;
+        this.VpsMonitoringConstant = VpsMonitoringConstant;
         this.VpsService = VpsService;
 
         this.loaders = {
             init: false
         };
         this.data = {};
-        this.monitoring = {
-            cpu: [],
-            ram: [],
-            net: [[], []],
-            labels: []
-        };
         this.period = "LASTDAY";
     }
 
@@ -74,80 +69,10 @@ class VpsMonitoringCtrl {
     }
 
     loadOptions () {
-        this.colors = ['#F1C40F', '#3498DB', '#717984', '#72C02C'];
+        this.colors = this.VpsMonitoringConstant.colors;
         this.series = [this.$translate.instant("vps_monitoring_network_netRx"), this.$translate.instant("vps_monitoring_network_netTx")];
-        this.option1 = {
-            scales: {
-                xAxes: [{
-                    gridLines: {
-                        display: false
-                    }
-                }],
-                yAxes: [{
-                    id: "y-axe",
-                    type: "linear",
-                    ticks: {
-                        min: 0,
-                        max: 100,
-                        beginAtZero: true
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: "%"
-
-                    }
-                }]
-            },
-            elements: {
-                line: {
-                    fill: "bottom",
-                    backgroundColor: "#59d2ef",
-                    borderColor: "#00a2bf",
-                    borderWidth: 4
-                },
-                point: {
-                    radius: 0
-                }
-            }
-        };
-        this.option2 = {
-            legend: {
-                display: true,
-            },
-            scales: {
-                xAxes: [{
-                    gridLines: {
-                        display: false
-                    }
-                }],
-                yAxes: [
-                {
-                    id: 'y-axis-1',
-                    type: 'linear',
-                    display: true,
-                    position: 'left',
-                    ticks: {
-                        min: 0,
-                        beginAtZero: true
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: "BPS"
-
-                    }
-                }
-              ]
-            },
-            elements: {
-                line: {
-                    borderColor: "#00a2bf",
-                    borderWidth: 4
-                },
-                point: {
-                    radius: 0
-                }
-            }
-        };
+        this.option1 = this.VpsMonitoringConstant.option1;
+        this.option2 = this.VpsMonitoringConstant.option2;
     }
 }
 
