91d89296a7a1d38341ad9790adf202807c646644
Jean-Daniel Pearson <1476965+JDownloader@users.noreply.github.com>
2017-12-06 14:24:45 -0500

    refactor vps

--- client/app/vps/import/polling.service.js
+++ client/app/vps/import/polling.service.js
@@ -1,26 +1,26 @@
-angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$timeout', function ($http, $q, $rootScope, $timeout) {
-    'use strict';
+angular.module("managerApp").service("Polling", ["$http", "$q", "$rootScope", "$timeout", function ($http, $q, $rootScope, $timeout) {
+    "use strict";
 
-    //@TODO when angular version will be >=1.2.x use Notify instead state param (resolveState / notifyState)
+    // @TODO when angular version will be >=1.2.x use Notify instead state param (resolveState / notifyState)
 
-    var to = true,
-        tofast = true,
-        watchedTasksPromise = [],
-        resolveState = 'resolve',
-        notifyState = 'notify',
-        notifyExist = 'alreadyExist',
+    let to = true;
+    let tofast = true;
+    let watchedTasksPromise = [];
+    const resolveState = "resolve";
+    const notifyState = "notify";
+    const notifyExist = "alreadyExist";
 
-        defaultElapse = 15000,
-        elapse = defaultElapse,
-        isRun = false,
-        watchedTasks = [],
-        killedScope = [],
+    const defaultElapse = 15000;
+    let elapse = defaultElapse;
+    let isRun = false;
+    let watchedTasks = [];
+    let killedScope = [];
 
-        defaultElapseFast = 5000,
-        elapsefast = defaultElapseFast,
-        isFastRun = false,
-        watchedTasksFast = [],
-        killedScopeFast = [];
+    const defaultElapseFast = 5000;
+    let elapsefast = defaultElapseFast;
+    let isFastRun = false;
+    let watchedTasksFast = [];
+    let killedScopeFast = [];
 
 
     this.setElapse = function (newElapse) {
@@ -30,7 +30,6 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
         elapsefast = newElapse;
     };
 
-
     /*
         poll after 5 sec
         taskUrl : url to get task status
@@ -39,21 +38,21 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
         cancelIfExist : no watch if task id is already in poll
     */
     this.addTaskFast = function (taskUrl, task, scopeId, cancelIfExist) {
-        var deferPromise = $q.defer();
+        const deferPromise = $q.defer();
         if (cancelIfExist && watchedTasksPromise[idtask(task.id || task.taskId)]) {
-            deferPromise.resolve({state : notifyExist});
+            deferPromise.resolve({ state: notifyExist });
         } else {
             if (!watchedTasksPromise[idtask(task.id || task.taskId)]) {
                 watchedTasksPromise[idtask(task.id || task.taskId)] = [];
             }
             watchedTasksPromise[idtask(task.id || task.taskId)].push({
                 pollPromise: deferPromise,
-                scopeId: scopeId
+                scopeId
             });
             watchedTasksFast[idtask(task.id || task.taskId)] = {
                 url: taskUrl,
-                task: task,
-                scopeId: scopeId
+                task,
+                scopeId
             };
             if (!isFastRun) {
                 run(true);
@@ -70,22 +69,22 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
         scopeId : the scope calling polling
         cancelIfExist : no watch if task id is already in poll
     */
-    this.addTask = function (taskUrl, task, scopeId, cancelIfExist){
-        var deferPromise = $q.defer();
-        if (cancelIfExist && watchedTasksPromise[idtask(task.id || task.taskId)]){
-            deferPromise.resolve({state : notifyExist});
-        }else{
+    this.addTask = function (taskUrl, task, scopeId, cancelIfExist) {
+        const deferPromise = $q.defer();
+        if (cancelIfExist && watchedTasksPromise[idtask(task.id || task.taskId)]) {
+            deferPromise.resolve({ state: notifyExist });
+        } else {
             if (!watchedTasksPromise[idtask(task.id || task.taskId)]) {
                 watchedTasksPromise[idtask(task.id || task.taskId)] = [];
             }
             watchedTasksPromise[idtask(task.id || task.taskId)].push({
                 pollPromise: deferPromise,
-                scopeId: scopeId
+                scopeId
             });
             watchedTasks[idtask(task.id || task.taskId)] = {
                 url: taskUrl,
-                task: task,
-                scopeId: scopeId
+                task,
+                scopeId
             };
             if (!isRun) {
                 run(false);
@@ -97,11 +96,11 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
     /*
         kill all tasks poll from scope id parameter
     */
-    this.addKilledScope = function(scopeId) {
-        if (scopeId){
+    this.addKilledScope = function (scopeId) {
+        if (scopeId) {
             killedScope[scopeId] = true;
             killedScopeFast[scopeId] = true;
-        }else{
+        } else {
             stopfast();
             stop();
             watchedTasksPromise = [];
@@ -109,7 +108,7 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
     };
 
     /* test state of polling return */
-    this.isResolve = function (state){
+    this.isResolve = function (state) {
         return state.state === resolveState;
     };
 
@@ -122,74 +121,72 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
     };
 
     this.isDone = function (state) {
-        return state.task && (state.task.status || state.task.state || "").toUpperCase() === 'DONE';
+        return state.task && (state.task.status || state.task.state || "").toUpperCase() === "DONE";
     };
 
 
-    function run(fast) {
-        if (fast){
-            if (!isFastRun){
+    function run (fast) {
+        if (fast) {
+            if (!isFastRun) {
                 isFastRun = true;
                 poll(true);
-            }else{
-                tofast = $timeout(function () {
+            } else {
+                tofast = $timeout(() => {
                     poll(true);
                 }, elapsefast);
             }
-        }else{
-            if (!isRun){
-                isRun = true;
+        } else if (!isRun) {
+            isRun = true;
+            poll(false);
+        } else {
+            to = $timeout(() => {
                 poll(false);
-            }else{
-                to = $timeout(function () {
-                    poll(false);
-                }, elapse);
-            }
+            }, elapse);
         }
     }
 
-    function stopPoll(fast){
+    function stopPoll (fast) {
         if (fast) {
             stopfast();
-        }else{
+        } else {
             stop();
         }
     }
 
-    function stop() {
+    function stop () {
         to = $timeout.cancel(to);
         isRun = false;
         clean();
     }
-    function stopfast() {
+    function stopfast () {
         tofast = $timeout.cancel(tofast);
         isFastRun = false;
         cleanFast();
     }
 
-    function clean(){
+    function clean () {
         elapse = defaultElapse;
         watchedTasks = [];
         killedScope = [];
     }
-    function cleanFast(){
+    function cleanFast () {
         elapsefast = defaultElapseFast;
         watchedTasksFast = [];
         killedScopeFast = [];
     }
 
     function poll (fast) {
-        var resultsTasks = [],
-            eachTask = null,
-            eachPromise = null,
-            watchedTasksList = [],
-            killedScopeList = [],
-            hasElements = false;
+        const resultsTasks = [];
+        let eachTask = null;
+        let eachPromise = null;
+        let watchedTasksList = [];
+        let killedScopeList = [];
+        let hasElements = false;
 
         if (fast) {
             watchedTasksList = watchedTasksFast;
             killedScopeList = killedScopeFast;
-        }else{
+        } else {
             watchedTasksList = watchedTasks;
             killedScopeList = killedScope;
         }
@@ -197,29 +194,29 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
         for (eachTask in watchedTasksList) {
             if (watchedTasksList.hasOwnProperty(eachTask)) {
 
-                //Delete polling of dead scope
-                if (killedScopeList[watchedTasksList[eachTask].scopeId]){
+                // Delete polling of dead scope
+                if (killedScopeList[watchedTasksList[eachTask].scopeId]) {
 
-                    //Delete promise of dead scope
+                    // Delete promise of dead scope
                     for (eachPromise in watchedTasksPromise[eachTask]) {
-                        if ((watchedTasksPromise[eachTask]).hasOwnProperty(eachPromise)) {
+                        if (watchedTasksPromise[eachTask].hasOwnProperty(eachPromise)) {
 
-                            if (killedScopeList[watchedTasksPromise[eachTask][eachPromise].scopeId]){
-                                delete (watchedTasksPromise[eachTask][eachPromise]);
+                            if (killedScopeList[watchedTasksPromise[eachTask][eachPromise].scopeId]) {
+                                delete watchedTasksPromise[eachTask][eachPromise];
                             }
                         }
                     }
 
-                    //Delete taskid of dead scope if promise = 0
-                    if (!watchedTasksPromise[eachTask] || watchedTasksPromise[eachTask].length === 0){
+                    // Delete taskid of dead scope if promise = 0
+                    if (!watchedTasksPromise[eachTask] || watchedTasksPromise[eachTask].length === 0) {
                         delete watchedTasksPromise[eachTask];
                         delete watchedTasksList[eachTask];
-                    }else{
+                    } else {
                         resultsTasks.push($http.get(watchedTasksList[eachTask].url));
                         hasElements = true;
                     }
 
-                }else{
+                } else {
                     resultsTasks.push($http.get(watchedTasksList[eachTask].url));
                     hasElements = true;
                 }
@@ -228,18 +225,18 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
 
         killedScopeList = [];
 
-        //STOP if no elements
-        if (!hasElements){
+        // STOP if no elements
+        if (!hasElements) {
             stopPoll(fast);
-        }else{
-            $q.all(resultsTasks).then(function (tasks) {
-                angular.forEach(tasks, function (task) {
+        } else {
+            $q.all(resultsTasks).then(tasks => {
+                angular.forEach(tasks, task => {
                     taskHandling(task, watchedTasksList);
                 });
                 run(fast);
 
-            },function (){
-                var eachTask2 = null;
+            }, () => {
+                let eachTask2 = null;
                 for (eachTask2 in watchedTasksList) {
                     if (watchedTasksList.hasOwnProperty(eachTask2)) {
                         cleanFailTask(eachTask2, watchedTasksList);
@@ -250,18 +247,18 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
         }
     }
 
-    function taskHandling(task, watchedTasksList) {
+    function taskHandling (task, watchedTasksList) {
 
-        var taskId = task.data.id || task.data.taskId;
+        const taskId = task.data.id || task.data.taskId;
 
         switch ((task.data.status || task.data.state || "").toUpperCase()) {
 
-            case 'DOING':
-            case 'INIT':
-            case 'TODO':
-            case 'PAUSED':
-            case 'WAITINGACK':
-                angular.forEach(watchedTasksPromise[idtask(taskId)], function (value) {
+            case "DOING":
+            case "INIT":
+            case "TODO":
+            case "PAUSED":
+            case "WAITINGACK":
+                angular.forEach(watchedTasksPromise[idtask(taskId)], value => {
                     if (value.pollPromise) {
                         value.pollPromise.resolve(
                             {
@@ -273,9 +270,9 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
                 });
                 break;
 
-            case 'CANCELLED':
-            case 'DONE':
-                angular.forEach(watchedTasksPromise[idtask(taskId)], function (value) {
+            case "CANCELLED":
+            case "DONE":
+                angular.forEach(watchedTasksPromise[idtask(taskId)], value => {
                     if (value.pollPromise) {
                         value.pollPromise.resolve(
                             {
@@ -287,14 +284,14 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
                 });
                 break;
 
-            case 'CUSTOMER_ERROR':
-            case 'OVH_ERROR':
-            case 'ERROR':
-            case 'BLOCKED':
-                angular.forEach(watchedTasksPromise[idtask(taskId)], function (value) {
+            case "CUSTOMER_ERROR":
+            case "OVH_ERROR":
+            case "ERROR":
+            case "BLOCKED":
+                angular.forEach(watchedTasksPromise[idtask(taskId)], value => {
                     if (value.pollPromise) {
                         value.pollPromise.reject({
-                            type: 'ERROR',
+                            type: "ERROR",
                             message: task.data.comment,
                             task: task.data
                         });
@@ -303,7 +300,7 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
                 break;
 
             default:
-                angular.forEach(watchedTasksPromise[idtask(taskId)], function (value) {
+                angular.forEach(watchedTasksPromise[idtask(taskId)], value => {
                     if (value.pollPromise) {
                         value.pollPromise.resolve(
                             {
@@ -319,19 +316,19 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
         delete watchedTasksList[idtask(taskId)];
     }
 
-    //Because if q.all reject, it don't sent fail task id
-    function cleanFailTask(eachTask, watchedTasksList){
-        var idTask = idtask(watchedTasksList[eachTask].task.id || watchedTasksList[eachTask].task.taskId);
+    // Because if q.all reject, it don't sent fail task id
+    function cleanFailTask (eachTask, watchedTasksList) {
+        const idTask = idtask(watchedTasksList[eachTask].task.id || watchedTasksList[eachTask].task.taskId);
 
-        $http.get(watchedTasksList[eachTask].url).then(function (task){
+        $http.get(watchedTasksList[eachTask].url).then(task => {
             taskHandling(task, watchedTasksList);
-        }, function (error){
-            angular.forEach(watchedTasksPromise[idTask], function (value) {
+        }, error => {
+            angular.forEach(watchedTasksPromise[idTask], value => {
                 //  Some API delete tasks right after their completition (ex: housing.)
                 if (error.status === 404) {
                     value.pollPromise.resolve({
-                            state: resolveState
-                        }
+                        state: resolveState
+                    }
                     );
                 }
 
@@ -342,7 +339,7 @@ angular.module('managerApp').service('Polling', ['$http', '$q', '$rootScope', '$
         });
     }
 
-    function idtask(taskId){
-        return 't' + taskId;
+    function idtask (taskId) {
+        return `t${taskId}`;
     }
 }]);
