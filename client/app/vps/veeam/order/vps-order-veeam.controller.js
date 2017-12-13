acb94aee02bce42f9f74afc3756e1568f1e2e6a1
Jean-Daniel Pearson <1476965+JDownloader@users.noreply.github.com>
2017-12-13 15:58:47 -0500

    UXMDC-69 complete vps veeam order

--- client/app/vps/veeam/order/vps-order-veeam.controller.js
+++ client/app/vps/veeam/order/vps-order-veeam.controller.js
@@ -1,21 +1,28 @@
 class VpsOrderVeeamCtrl {
-    constructor ($scope, $stateParams, $translate, $window, CloudMessage, VpsService) {
+    constructor ($scope, $stateParams, $translate, $window, CloudMessage, CloudNavigation, VpsService) {
         "use strict";
+        this.$scope = $scope;
+        this.$translate = $translate;
+        this.$window = $window;
+        this.CloudMessage = CloudMessage;
+        this.CloudNavigation = CloudNavigation;
         this.serviceName = $stateParams.serviceName;
+        this.VpsService = VpsService;
+
 
         $scope.model = {
-            vps: null,
-            optionDetails: null,
-            url: null,
-            contractsValidated: null
+            vps: undefined,
+            optionDetails: undefined,
+            url: undefined,
+            contractsValidated: undefined
         };
+
         VpsService.getSelectedVps(this.serviceName)
             .then(data => {
                 $scope.model.vps = data;
                 VpsService.getVeeamOption(this.serviceName)
                     .then(veeamOption => {
                         $scope.model.optionDetails = veeamOption;
-                        // TODO: Remove that !!!
                         VpsService.getPriceOptions($scope.model.vps).then(price => {
                             $scope.model.optionDetails.unitaryPrice = price.data.text;
                         });
@@ -26,25 +33,29 @@ class VpsOrderVeeamCtrl {
                 $scope.resetAction();
                 $scope.setMessage($translate.instant("vps_configuration_veeam_order_step1_loading_error"), data);
             });
+    }
 
+    $onInit () {
+        this.previousState = this.CloudNavigation.getPreviousState();
+    }
 
-        $scope.orderOption = () => {
-            if ($scope.model.optionDetails && $scope.model.contractsValidated) {
-                VpsService.orderVeeamOption(this.serviceName, $scope.model.optionDetails.duration.duration)
-                    .then(order => {
-                        $scope.model.url = order.url;
-                    }).catch(error => {
-                        CloudMessage.error($translate.instant("vps_configuration_veeam_order_fail"));
-                    });
-            } else if ($scope.model.contractsValidated) {
-                CloudMessage.error($translate.instant("vps_configuration_veeam_order_fail"));
-            }
-        };
+    orderOption () {
+        if (this.$scope.model.optionDetails && this.$scope.model.contractsValidated) {
+            this.VpsService.orderVeeamOption(this.serviceName, this.$scope.model.optionDetails.duration.duration)
+                .then(order => {
+                    this.$scope.model.url = order.url;
+                    this.$window.open(order.url, "_blank");
+                    this.previousState.go();
+                }).catch(error => {
+                    this.CloudMessage.error({ textHtml: this.$translate.instant("vps_configuration_veeam_order_fail") + " " + error.data });
+                });
+        } else if (this.$scope.model.contractsValidated) {
+            this.CloudMessage.error(this.$translate.instant("vps_configuration_veeam_order_fail"));
+        }
+    }
 
-        $scope.displayBC = function () {
-            $scope.resetAction();
-            $window.open($scope.model.url, "_blank");
-        };
+    cancel () {
+        this.previousState.go();
     }
 }
 
