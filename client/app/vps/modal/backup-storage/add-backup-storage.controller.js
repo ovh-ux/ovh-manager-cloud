50f3f250d57b60153610e32bdb7bb38bdf1114bf
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-12-20 13:30:07 -0500

    bugfix minor : translations +error handler

--- client/app/vps/modal/backup-storage/add-backup-storage.controller.js
+++ client/app/vps/modal/backup-storage/add-backup-storage.controller.js
@@ -42,7 +42,13 @@ class AddBackupStorageCtrl {
     confirm () {
         this.loader.save = true;
         this.VpsService.postBackupStorageAccess(this.serviceName, this.model.ip, this.model.ftp, this.model.nfs, this.model.cifs)
-            .then(() => this.CloudMessage.success(this.$translate.instant("vps_backup_storage_access_add_success")))
+            .then(data => {
+                if (data.state == "ERROR") {
+                    this.CloudMessage.error(data.messages[0].message || this.$translate.instant("vps_backup_storage_access_add_failure"));
+                } else {
+                    this.CloudMessage.success(this.$translate.instant("vps_backup_storage_access_add_success"));
+                }
+            })
             .catch(() => this.CloudMessage.error(this.$translate.instant("vps_backup_storage_access_add_failure")))
             .finally(() => {
                 this.loader.save = false;
