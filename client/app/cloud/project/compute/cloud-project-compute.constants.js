3628dbe606f2e40cae5527a1eb1fb086f4d270e9
Alveric Alie <alveric.alie@corp.ovh.com>
2017-12-20 08:58:46 -0500

    Revert "Merge pull request #339 from ovh-ux/feature/ads"
    This reverts commit 5b6289e7052264fa65c09323eeba7aa7886c06f0, reversing
    changes made to 359c9a4680edd70107902e96fb7bfcfb9cb3120b.

--- client/app/cloud/project/compute/cloud-project-compute.constants.js
+++ client/app/cloud/project/compute/cloud-project-compute.constants.js
@@ -1,15 +1,69 @@
 angular.module("managerApp")
     .constant("PCI_ANNOUNCEMENTS", [{
-        type: "image",
-        messageId: "cloud_message_pci_bandwitdh_xmas",
-        messageStart: "2017-12-18",
-        messageEnd: "2018-01-08",
-        sources: {
-            en: "assets/images/announcement/bandwidth/UK_BANNER.png",
-            fr: "assets/images/announcement/bandwidth/FR_BANNER.png"
-        },
-        description: {
-            en: "Announcement: From December 19th until January 8th inclusive, get up to 10 GBits/s of network bandwidth (overall) for C2-120, B2-120, R2-240 and G3-120 instances",
-            fr: "Annonce: Du 19 décembre au 8 janvier inclus, profitez d'une bande passante réseau allant jusqu'à 10 Gbits/s (au global) pour les instances C2-120, B2-120, R2-240 et G3-120"
+        type: "info",
+        messageId: "cloud_message_pci_de1",
+        messageStart: "2017-10-12",
+        messageEnd: "2017-10-27",
+        hasLinkText: true,
+        linkURL: {
+            FR: "https://www.ovh.com/fr/public-cloud/instances/datacenters.xml",
+            TN: "https://www.ovh.com/tn/public-cloud/instances/datacenters.xml",
+            MA: "https://www.ovh.com/ma/public-cloud/instances/datacenters.xml",
+            SN: "https://www.ovh.sn/public-cloud/instances/datacenters.xml",
+            DE: "https://www.ovh.de/public-cloud/instances/datacenters.xml",
+            ES: "https://www.ovh.es/public-cloud/instancias/datacenters.xml",
+            WS: "https://www.ovh.com/us/es/public-cloud/instancias/datacenters.xml",
+            FI: "https://www.ovh-hosting.fi/public-cloud/instances/datacenters.xml",
+            EN: "https://www.ovh.co.uk/public-cloud/instances/datacenters.xml",
+            IT: "https://www.ovh.it/public-cloud/istanze/datacenters.xml"
         }
-    }]);
+    },{
+        type: "info",
+        messageId: "cloud_message_pci_uk1",
+        messageStart: "2017-10-31",
+        messageEnd: "2017-11-19",
+        hasLinkText: true,
+        linkURL: {
+            FR: "https://www.ovh.com/fr/public-cloud/instances/datacenters.xml",
+            TN: "https://www.ovh.com/tn/public-cloud/instances/datacenters.xml",
+            MA: "https://www.ovh.com/ma/public-cloud/instances/datacenters.xml",
+            SN: "https://www.ovh.sn/public-cloud/instances/datacenters.xml",
+            DE: "https://www.ovh.de/public-cloud/instances/datacenters.xml",
+            ES: "https://www.ovh.es/public-cloud/instancias/datacenters.xml",
+            WS: "https://www.ovh.com/us/es/public-cloud/instancias/datacenters.xml",
+            FI: "https://www.ovh-hosting.fi/public-cloud/instances/datacenters.xml",
+            EN: "https://www.ovh.co.uk/public-cloud/instances/datacenters.xml",
+            IT: "https://www.ovh.it/public-cloud/istanze/datacenters.xml"
+        }
+    }, {
+        type: "info",
+        messageId: "cloud_message_pci_g3",
+        messageStart: "2017-10-12",
+        messageEnd: "2017-10-27",
+        linkURL: {
+            FR: "https://www.ovh.com/fr/public-cloud/instances/gpu/",
+            TN: "https://www.ovh.com/tn/public-cloud/instances/gpu/",
+            MA: "https://www.ovh.com/ma/public-cloud/instances/gpu/",
+            SN: "https://www.ovh.sn/public-cloud/instances/gpu/",
+            DE: "https://www.ovh.de/public-cloud/instances/gpu/",
+            ES: "https://www.ovh.es/public-cloud/instancias/gpu/",
+            FI: "https://www.ovh-hosting.fi/public-cloud/instances/gpu/",
+            CZ: "http://www.ovh.cz/public-cloud/instances/gpu/",
+            GB: "http://www.ovh.co.uk/public-cloud/instances/gpu/",
+            EN: "http://www.ovh.co.uk/public-cloud/instances/gpu/",
+            IT: "http://www.ovh.it/public-cloud/instances/gpu/",
+            LT: "http://www.ovh.lt/public-cloud/instances/gpu/",
+            NL: "http://www.ovh.nl/public-cloud/instances/gpu/",
+            PL: "https://www.ovh.pl/public-cloud/instances/gpu/",
+            PT: "https://www.ovh.pt/public-cloud/instances/gpu/",
+            ASIA: "https://www.ovh.com/asia/public-cloud/instances/gpu/",
+            AU: "https://www.ovh.com.au/public-cloud/instances/gpu/",
+            CA: "https://www.ovh.co.uk/public-cloud/instances/gpu/",
+            QC: "https://www.ovh.com/fr/public-cloud/instances/gpu/",
+            SG: "https://www.ovh.com/sg/public-cloud/instances/gpu/",
+            WE: "https://www.ovh.co.uk/public-cloud/instances/gpu/",
+            WS: "https://www.ovh.com/us/es/public-cloud/instancias/gpu/",
+            US: "https://www.ovh.us/public-cloud/instances/gpu/"
+        }
+    }
+    ]);
