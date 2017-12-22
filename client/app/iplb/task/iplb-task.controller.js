efefb6e878210bc96e79dcbc35294f32d36b76d6
Kevin Manson <kevin.manson@corp.ovh.com>
2017-12-22 09:50:18 -0500

    feat(iplb): use ouiDatagrid

--- client/app/iplb/task/iplb-task.controller.js
+++ client/app/iplb/task/iplb-task.controller.js
@@ -48,26 +48,6 @@ class IpLoadBalancerTaskCtrl {
             }
         });
     }
-
-    actionTemplate () {
-        return `
-            <cui-dropdown-menu>
-                <cui-dropdown-menu-button>
-                    <ng-include src="'app/ui-components/icons/button-action.html'"></ng-include>
-                </cui-dropdown-menu-button>
-                <cui-dropdown-menu-body>
-                    <div class="oui-action-menu">
-                        <div class="oui-action-menu__item oui-action-menu-item">
-                            <div class="oui-action-menu-item__icon"></div>
-                            <button class="oui-button oui-button_link oui-action-menu-item__label"
-                                type="button"
-                                data-ng-bind="'common_preview_see' | translate"
-                                data-ng-click="ctrl.showTaskPreview($row)"></button>
-                        </div>
-                    </div>
-                </cui-dropdown-menu-body>
-            <cui-dropdown-menu>`;
-    }
 }
 
 angular.module("managerApp").controller("IpLoadBalancerTaskCtrl", IpLoadBalancerTaskCtrl);
