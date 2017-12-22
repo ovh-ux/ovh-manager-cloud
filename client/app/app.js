2fcd27f03164121dafaf9651744c267a2bda1670
Kevin Manson <kevin.manson@corp.ovh.com>
2017-12-22 09:33:25 -0500

    feat(uiKit): add uiKit v2 (replace ouiTable, new styles...)

--- client/app/app.js
+++ client/app/app.js
@@ -110,7 +110,8 @@ angular.module("managerApp", [
               </div>
             `);
     })
-    .run(($rootScope, $translate, $translatePartialLoader, ouiTableConfiguration) => {
+    .run(($rootScope, $translate, $translatePartialLoader, ouiTableConfiguration,
+          ouiDatagridConfiguration, ouiPaginationConfiguration) => {
         "use strict";
         $translatePartialLoader.addPart("components");
 
@@ -123,7 +124,19 @@ angular.module("managerApp", [
                 next: $translate.instant("common_pagination_next"),
                 previous: $translate.instant("common_pagination_previous")
             };
+
+            ouiDatagridConfiguration.translations = {
+                emptyPlaceholder: $translate.instant("common_datagrid_nodata")
+            };
+
+            ouiPaginationConfiguration.translations = {
+                resultsPerPage: $translate.instant("common_pagination_resultsperpage"),
+                ofNResults: $translate.instant("common_pagination_ofnresults"),
+                currentPageOfPageCount: $translate.instant("common_pagination_currentpageofpagecount"),
+                previousPage: $translate.instant("common_pagination_previous"),
+                nextPage: $translate.instant("common_pagination_next")
+            };
+
             off();
         });
-
     });
