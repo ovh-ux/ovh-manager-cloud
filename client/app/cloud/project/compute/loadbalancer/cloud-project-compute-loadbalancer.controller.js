75ce3caa2fb6d859e23d93b5380b917cf83ae84e
Kevin Manson <kevin.manson@corp.ovh.com>
2018-01-16 10:38:14 -0500

    fix(pci): use datagrid in iblb tab

--- client/app/cloud/project/compute/loadbalancer/cloud-project-compute-loadbalancer.controller.js
+++ client/app/cloud/project/compute/loadbalancer/cloud-project-compute-loadbalancer.controller.js
@@ -1,7 +1,8 @@
 class CloudProjectComputeLoadbalancerCtrl {
-    constructor ($q, $translate, $stateParams, CloudProjectComputeLoadbalancerService, OvhApiCloudProjectIplb, OvhApiIpLoadBalancing, CloudMessage, OvhApiMe, URLS) {
+    constructor ($q, $translate, $state, $stateParams, CloudProjectComputeLoadbalancerService, OvhApiCloudProjectIplb, OvhApiIpLoadBalancing, CloudMessage, OvhApiMe, URLS) {
         this.$q = $q;
         this.$translate = $translate;
+        this.$state = $state;
         this.CloudProjectComputeLoadbalancerService = CloudProjectComputeLoadbalancerService;
         this.OvhApiCloudProjectIplb = OvhApiCloudProjectIplb;
         this.OvhApiIpLoadBalancing = OvhApiIpLoadBalancing;
@@ -33,6 +34,18 @@ class CloudProjectComputeLoadbalancerCtrl {
         this.getLoadbalancers(true);
     }
 
+    goToLoadbalancerConfigure (serviceName) {
+        this.$state.go("iaas.pci-project.compute.loadbalancerConfigure", {
+            loadbalancerId: serviceName
+        });
+    }
+
+    goToIPLB (serviceName) {
+        this.$state.go("network.iplb.detail.home", {
+            serviceName
+        });
+    }
+
     getLoadbalancers (clearCache) {
         if (!this.loaders.table.loadbalancer) {
             this.loaders.table.loadbalancer = true;
@@ -42,25 +55,24 @@ class CloudProjectComputeLoadbalancerCtrl {
             }
             this.$q.all({
                 loadbalancers:
-                this.OvhApiIpLoadBalancing.Lexi().query().$promise.then(response => this.$q.all(
-                    _.map(response, id => this.CloudProjectComputeLoadbalancerService.getLoadbalancer(id))
-                )),
-                laodbalancersImportedArray:
-                this.OvhApiCloudProjectIplb.Lexi().query({
-                    serviceName: this.serviceName
-                }).$promise.then(ids => this.$q.all(
-                    _.map(ids, id =>
-                        this.OvhApiCloudProjectIplb.Lexi().get({
-                            serviceName: this.serviceName,
-                            id
-                        }).$promise
-                    )
-                )
-                )
-            }).then(({ loadbalancers, laodbalancersImportedArray }) => {
+                    this.OvhApiIpLoadBalancing.Lexi().query().$promise.then(response => this.$q.all(
+                        _.map(response, id => this.CloudProjectComputeLoadbalancerService.getLoadbalancer(id))
+                    )),
+                loadbalancersImportedArray:
+                    this.OvhApiCloudProjectIplb.Lexi().query({
+                        serviceName: this.serviceName
+                    }).$promise.then(ids => this.$q.all(
+                        _.map(ids, id =>
+                            this.OvhApiCloudProjectIplb.Lexi().get({
+                                serviceName: this.serviceName,
+                                id
+                            }).$promise
+                        )
+                    ))
+            }).then(({ loadbalancers, loadbalancersImportedArray }) => {
             // Create a map of imported loadbalancers
                 const loadBalancerImported = {};
-                _.forEach(laodbalancersImportedArray, lb => { loadBalancerImported[lb.iplb] = lb; });
+                _.forEach(loadbalancersImportedArray, lb => { loadBalancerImported[lb.iplb] = lb; });
 
                 // Set openstack importation status
                 this.table.loadbalancer = _.map(loadbalancers, lb => {
@@ -76,34 +88,6 @@ class CloudProjectComputeLoadbalancerCtrl {
                 this.CloudMessage.error([this.$translate.instant("cpc_loadbalancer_error"), err.data && err.data.message || ""].join(" "));
             }).finally(() => { this.loaders.table.loadbalancer = false; });
         }
-    }
-
-    actionTemplate () {
-        return `
-                <cui-dropdown-menu>
-                   <cui-dropdown-menu-button>
-                       <ng-include src="'app/ui-components/icons/button-action.html'"></ng-include>
-                   </cui-dropdown-menu-button>
-                   <cui-dropdown-menu-body>
-                       <div class="oui-action-menu">
-                           <div class="oui-action-menu__item oui-action-menu-item" ng-if="$row.status !== 'unavailable'">
-                                   <div class="oui-action-menu-item__icon"><i class="glyphicon glyphicon-edit right-space-m8"></i></div>
-                                   <a class="oui-button oui-button_link oui-action-menu-item__label"
-                                      data-ui-sref="iaas.pci-project.compute.loadbalancerConfigure({'loadbalancerId' : $row.serviceName})"
-                                      data-translate="cpc_loadbalancer_actions_configure">
-                                   </a>
-                            </div>
-                            <div class="oui-action-menu__item oui-action-menu-item" ng-if="$row.status === 'unavailable'">
-                                    <div class="oui-action-menu-item__icon"><i class="glyphicon glyphicon-edit right-space-m8"></i></div>
-                                    <a class="oui-button oui-button_link oui-action-menu-item__label"
-                                       data-ui-sref="network.iplb.detail.home({'serviceName': $row.serviceName})"
-                                       data-translate="cpc_loadbalancer_configure_advanced">
-                                    </a>
-                             </div>
-                       </div>
-                   </cui-dropdown-menu-body>
-               </cui-dropdown-menu>
-        `;
     }
 }
 
