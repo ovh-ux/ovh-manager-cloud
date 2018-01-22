e1f52dc6b49c2c4977397753492a7550bc7efb98
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2018-01-22 11:52:21 -0500

    Improvement(vps): datagrid

--- client/app/vps/secondary-dns/vps-secondary-dns.controller.js
+++ client/app/vps/secondary-dns/vps-secondary-dns.controller.js
@@ -41,28 +41,6 @@ class VpsSecondaryDnsCtrl {
             .finally(() => this.refreshSecondaryDnsList());
     }
 
-    actionTemplate () {
-        return `
-            <cui-dropdown-menu>
-                <cui-dropdown-menu-button>
-                    <ng-include src="'app/ui-components/icons/button-action.html'"></ng-include>
-                </cui-dropdown-menu-button>
-                <cui-dropdown-menu-body>
-                    <div class="oui-action-menu">
-                        <div class="oui-action-menu__item oui-action-menu-item">
-                            <div class="oui-action-menu-item__icon">
-                            </div>
-                            <button class="oui-button oui-button_link oui-action-menu-item__label"
-                                type="button"
-                                data-translate="common_delete"
-                                data-ng-click="$ctrl.deleteOne($row)"></button>
-                        </div>
-                    </div>
-                </cui-dropdown-menu-body>
-            </cui-dropdown-menu>
-        `;
-    }
-
 }
 
 angular.module("managerApp").controller("VpsSecondaryDnsCtrl", VpsSecondaryDnsCtrl);
