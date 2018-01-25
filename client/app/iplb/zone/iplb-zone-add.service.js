52173b7e81799e78b324dd6888cc982fa6650f7d
Mathieu <mathieu.boulianne@corp.ovh.com>
2018-01-25 10:46:04 -0500

    Prepare for zone activation

--- client/app/iplb/zone/iplb-zone-add.service.js
+++ client/app/iplb/zone/iplb-zone-add.service.js
@@ -11,9 +11,20 @@ class IpLoadBalancerZoneAddService {
     }
 
     getOrderableZones (serviceName) {
-        return this.OvhApiIpLoadBalancing.Lexi().get({ serviceName })
-            .$promise
-            .then(response => _.map(response.orderableZone, zone => _.extend(zone, this.RegionService.getRegion(zone.name))))
+        return this.$q.all({
+            orderableZones: this._getOrderableZones(serviceName),
+            suspendedZones: this._getSuspendedZones(serviceName)
+        })
+            .then(response => {
+                const availableZones = response.orderableZones.concat(response.suspendedZones);
+                return _.map(availableZones, zone => _.extend(zone, this.RegionService.getRegion(zone.name)));
+            })
+            .then(availableZones => _.map(availableZones, zone => _.extend(zone, {
+                selectable: {
+                    value: true,
+                    reason: zone.state === "released" ? this.$translate.instant("iplb_zone_add_available_released") : ""
+                }
+            })))
             .catch(this.ServiceHelper.errorHandler("iplb_zone_add_loading_error"));
     }
 
@@ -39,6 +50,24 @@ class IpLoadBalancerZoneAddService {
                 })(response);
             })
             .catch(this.ServiceHelper.errorHandler(zones.length > 1 ? "iplb_zone_add_plural_error" : "iplb_zone_add_single_error"));
+    }
+
+    _getOrderableZones (serviceName) {
+        return this.OvhApiIpLoadBalancing.Lexi().get({ serviceName })
+            .$promise
+            .then(response => _.map(response.orderableZone, zone => ({
+                name: zone
+            })));
+    }
+
+    _getSuspendedZones (serviceName) {
+        return this.OvhApiIpLoadBalancing.Zone().Lexi().query({ serviceName })
+            .$promise
+            .then(zoneIds => {
+                const promises = _.map(zoneIds, zoneId => this.OvhApiIpLoadBalancing.Zone().Lexi().get({ serviceName, name: zoneId }).$promise);
+                return this.$q.all(promises);
+            })
+            .then(zones => _.filter(zones, zone => zone.state === "released"));
     }
 }
 
