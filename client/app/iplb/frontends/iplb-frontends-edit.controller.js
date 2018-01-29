d1b92bb3a73ecb92e2ec2f3d0b6b5d7459d9193d
Mathieu <mathieu.boulianne@corp.ovh.com>
2018-01-29 14:00:29 -0500

    hotfix (iplb) fix iplb frontend add (ssk / udp)

--- client/app/iplb/frontends/iplb-frontends-edit.controller.js
+++ client/app/iplb/frontends/iplb-frontends-edit.controller.js
@@ -212,6 +212,11 @@ class IpLoadBalancerFrontendsEditCtrl {
         if (this.type === "udp") {
             delete request.ssl;
         }
+
+        if (_.includes(["udp", "tcp"], this.type)) {
+            delete request.hsts;
+        }
+
         if (!request.ssl || !request.defaultSslId) {
             delete request.defaultSslId;
         }
