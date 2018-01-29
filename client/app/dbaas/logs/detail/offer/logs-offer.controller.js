16df6626db017040bd59633e72172a6b443f733f
varun shivaprasad <varun257@gmail.com>
2018-01-29 12:21:38 +0100

    Feature for offers
    - Populates list of offers
    - Selects offer based on previously selected offers
    - Validation to save only if offer selection is changed
    - Saves offer and redirects to payment page
    
    Feature: UML-3

--- client/app/dbaas/logs/detail/offer/logs-offer.controller.js
+++ client/app/dbaas/logs/detail/offer/logs-offer.controller.js
@@ -1,7 +1,46 @@
 class LogsOfferCtrl {
-    constructor ($stateParams) {
+    constructor (LogsOfferService, $stateParams, ControllerHelper, LogsOrderService, OrderHelperService, $window) {
         this.$stateParams = $stateParams;
         this.serviceName = this.$stateParams.serviceName;
+        this.LogsOfferService = LogsOfferService;
+        this.LogsOrderService = LogsOrderService;
+        this.ControllerHelper = ControllerHelper;
+        this.OrderHelperService = OrderHelperService;
+        this.$window = $window;
+        this.quantity = 1;
+        this.selectedOffer = "";
+        this.currentOffer = "";
+        this._initLoaders();
+    }
+
+    $onInit () {
+        // this.LogsOfferService.getOffer(this.serviceName);
+        this.getSelectedPlan.load();
+        this.offers.load();
+    }
+
+    _initLoaders () {
+        this.getSelectedPlan = this.ControllerHelper.request.getArrayLoader({
+            loaderFunction: () => this.LogsOfferService.getOffer(this.serviceName)
+                .then(selectedPlan => this.selectOffer(selectedPlan))
+        });
+
+        this.offers = this.ControllerHelper.request.getArrayLoader({
+            loaderFunction: () => this.LogsOrderService.getOrder(this.serviceName)
+        });
+    }
+
+    selectOffer (offerObj) {
+        this.selectedOffer = offerObj.reference;
+        this.currentOffer = offerObj.reference;
+    }
+
+    saveOffer () {
+        this.savingOffer = this.ControllerHelper.request.getArrayLoader({
+            loaderFunction: () => this.LogsOrderService.saveOrder(this.serviceName, this.selectedOffer, this.quantity)
+                .then(response => this.$window.open(response.data.order.url, "_self"))
+        });
+        this.savingOffer.load();
     }
 }
 
--- client/app/dbaas/logs/detail/offer/logs-offer.js
+++ client/app/dbaas/logs/detail/offer/logs-offer.js
@@ -9,6 +9,6 @@ angular.module("managerApp").config($stateProvider => {
                     controllerAs: "ctrl"
                 }
             },
-            translations: ["common", "dbaas/logs", "dbaas/logs/offer"]
+            translations: ["common", "dbaas/logs", "dbaas/logs/detail/offer"]
         });
 });
