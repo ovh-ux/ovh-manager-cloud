7a258c5fa4e4c1e35868264994056a71453689e6
Ravindra Adireddy <ravindra.adireddy@corp.ovh.com>
2018-02-05 13:00:01 +0530

    added constants service for log streams

---
+++ client/app/dbaas/logs/detail/streams/logs-streams.constants.js
@@ -0,0 +1,10 @@
+angular.module("managerApp").constant("LogStreamsConstants", {
+    MESSAGE_THRESHOLD: 1000,
+    WEB_SOCKET_URL: "WEB_SOCKET",
+    RFC_URL: "TCP_TLS_RFC5424",
+    LTSV_URL: "TCP_TLS_LTSV_NUL",
+    GELF_URL: "TCP_TLS_GELF",
+    GELF: "GELF",
+    LTSV: "LTSV",
+    RFC5424: "RFC5424"
+});
