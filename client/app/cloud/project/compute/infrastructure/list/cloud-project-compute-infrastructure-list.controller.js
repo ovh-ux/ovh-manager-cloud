e1459d04bf4ed004c7249a696c2653898e0df210
rfougier <romain.fougier@corp.ovh.com>
2018-02-07 16:47:25 -0500

    :sparkles: feat(pci): Improve price displaying and list view

--- client/app/cloud/project/compute/infrastructure/list/cloud-project-compute-infrastructure-list.controller.js
+++ client/app/cloud/project/compute/infrastructure/list/cloud-project-compute-infrastructure-list.controller.js
@@ -25,6 +25,10 @@ class CloudProjectComputeInfrastructureListCtrl {
             items: undefined
         };
 
+        this.$scope.$watchCollection(() => _.get(this.infra, "vrack.publicCloud.sortedKeys"), (newValues, oldValues) => {
+            this.addOrRemoveInstance(newValues, oldValues);
+        });
+
         return this.initInfra();
     }
 
@@ -47,6 +51,22 @@ class CloudProjectComputeInfrastructureListCtrl {
         }).finally(() => {
             this.loaders.infra = false;
         });
+    }
+
+    addOrRemoveInstance (newIds, oldIds) {
+        if (oldIds != null) {
+            if (newIds.length > oldIds.length) {
+                const foundId = _.find(newIds, key => _.indexOf(oldIds, key) === -1);
+                const foundItem = this.infra.vrack.publicCloud.items[foundId];
+                if (foundItem) {
+                    _.set(foundItem, "volumes", _.get(this.volumes, foundItem.id, []));
+                    this.table.items.push(foundItem);
+                }
+            } else if (newIds.length < oldIds.length) {
+                const foundId = _.find(oldIds, key => _.indexOf(newIds, key) === -1);
+                _.remove(this.table.items, item => item.id === foundId);
+            }
+        }
     }
 }
 
