673b19c33b3d802f599ebb91c1de75b95b5fa8cd
Ludovic Lamarche <ludovic.lamarche@corp.ovh.com>
2018-02-14 15:54:15 +0100

    remove deprecated CloudPrice

--- client/app/cloud/project/compute/infrastructure/list/cloud-project-compute-infrastructure-list.controller.js
+++ client/app/cloud/project/compute/infrastructure/list/cloud-project-compute-infrastructure-list.controller.js
@@ -1,7 +1,7 @@
 class CloudProjectComputeInfrastructureListCtrl {
     constructor ($scope, $q, $stateParams, $translate,
                  CloudMessage, CloudProjectOrchestrator, CloudProjectComputeInfrastructureService,
-                 OvhApiCloudPrice, OvhApiCloudProjectVolume, RegionService) {
+                 OvhApiCloudProjectVolume, RegionService) {
         this.$scope = $scope;
         this.$q = $q;
         this.$stateParams = $stateParams;
@@ -9,7 +9,6 @@ class CloudProjectComputeInfrastructureListCtrl {
         this.CloudMessage = CloudMessage;
         this.CloudProjectOrchestrator = CloudProjectOrchestrator;
         this.InfrastructureService = CloudProjectComputeInfrastructureService;
-        this.OvhApiCloudPrice = OvhApiCloudPrice;
         this.OvhApiCloudProjectVolume = OvhApiCloudProjectVolume;
         this.RegionService = RegionService;
     }
@@ -32,12 +31,10 @@ class CloudProjectComputeInfrastructureListCtrl {
         this.loaders.infra = true;
         return this.$q.all({
             infra: this.CloudProjectOrchestrator.initInfrastructure({ serviceName: this.serviceName }),
-            prices: this.OvhApiCloudPrice.Lexi().query().$promise.then(prices => (this.prices = prices)).catch(this.prices = {}),
             volumes: this.CloudProjectOrchestrator.initVolumes({ serviceName: this.serviceName }).then(volumes => (this.volumes = _.get(volumes, "volumes")))
         }).then(({ infra }) => {
             this.infra = infra;
             this.table.items = _.map(this.infra.vrack.publicCloud.items, instance => {
-                _.set(instance, "price", _.find(this.prices.instances, flavor => flavor.flavorId === _.get(instance, "flavor.id")));
                 _.set(instance, "volumes", _.get(this.volumes, instance.id, []));
                 return instance;
             });
