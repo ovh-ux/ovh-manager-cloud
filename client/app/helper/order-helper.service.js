4c19624185c0ac08cacf3e372481d8d6e32ea2ae
rfougier <romain.fougier@corp.ovh.com>
2018-02-16 13:45:29 -0500

    :ambulance: fix(order): fix order service (tests failed)

--- client/app/helper/order-helper.service.js
+++ client/app/helper/order-helper.service.js
@@ -16,38 +16,35 @@ class OrderHelperService {
 
         const formattedConfigs = [];
         _.forEach(configs, config => {
-            const formattedConfigs = _.assign({}, config);
-            if (formattedConfigs.configuration && !_.isArray(formattedConfigs.configuration)) {
-                formattedConfigs.configuration = this.transformToOrderValues(formattedConfigs.configuration);
+            const formattedConfig = _.assign({}, config);
+            if (formattedConfig.configuration && !_.isArray(formattedConfig.configuration)) {
+                formattedConfig.configuration = this.transformToOrderValues(formattedConfig.configuration);
             }
 
-            if (formattedConfigs.option) {
-                const formattedOptions = formattedConfigs.option.map(option => {
+            if (formattedConfig.option) {
+                formattedConfig.option = formattedConfig.option.map(option => {
                     if (option.configuration && !_.isArray(option.configuration)) {
                         option.configuration = this.transformToOrderValues(option.configuration);
                     }
                     return option;
                 });
-                formattedConfigs.option = formattedOptions;
             }
-            formattedConfigs.push(formattedConfigs);
+            formattedConfigs.push(formattedConfig);
         });
         // Transform configuration and option value if necessary
 
-        const paramsPart = this.$httpParamSerializerJQLike(_.assign({}, urlParams, {
+        return this.$httpParamSerializerJQLike(_.assign({}, urlParams, {
             products: JSURL.stringify(formattedConfigs)
         }));
-
-        return paramsPart;
     }
 
     /**
      * Transform an object to an Order compliant array
-     * @param  {[Object} config plain json
-     * @return {Array}          an array compatible with Order
+     * @param  {Object} config plain json
+     * @return {Array} an array compatible with Order
      */
     transformToOrderValues (config) {
-        let orderConfig = [];
+        const orderConfig = [];
         _.forEach(_.keys(config), key => {
             const configParam = {
                 label: key
