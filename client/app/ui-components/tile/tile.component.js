a16398d5050f94f6de7517dae0b5f79bbb920747
Alveric Alie <alveric.alie@corp.ovh.com>
2018-03-12 15:59:51 -0400

    feat(homepage): add homepage with guides
    Add a homepage with links to guides in FR, EN and US

--- client/app/ui-components/tile/tile.component.js
+++ client/app/ui-components/tile/tile.component.js
@@ -197,6 +197,7 @@ angular.module("managerApp")
                 <button data-ng-if="$ctrl.action.callback || ($ctrl.action.isAvailable && !$ctrl.action.isAvailable())"
                     class="oui-button oui-button_link oui-button_icon-right oui-button_full-width cui-tile__button"
                     data-ng-click="$ctrl.action.callback()"
+                    data-at-internet-click="{ name: $ctrl.action.atInternetClickTag }"
                     data-ng-disabled="$ctrl.action.isAvailable && !$ctrl.action.isAvailable()">
                     <span data-ng-bind="$ctrl.action.text"></span>
                     <i class="oui-icon oui-icon-chevron-right" aria-hidden="true"></i>
@@ -208,10 +209,10 @@ angular.module("managerApp")
                     <i class="oui-icon oui-icon-chevron-right" aria-hidden="true"></i>
                 </a>
                 <a data-ng-if="$ctrl.action.href && (!$ctrl.action.isAvailable || $ctrl.action.isAvailable())"
-                    class="oui-button oui-button_link oui-button_icon-right oui-button_full-width cui-tile__button"
+                    class="oui-button oui-button_link oui-button_full-width cui-tile__button"
                     data-ng-href="{{ $ctrl.action.href }}">
                     <span data-ng-bind="$ctrl.action.text"></span>
-                    <i class="oui-icon oui-icon-chevron-right" aria-hidden="true"></i>
+                    <span class="oui-icon oui-icon-external_link" aria-hidden="true"></span>
                 </a>
             </div>`,
         bindToController: {
