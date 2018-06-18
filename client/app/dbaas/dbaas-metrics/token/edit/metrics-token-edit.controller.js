073bdcb7c7e547d9a84e999d5a78e56503ee39e1
varun shivaprasad <varun257@gmail.com>
2018-06-18 12:09:08 +0530

    fix: fix for modals in dbaas metrics and deskaas
    replace cui-modal with oui-modal
    replace input with oui-field
    add loading and primary and secondary labels and actions
    
    oui modal fix

--- client/app/dbaas/dbaas-metrics/token/edit/metrics-token-edit.controller.js
+++ client/app/dbaas/dbaas-metrics/token/edit/metrics-token-edit.controller.js
@@ -10,24 +10,20 @@
             this.value = metricsValue;
         }
 
-
         confirm () {
-            this.loading = true;
             if (this.type === "name") {
-                this.MetricService.updateToken(this.serviceName, this.tokenID, this.value)
-                    .then(response => this.$uibModalInstance.close(response))
-                    .catch(err => this.$uibModalInstance.dismiss(err))
-                    .finally(() => {
-                        this.loading = false;
-                    });
+                this.deleteToken = this.ControllerHelper.request.getHashLoader({
+                    loaderFunction: () => this.MetricService.updateToken(this.serviceName, this.tokenID, this.value)
+                        .then(response => this.$uibModalInstance.close(response))
+                        .catch(err => this.$uibModalInstance.dismiss(err))
+                });
+                this.deleteToken.load();
             }
-
         }
 
         cancel () {
             this.$uibModalInstance.dismiss();
         }
-
     }
 
     angular.module("managerApp").controller("MetricsTokenEditCtrl", MetricsTokenEditCtrl);
