92b1d69a159f15dfb24fac695ebfe61afb9c6e5c
Frederic Espiau <frederic.espiau@corp.ovh.com>
2018-06-26 14:13:10 +0200

    chore: remove commented code

--- client/app/cloud/project/compute/infrastructure/virtualMachine/delete/cloud-project-compute-infrastructure-virtual-machine-delete.controller.js
+++ client/app/cloud/project/compute/infrastructure/virtualMachine/delete/cloud-project-compute-infrastructure-virtual-machine-delete.controller.js
@@ -26,25 +26,6 @@ angular.module("managerApp")
         function init () {
             self.isMonthlyBilling = vmToDelete.monthlyBilling && vmToDelete.monthlyBilling.status === "ok";
 
-            /*
-             * IP Failover are not automatically deleted so we comment the check below.
-             */
-            // check if the instance is routed to failover IPs
-            /*self.loaders.ips = true;
-            OvhApiCloudProjectIpFailover.v6().query({
-                serviceName : serviceName
-            }).$promise.then(function (ips) {
-                if (vmToDelete && vmToDelete.routedTo) {
-                    angular.forEach(vmToDelete.routedTo, function (route) {
-                        var ipfo = _.find(ips, { id : route });
-                        if (ipfo) {
-                            self.routedIpsFo.push(ipfo);
-                        }
-                    });
-                }
-            })["finally"](function () {
-                self.loaders.ips = false;
-            });*/
         }
 
 
