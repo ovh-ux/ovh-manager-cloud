f0f791b016668cf73fec4900d5ffaa8957f0c386
radireddy <adireddyravindra@gmail.com>
2018-07-24 13:20:35 +0530

    refactor: replaced cui-modal with oui-modal (#796)
    Replace cui-modal with oui-modal in below pages. Alos used ControllerHelper to make API calls.
    
    VEEAM storage update quota page
    VPS add backup storage
    VPS delete backup storage
    VPS edit backup storage
    VPS password backup storage
    VPS option termination
    VPS add secondary DNS
    VPS delete secondary DNS
    VPS display IPS
    VPS KVM
    VPS Monitoring SLA
    VPS password
    VPS reboot
    VPS re-install
    VPS reverse DNS
    VPS snapshot delete
    VPS snapshot restore
    VPS snapshot take
    VPS mount
    VPS restore
    
    ref: SCDC-823

--- client/app/vps/modal/backup-storage/edit-backup-storage.controller.js
+++ client/app/vps/modal/backup-storage/edit-backup-storage.controller.js
@@ -1,16 +1,12 @@
 class EditBackupStorageCtrl {
-    constructor ($translate, $uibModalInstance, row, CloudMessage, serviceName, VpsService) {
+    constructor ($translate, $uibModalInstance, ControllerHelper, row, CloudMessage, serviceName, VpsService) {
         this.$translate = $translate;
         this.$uibModalInstance = $uibModalInstance;
+        this.ControllerHelper = ControllerHelper;
         this.row = row;
         this.serviceName = serviceName;
         this.CloudMessage = CloudMessage;
         this.VpsService = VpsService;
-
-        this.loader = {
-            save: false
-        };
-
         this.model = {
             ftp: row.ftp,
             nfs: row.nfs,
@@ -27,14 +23,19 @@ class EditBackupStorageCtrl {
     }
 
     confirm () {
-        this.loader.save = true;
-        this.VpsService.putBackupStorageAccess(this.serviceName, this.row.ipBlock, this.model.ftp, this.model.nfs, this.model.cifs)
-            .then(() => this.CloudMessage.success(this.$translate.instant("vps_tab_backup_storage_set_success", {access: this.row.ipBlock})))
-            .catch(() => this.CloudMessage.error(this.$translate.instant("vps_tab_backup_storage_set_fail", {access: this.row.ipBlock})))
-            .finally(() => {
-                this.loader.save = false;
-                this.$uibModalInstance.close();
-            });
+        this.CloudMessage.flushChildMessage();
+        this.loader = this.ControllerHelper.request.getHashLoader({
+            loaderFunction: () => this.VpsService.putBackupStorageAccess(this.serviceName, this.row.ipBlock, this.model.ftp, this.model.nfs, this.model.cifs)
+                .then(() => this.CloudMessage.success(this.$translate.instant("vps_tab_backup_storage_set_success", { access: this.row.ipBlock })))
+                .catch(err => {
+                    if (_(err).has("data.message")) {
+                        this.CloudMessage.error(err.data.message);
+                    }
+                    this.CloudMessage.error(this.$translate.instant("vps_tab_backup_storage_set_fail", { access: this.row.ipBlock }));
+                })
+                .finally(() => this.$uibModalInstance.close())
+        });
+        return this.loader.load();
     }
 }
 
--- client/app/vps/modal/shortcut/display-ips/vps-display-ips.controller.js
+++ client/app/vps/modal/shortcut/display-ips/vps-display-ips.controller.js
@@ -1,24 +1,21 @@
 class VpsDisplayIpsCtrl {
-    constructor ($translate, $uibModalInstance, CloudMessage, serviceName, VpsService) {
+    constructor ($translate, $uibModalInstance, ControllerHelper, CloudMessage, serviceName, VpsService) {
         this.$translate = $translate;
         this.$uibModalInstance = $uibModalInstance;
         this.CloudMessage = CloudMessage;
         this.serviceName = serviceName;
         this.VpsService = VpsService;
-
-        this.loader = {
-            init: false
-        };
-
+        this.ControllerHelper = ControllerHelper;
         this.ips = [];
     }
 
     $onInit () {
-        this.loader.init = true;
-        this.VpsService.getIps(this.serviceName)
-            .then(data => { this.ips = data.results; })
-            .catch(() => this.CloudMessage.error(this.$translate.instant("vps_configuration_reversedns_fail")))
-            .finally(() => { this.loader.init = false; });
+        this.ipsLoader = this.ControllerHelper.request.getHashLoader({
+            loaderFunction: () => this.VpsService.getIps(this.serviceName)
+                .then(data => { this.ips = data.results; })
+                .catch(() => this.CloudMessage.error(this.$translate.instant("vps_configuration_reversedns_fail")))
+        });
+        return this.ipsLoader.load();
     }
 
     cancel () {
