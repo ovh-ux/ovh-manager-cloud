0f5235eec4e04d4d3da48bf63326c1ca948bbb8f
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-19 10:44:42 +0200

    fix(common): apply eslint rules

--- client/app/common/cloud-message.service.js
+++ client/app/common/cloud-message.service.js
@@ -1,131 +1,138 @@
 class CloudMessage {
-    constructor ($transitions, $state) {
-        this.$state = $state;
-        this.messages = {};
-        this.subscribers = {};
+  constructor($transitions, $state) {
+    this.$state = $state;
+    this.messages = {};
+    this.subscribers = {};
 
-        $transitions.onSuccess({}, () => {
-            this.flushChildMessage();
-        });
+    $transitions.onSuccess({}, () => {
+      this.flushChildMessage();
+    });
+  }
+
+  success(message, containerName) {
+    this.logMessage(_.isPlainObject(message) ? message : { text: message }, 'success', containerName);
+  }
+
+  error(message, containerName) {
+    this.logMessage(_.isPlainObject(message) ? message : { text: message }, 'error', containerName);
+  }
+
+  warning(message, containerName) {
+    this.logMessage(_.isPlainObject(message) ? message : { text: message }, 'warning', containerName);
+  }
+
+  info(message, containerName) {
+    this.logMessage(_.isPlainObject(message) ? message : { text: message }, 'info', containerName);
+  }
+
+  /*
+   * Handle the message type (error, warning, ...etc) and push the message to the messageHandler
+   */
+  logMessage(messageHash, type, containerName) {
+    if (!messageHash.text && !messageHash.textHtml) {
+      return;
     }
 
-    success (message, containerName) {
-        this.logMessage(_.isPlainObject(message) ? message : { text: message }, "success", containerName);
-    }
+    const messageHandler = this.getMessageHandler(containerName);
 
-    error (message, containerName) {
-        this.logMessage(_.isPlainObject(message) ? message : { text: message }, "error", containerName);
+    if (messageHandler) {
+      // Message age defines how many flush the message went through.
+      messageHandler.messages.push(_.extend({
+        type,
+        origin: containerName || this.$state.current.name,
+        timestamp: moment().valueOf(),
+      }, messageHash));
+      messageHandler.onMessage();
+    } else {
+      console.log(`Unhandled message ${messageHash.text}`);
     }
+  }
 
-    warning (message, containerName) {
-        this.logMessage(_.isPlainObject(message) ? message : { text: message }, "warning", containerName);
+  /*
+   * Handle message to dispatch.
+   * @params containerName : custom name for the container
+   * if no params is passed, it will take the current state name as containerName
+   */
+  /* eslint-disable no-param-reassign */
+  getMessageHandler(containerName) {
+    containerName = `${containerName || this.$state.current.name}.`;
+    let messageHandler = null;
+    do {
+      containerName = containerName.substring(0, _.lastIndexOf(containerName, '.'));
+      messageHandler = this.subscribers[containerName];
+    } while (!messageHandler && _.includes(containerName, '.'));
+    return messageHandler;
+  }
+  /* eslint-enable no-param-reassign */
+
+  /*
+   * Retrieve messages
+   */
+  getMessages(containerName) {
+    return this.subscribers[containerName].messages;
+  }
+
+  /*
+   * Flush currents messages
+   */
+  flushMessages(containerName) {
+    const messageHandler = this.getMessageHandler(containerName);
+
+    if (messageHandler) {
+      messageHandler.messages = [];
+      messageHandler.onMessage();
     }
+  }
 
-    info (message, containerName) {
-        this.logMessage(_.isPlainObject(message) ? message : { text: message }, "info", containerName);
-    }
+  flushChildMessage(containerName) {
+    const messageHandler = this.getMessageHandler(containerName);
 
-    /*
-     * Handle the message type (error, warning, ...etc) and push the message to the messageHandler
-     */
-    logMessage (messageHash, type, containerName) {
-        if (!messageHash.text && !messageHash.textHtml) {
-            return;
+    if (messageHandler) {
+      const now = moment().valueOf();
+      messageHandler.messages = _.filter(
+        messageHandler.messages,
+        message => message.origin === messageHandler.containerName
+          || (!message.forceFlush && now - 500 < message.timestamp),
+      );
+
+      _.forEach(messageHandler.messages, (message) => {
+        if (message.origin !== messageHandler.containerName) {
+          _.set(message, 'forceFlush', true);
         }
+      });
 
-        const messageHandler = this.getMessageHandler(containerName);
-
-        if (messageHandler) {
-            // Message age defines how many flush the message went through.
-            messageHandler.messages.push(_.extend({ type, origin: containerName || this.$state.current.name, timestamp: moment().valueOf() }, messageHash));
-            messageHandler.onMessage();
-        } else {
-            console.log(`Unhandled message ${messageHash.text}`);
-        }
+      messageHandler.onMessage();
     }
+  }
 
-    /*
-     * Handle message to dispatch.
-     * @params containerName : custom name for the container
-     * if no params is passed, it will take the current state name as containerName
-     */
-    getMessageHandler (containerName) {
-        containerName = `${containerName || this.$state.current.name}.`;
-        let messageHandler = null;
-        do {
-            containerName = containerName.substring(0, _.lastIndexOf(containerName, "."));
-            messageHandler = this.subscribers[containerName];
-        } while (!messageHandler && _.includes(containerName, "."));
-        return messageHandler;
+  /*
+   * unsubscribe to the message receiver.
+   */
+  unSubscribe(containerName) {
+    const subscriber = this.subscribers[containerName];
+    if (subscriber) {
+      this.subscribers[containerName].messages = [];
+      this.subscribers[containerName].onMessage();
     }
+    this.subscribers = _.omit(this.subscribers, containerName);
+  }
 
-    /*
-     * Retrieve messages
-     */
-    getMessages (containerName) {
-        return this.subscribers[containerName].messages;
-    }
-
-    /*
-     * Flush currents messages
-     */
-    flushMessages (containerName) {
-        const messageHandler = this.getMessageHandler(containerName);
-
-        if (messageHandler) {
-            messageHandler.messages = [];
-            messageHandler.onMessage();
-        }
-    }
-
-    flushChildMessage (containerName) {
-        const messageHandler = this.getMessageHandler(containerName);
-
-        if (messageHandler) {
-            const now = moment().valueOf();
-            messageHandler.messages = _.filter(messageHandler.messages, message => {
-                return message.origin === messageHandler.containerName ||
-                    (!message.forceFlush && now - 500 < message.timestamp);
-            });
-
-            _.forEach(messageHandler.messages, message => {
-                if (message.origin !== messageHandler.containerName) {
-                    message.forceFlush = true;
-                }
-            });
-
-            messageHandler.onMessage();
-        }
-    }
-
-    /*
-     * unsubscribe to the message receiver.
-     */
-    unSubscribe (containerName) {
-        const subscriber = this.subscribers[containerName];
-        if (subscriber) {
-            this.subscribers[containerName].messages = [];
-            this.subscribers[containerName].onMessage();
-        }
-        this.subscribers = _.omit(this.subscribers, containerName);
-    }
-
-    /*
-     * subscibe to the message receiver.
-     * @params containerName : container name or the state name to subscribe
-     * @params params actions to do
-     * ex params : { onMessage: () => this.getMessage() }
-     */
-    subscribe (containerName, params) {
-        this.subscribers[containerName] = _.extend({
-            containerName,
-            messages: [],
-            onMessage: _.noop()
-        }, params);
-        return {
-            getMessages: () => this.getMessages(containerName)
-        };
-    }
+  /*
+   * subscibe to the message receiver.
+   * @params containerName : container name or the state name to subscribe
+   * @params params actions to do
+   * ex params : { onMessage: () => this.getMessage() }
+   */
+  subscribe(containerName, params) {
+    this.subscribers[containerName] = _.extend({
+      containerName,
+      messages: [],
+      onMessage: _.noop(),
+    }, params);
+    return {
+      getMessages: () => this.getMessages(containerName),
+    };
+  }
 }
 
-angular.module("managerApp").service("CloudMessage", CloudMessage);
+angular.module('managerApp').service('CloudMessage', CloudMessage);
--- client/app/common/cloud-navigation.service.js
+++ client/app/common/cloud-navigation.service.js
@@ -1,57 +1,68 @@
 class CloudNavigation {
-    constructor ($transitions, $state, $stateParams, TabsService) {
-        this.$state = $state;
-        this.$stateParams = $stateParams;
-        this.TabsService = TabsService;
+  constructor($transitions, $state, $stateParams, TabsService) {
+    this.$state = $state;
+    this.$stateParams = $stateParams;
+    this.TabsService = TabsService;
 
-        this.rootElement = undefined;
+    this.rootElement = undefined;
 
-        $transitions.onSuccess({}, transition => {
-            const toState = transition.to();
-            const toParams = transition.params();
-            const fromState = transition.from();
-            const fromParams = transition.params("from");
-            const correspondingState = _.find(this.history, elem => elem.state === toState.name && _.isEqual(elem.stateParams, toParams));
+    $transitions.onSuccess({}, (transition) => {
+      const toState = transition.to();
+      const toParams = transition.params();
+      const fromState = transition.from();
+      const fromParams = transition.params('from');
+      const correspondingState = _.find(
+        this.history,
+        elem => elem.state === toState.name && _.isEqual(elem.stateParams, toParams),
+      );
 
-            if (correspondingState) {
-                while (_.last(this.history) !== correspondingState) { this.history.pop(); }
-                this.history.pop();
-            } else {
-                const element = { state: fromState.name, stateParams: fromParams, sref: this.getSref({ state: fromState.name }, fromParams) };
-                this.history.push(element);
-            }
-        });
+      if (correspondingState) {
+        while (_.last(this.history) !== correspondingState) { this.history.pop(); }
+        this.history.pop();
+      } else {
+        const element = {
+          state: fromState.name,
+          stateParams: fromParams,
+          sref: this.constructor.getSref({ state: fromState.name }, fromParams),
+        };
+        this.history.push(element);
+      }
+    });
+  }
+
+  $onInit() {
+    this.init();
+  }
+
+  init(rootElement) {
+    this.history = [];
+    this.rootElement = undefined;
+    if (rootElement) {
+      this.rootElement = rootElement;
     }
+  }
 
-    $onInit () {
-        this.init();
-    }
+  getPreviousState() {
+    const previousState = _.last(this.history) || this.getActiveTabState() || this.rootElement;
+    previousState.go = () => this.$state.go(previousState.state, previousState.stateParams);
+    return previousState;
+  }
 
-    init (rootElement) {
-        this.history = [];
-        this.rootElement = undefined;
-        if (rootElement) {
-            this.rootElement = rootElement;
-        }
-    }
+  getActiveTabState() {
+    return this.TabsService.getActiveTab();
+  }
 
-    getPreviousState () {
-        const previousState = _.last(this.history) || this.getActiveTabState() || this.rootElement;
-        previousState.go = () => this.$state.go(previousState.state, previousState.stateParams);
-        return previousState;
-    }
+  findInHistory(stateToFind) {
+    return _.find(
+      this.history,
+      state => state.state === stateToFind.state
+        && _.isEqual(state.stateParams, stateToFind.stateParams),
+    );
+  }
 
-    getActiveTabState () {
-        return this.TabsService.getActiveTab();
-    }
-
-    findInHistory (stateToFind) {
-        return _.find(this.history, state => state.state === stateToFind.state && _.isEqual(state.stateParams, stateToFind.stateParams));
-    }
-
-    getSref (element, params) {
-        return `${element.state}(${JSON.stringify(params)})`;
-    }
+  static getSref(element, params) {
+    return `${element.state}(${JSON.stringify(params)})`;
+  }
 }
 
-angular.module("managerApp").service("CloudNavigation", CloudNavigation);
+angular.module('managerApp').service('CloudNavigation', CloudNavigation);
