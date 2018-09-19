292d6181d54517a17be57e93f1ae2208f4f6cc55
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-19 16:30:49 +0200

    fix(home): apply eslint rules

--- client/app/home/announcement/announcement-summit.config.js
+++ client/app/home/announcement/announcement-summit.config.js
@@ -1,13 +1,13 @@
-angular.module("managerApp")
-    .config(atInternetControllerDecoratorsProvider => {
-        atInternetControllerDecoratorsProvider.decorate({
-            AnnouncementSummitCtrl: {
-                confirm (atInternet) {
-                    atInternet.trackClick({
-                        name: "Summit-2017-announcement-register",
-                        type: "action"
-                    });
-                }
-            }
-        });
+angular.module('managerApp')
+  .config((atInternetControllerDecoratorsProvider) => {
+    atInternetControllerDecoratorsProvider.decorate({
+      AnnouncementSummitCtrl: {
+        confirm(atInternet) {
+          atInternet.trackClick({
+            name: 'Summit-2017-announcement-register',
+            type: 'action',
+          });
+        },
+      },
     });
+  });
--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,48 +1,49 @@
 class HomeCtrl {
-    constructor ($q, DocsService, FeatureAvailabilityService, OvhApiCloudProject, OvhApiCloudProjectRegion) {
-        this.$q = $q;
-        this.DocsService = DocsService;
-        this.FeatureAvailabilityService = FeatureAvailabilityService;
-        this.OvhApiCloudProject = OvhApiCloudProject;
-        this.OvhApiCloudProjectRegion = OvhApiCloudProjectRegion;
-    }
+  constructor($q, DocsService, FeatureAvailabilityService, OvhApiCloudProject,
+    OvhApiCloudProjectRegion) {
+    this.$q = $q;
+    this.DocsService = DocsService;
+    this.FeatureAvailabilityService = FeatureAvailabilityService;
+    this.OvhApiCloudProject = OvhApiCloudProject;
+    this.OvhApiCloudProjectRegion = OvhApiCloudProjectRegion;
+  }
 
-    $onInit () {
-        this.defaultSections = ["PROJECT", "VPS"];
-        this.guides = {};
-        this.guides.all = this.DocsService.getAllGuidesLink();
-        this.hasAScheduledMaintenanceOperation = null;
+  $onInit() {
+    this.defaultSections = ['PROJECT', 'VPS'];
+    this.guides = {};
+    this.guides.all = this.DocsService.getAllGuidesLink();
+    this.hasAScheduledMaintenanceOperation = null;
 
-        return this.setSections()
-            .then(() => this.getAllRegions());
-    }
+    return this.setSections()
+      .then(() => this.getAllRegions());
+  }
 
-    setSections () {
-        const sectionsPromise = _.map(this.defaultSections, section => this.FeatureAvailabilityService.hasFeaturePromise(section, "guides"));
+  setSections() {
+    const sectionsPromise = _.map(this.defaultSections, section => this.FeatureAvailabilityService.hasFeaturePromise(section, 'guides'));
 
-        return this.$q.all(sectionsPromise)
-            .then(sections => {
-                this.guides.sections = _.chain(this.defaultSections)
-                    .filter((value, index) => sections[index])
-                    .map(section => this.DocsService.getGuidesOfSection(section))
-                    .value();
-                return this.guides.sections;
-            });
-    }
+    return this.$q.all(sectionsPromise)
+      .then((sections) => {
+        this.guides.sections = _.chain(this.defaultSections)
+          .filter((value, index) => sections[index])
+          .map(section => this.DocsService.getGuidesOfSection(section))
+          .value();
+        return this.guides.sections;
+      });
+  }
 
-    getAllRegions () {
-        return this.OvhApiCloudProject.v6()
-            .query().$promise
-            .then(cloudProjects => this.$q
-                .all(_.map(cloudProjects, cloudProject => this.OvhApiCloudProjectRegion.v6()
-                    .query({ serviceName: cloudProject }).$promise
-                    .catch(() => [])))
-                .then(allRegions => _(allRegions).flatten().uniq().value()))
-            .then(regions => {
-                this.hasAScheduledMaintenanceOperation = _.indexOf(regions, "BHS1") > -1 && moment().isBefore("2018-09-20");
-                return regions;
-            });
-    }
+  getAllRegions() {
+    return this.OvhApiCloudProject.v6()
+      .query().$promise
+      .then(cloudProjects => this.$q
+        .all(_.map(cloudProjects, cloudProject => this.OvhApiCloudProjectRegion.v6()
+          .query({ serviceName: cloudProject }).$promise
+          .catch(() => [])))
+        .then(allRegions => _(allRegions).flatten().uniq().value()))
+      .then((regions) => {
+        this.hasAScheduledMaintenanceOperation = _.indexOf(regions, 'BHS1') > -1 && moment().isBefore('2018-09-20');
+        return regions;
+      });
+  }
 }
 
-angular.module("managerApp").controller("HomeCtrl", HomeCtrl);
+angular.module('managerApp').controller('HomeCtrl', HomeCtrl);
