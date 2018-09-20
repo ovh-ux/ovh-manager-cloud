5905356d7184b6ed12213ceeafb96c2a01d80c5f
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-20 15:37:54 +0200

    fix(vps.additional-disk): apply eslint rules

--- client/app/vps/additional-disk/order/additional-disk-option.filter.js
+++ client/app/vps/additional-disk/order/additional-disk-option.filter.js
@@ -1,7 +1,5 @@
-angular.module("managerApp").filter("additionalDiskOptions", function () {
-    "use strict";
-    return function (additionalDiskOption) {
-        additionalDiskOption = additionalDiskOption.replace(/[a-zA-Z]*/, "");
-        return additionalDiskOption;
-    };
+angular.module('managerApp').filter('additionalDiskOptions', () => function (additionalDiskOptionParam) {
+  let additionalDiskOption = additionalDiskOptionParam;
+  additionalDiskOption = additionalDiskOption.replace(/[a-zA-Z]*/, '');
+  return additionalDiskOption;
 });
