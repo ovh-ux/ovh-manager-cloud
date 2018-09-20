4dfd4c126b9615bc2e040f0723c186ed71c71fe8
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-20 15:15:35 +0200

    fix(ui-components): apply eslint rules

--- client/app/ui-components/modal/name-change-modal/name-change-modal.controller.js
+++ client/app/ui-components/modal/name-change-modal/name-change-modal.controller.js
@@ -1,46 +1,46 @@
 class NameChangeModalCtrl {
-    constructor ($uibModalInstance, params, ControllerHelper) {
-        this.$uibModalInstance = $uibModalInstance;
+  constructor($uibModalInstance, params, ControllerHelper) {
+    this.$uibModalInstance = $uibModalInstance;
 
-        this.params = params;
-        this.ControllerHelper = ControllerHelper;
+    this.params = params;
+    this.ControllerHelper = ControllerHelper;
+  }
+
+  $onInit() {
+    this.serviceName = this.params.serviceName;
+
+    this.model = {
+      displayName: {
+        value: null,
+        maxlength: this.params.maxlength || Infinity,
+      },
+    };
+
+    this.model.displayName.value = this.params.displayName;
+  }
+
+  confirm() {
+    this.saving = true;
+
+    if (this.params.onSave) {
+      return this.params.onSave(this.model.displayName.value, this.serviceName)
+        .then(response => this.$uibModalInstance.close(response))
+        .catch(response => this.$uibModalInstance.dismiss(response))
+        .finally(() => {
+          this.saving = false;
+        });
     }
 
-    $onInit () {
-        this.serviceName = this.params.serviceName;
+    return this.$uibModalInstance.close(this.model.displayName.value);
+  }
 
-        this.model = {
-            displayName: {
-                value: null,
-                maxlength: this.params.maxlength || Infinity
-            }
-        };
+  cancel() {
+    this.$uibModalInstance.dismiss();
+  }
 
-        this.model.displayName.value = this.params.displayName;
-    }
-
-    confirm () {
-        this.saving = true;
-
-        if (this.params.onSave) {
-            return this.params.onSave(this.model.displayName.value, this.serviceName)
-                .then(response => this.$uibModalInstance.close(response))
-                .catch(response => this.$uibModalInstance.dismiss(response))
-                .finally(() => {
-                    this.saving = false;
-                });
-        }
-
-        return this.$uibModalInstance.close(this.model.displayName.value);
-    }
-
-    cancel () {
-        this.$uibModalInstance.dismiss();
-    }
-
-    isModalLoading () {
-        return this.saving;
-    }
+  isModalLoading() {
+    return this.saving;
+  }
 }
 
-angular.module("managerApp").controller("NameChangeModalCtrl", NameChangeModalCtrl);
+angular.module('managerApp').controller('NameChangeModalCtrl', NameChangeModalCtrl);
