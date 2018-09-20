4e40176bc67cc8bdec686a27fb457b97d2819b2d
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-20 11:33:05 +0200

    fix(iplb.modal): apply eslint rules

--- client/app/iplb/modal/cipher/iplb-cipher-change.controller.js
+++ client/app/iplb/modal/cipher/iplb-cipher-change.controller.js
@@ -1,50 +1,51 @@
 class IpLoadBalancerCipherChangeCtrl {
-    constructor ($uibModalInstance, serviceName, ControllerHelper, IpLoadBalancerCipherService) {
-        this.$uibModalInstance = $uibModalInstance;
+  constructor($uibModalInstance, serviceName, ControllerHelper, IpLoadBalancerCipherService) {
+    this.$uibModalInstance = $uibModalInstance;
 
-        this.serviceName = serviceName;
-        this.ControllerHelper = ControllerHelper;
+    this.serviceName = serviceName;
+    this.ControllerHelper = ControllerHelper;
 
-        this.IpLoadBalancerCipherService = IpLoadBalancerCipherService;
+    this.IpLoadBalancerCipherService = IpLoadBalancerCipherService;
 
-        this.cipher = this.ControllerHelper.request.getHashLoader({
-            loaderFunction: () => this.IpLoadBalancerCipherService.getCipher(this.serviceName),
-            successHandler: () => { this.model.cipherType.value = this.cipher.data.type; }
-        });
+    this.cipher = this.ControllerHelper.request.getHashLoader({
+      loaderFunction: () => this.IpLoadBalancerCipherService.getCipher(this.serviceName),
+      successHandler: () => { this.model.cipherType.value = this.cipher.data.type; },
+    });
 
-        this.cipherTypes = this.ControllerHelper.request.getArrayLoader({
-            loaderFunction: () => this.IpLoadBalancerCipherService.getCipherTypes(this.serviceName)
-        });
+    this.cipherTypes = this.ControllerHelper.request.getArrayLoader({
+      loaderFunction: () => this.IpLoadBalancerCipherService.getCipherTypes(this.serviceName),
+    });
 
-        this.model = {
-            cipherType: {
-                value: ""
-            }
-        };
-    }
+    this.model = {
+      cipherType: {
+        value: '',
+      },
+    };
+  }
 
-    $onInit () {
-        this.cipher.load();
-        this.cipherTypes.load();
-    }
+  $onInit() {
+    this.cipher.load();
+    this.cipherTypes.load();
+  }
 
-    confirm () {
-        this.saving = true;
-        return this.IpLoadBalancerCipherService.updateCipher(this.serviceName, this.model.cipherType.value)
-            .then(response => this.$uibModalInstance.close(response))
-            .catch(response => this.$uibModalInstance.dismiss(response))
-            .finally(() => {
-                this.saving = false;
-            });
-    }
+  confirm() {
+    this.saving = true;
+    return this.IpLoadBalancerCipherService
+      .updateCipher(this.serviceName, this.model.cipherType.value)
+      .then(response => this.$uibModalInstance.close(response))
+      .catch(response => this.$uibModalInstance.dismiss(response))
+      .finally(() => {
+        this.saving = false;
+      });
+  }
 
-    cancel () {
-        this.$uibModalInstance.dismiss();
-    }
+  cancel() {
+    this.$uibModalInstance.dismiss();
+  }
 
-    isModalLoading () {
-        return this.cipher.loading || this.cipherTypes.loading || this.saving;
-    }
+  isModalLoading() {
+    return this.cipher.loading || this.cipherTypes.loading || this.saving;
+  }
 }
 
-angular.module("managerApp").controller("IpLoadBalancerCipherChangeCtrl", IpLoadBalancerCipherChangeCtrl);
+angular.module('managerApp').controller('IpLoadBalancerCipherChangeCtrl', IpLoadBalancerCipherChangeCtrl);
