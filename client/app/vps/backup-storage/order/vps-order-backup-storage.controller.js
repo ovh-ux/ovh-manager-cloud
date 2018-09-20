5fe3af6816c39a5d9103f94d98c31dae02bd5717
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-20 15:40:58 +0200

    fix(vps.backup-storage): apply eslint rules

--- client/app/vps/backup-storage/order/vps-order-backup-storage.controller.js
+++ client/app/vps/backup-storage/order/vps-order-backup-storage.controller.js
@@ -1,46 +1,46 @@
 class VpsOrderBackupStorageCtrl {
-    constructor ($stateParams, $translate, $window, CloudMessage, CloudNavigation, ServiceHelper, VpsService) {
-        "use strict";
-        this.$translate = $translate;
-        this.$window = $window;
-        this.CloudMessage = CloudMessage;
-        this.CloudNavigation = CloudNavigation;
-        this.serviceName = $stateParams.serviceName;
-        this.ServiceHelper = ServiceHelper;
-        this.VpsService = VpsService;
+  constructor($stateParams, $translate, $window, CloudMessage, CloudNavigation, ServiceHelper,
+    VpsService) {
+    this.$translate = $translate;
+    this.$window = $window;
+    this.CloudMessage = CloudMessage;
+    this.CloudNavigation = CloudNavigation;
+    this.serviceName = $stateParams.serviceName;
+    this.ServiceHelper = ServiceHelper;
+    this.VpsService = VpsService;
 
 
-        this.model = {
-            optionDetails: undefined,
-            url: undefined
-        };
-    }
+    this.model = {
+      optionDetails: undefined,
+      url: undefined,
+    };
+  }
 
-    $onInit () {
-        this.VpsService.getOptionDetails(this.serviceName, "ftpbackup")
-            .then(option => {
-                this.model.optionDetails = option.results[0];
-            })
-            .catch(error => {
-                this.CloudMessage.error(this.$translate.instant("vps_dashboard_loading_error") + " " + error.data);
-            });
+  $onInit() {
+    this.VpsService.getOptionDetails(this.serviceName, 'ftpbackup')
+      .then((option) => {
+        this.model.optionDetails = _.first(option.results);
+      })
+      .catch((error) => {
+        this.CloudMessage.error(`${this.$translate.instant('vps_dashboard_loading_error')} ${error.data}`);
+      });
 
-        this.previousState = this.CloudNavigation.getPreviousState();
-        this.VpsService.canOrderOption(this.serviceName, "ftpbackup").catch(() => {
-            this.CloudMessage.error(this.$translate.instant("vps_tab_BACKUP_STORAGE_dashboard_ftpbackup_unavailable"));
-        });
-    }
+    this.previousState = this.CloudNavigation.getPreviousState();
+    this.VpsService.canOrderOption(this.serviceName, 'ftpbackup').catch(() => {
+      this.CloudMessage.error(this.$translate.instant('vps_tab_BACKUP_STORAGE_dashboard_ftpbackup_unavailable'));
+    });
+  }
 
-    orderOption () {
-        this.ServiceHelper.loadOnNewPage(this.VpsService.orderOption(this.serviceName, "ftpbackup", this.model.optionDetails.duration.duration))
-            .then(({ url }) => {
-                this.model.url = url;
-            });
-    }
+  orderOption() {
+    this.ServiceHelper.loadOnNewPage(this.VpsService.orderOption(this.serviceName, 'ftpbackup', this.model.optionDetails.duration.duration))
+      .then(({ url }) => {
+        this.model.url = url;
+      });
+  }
 
-    cancel () {
-        this.previousState.go();
-    }
+  cancel() {
+    this.previousState.go();
+  }
 }
 
-angular.module("managerApp").controller("VpsOrderBackupStorageCtrl", VpsOrderBackupStorageCtrl);
+angular.module('managerApp').controller('VpsOrderBackupStorageCtrl', VpsOrderBackupStorageCtrl);
