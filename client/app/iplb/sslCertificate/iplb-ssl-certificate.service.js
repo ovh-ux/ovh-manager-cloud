952ae4bdad8f83887c3e644dd1b120d30fccb972
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-20 11:56:13 +0200

    fix(iplb.ssl.certificate): apply eslint rules

--- client/app/iplb/sslCertificate/iplb-ssl-certificate.service.js
+++ client/app/iplb/sslCertificate/iplb-ssl-certificate.service.js
@@ -1,68 +1,68 @@
 class IpLoadBalancerSslCertificateService {
-    constructor ($q, OvhApiIpLoadBalancing, OvhApiMe, OvhApiOrder, ServiceHelper) {
-        this.$q = $q;
-        this.OvhApiIpLoadBalancing = OvhApiIpLoadBalancing;
-        this.Ssl = OvhApiIpLoadBalancing.Ssl().v6();
-        this.User = OvhApiMe;
-        this.OvhApiOrder = OvhApiOrder;
-        this.ServiceHelper = ServiceHelper;
-    }
+  constructor($q, OvhApiIpLoadBalancing, OvhApiMe, OvhApiOrder, ServiceHelper) {
+    this.$q = $q;
+    this.OvhApiIpLoadBalancing = OvhApiIpLoadBalancing;
+    this.Ssl = OvhApiIpLoadBalancing.Ssl().v6();
+    this.User = OvhApiMe;
+    this.OvhApiOrder = OvhApiOrder;
+    this.ServiceHelper = ServiceHelper;
+  }
 
-    getCertificates (serviceName) {
-        return this.Ssl.query({ serviceName })
-            .$promise
-            .then(sslIds => this.$q.all(sslIds.map(sslId => this.getCertificate(serviceName, sslId))))
-            .catch(this.ServiceHelper.errorHandler("iplb_ssl_list_error"));
-    }
+  getCertificates(serviceName) {
+    return this.Ssl.query({ serviceName })
+      .$promise
+      .then(sslIds => this.$q.all(sslIds.map(sslId => this.getCertificate(serviceName, sslId))))
+      .catch(this.ServiceHelper.errorHandler('iplb_ssl_list_error'));
+  }
 
-    getCertificate (serviceName, sslId) {
-        return this.Ssl.get({ serviceName, sslId })
-            .$promise;
-    }
+  getCertificate(serviceName, sslId) {
+    return this.Ssl.get({ serviceName, sslId })
+      .$promise;
+  }
 
-    create (serviceName, ssl) {
-        return this.Ssl.post({ serviceName }, ssl)
-            .$promise
-            .then(this.ServiceHelper.successHandler("iplb_ssl_add_success"))
-            .catch(this.ServiceHelper.errorHandler("iplb_ssl_add_error"));
-    }
+  create(serviceName, ssl) {
+    return this.Ssl.post({ serviceName }, ssl)
+      .$promise
+      .then(this.ServiceHelper.successHandler('iplb_ssl_add_success'))
+      .catch(this.ServiceHelper.errorHandler('iplb_ssl_add_error'));
+  }
 
-    update (serviceName, sslId, ssl) {
-        return this.Ssl.put({
-            serviceName,
-            sslId
-        }, ssl)
-            .$promise
-            .then(this.ServiceHelper.successHandler("iplb_ssl_update_success"))
-            .catch(this.ServiceHelper.errorHandler("iplb_ssl_update_error"));
-    }
+  update(serviceName, sslId, ssl) {
+    return this.Ssl.put({
+      serviceName,
+      sslId,
+    }, ssl)
+      .$promise
+      .then(this.ServiceHelper.successHandler('iplb_ssl_update_success'))
+      .catch(this.ServiceHelper.errorHandler('iplb_ssl_update_error'));
+  }
 
-    delete (serviceName, sslId) {
-        return this.Ssl.delete({
-            serviceName,
-            sslId
-        })
-            .$promise
-            .then(this.ServiceHelper.successHandler("iplb_ssl_delete_success"))
-            .catch(this.ServiceHelper.errorHandler("iplb_ssl_delete_error"));
-    }
+  delete(serviceName, sslId) {
+    return this.Ssl.delete({
+      serviceName,
+      sslId,
+    })
+      .$promise
+      .then(this.ServiceHelper.successHandler('iplb_ssl_delete_success'))
+      .catch(this.ServiceHelper.errorHandler('iplb_ssl_delete_error'));
+  }
 
-    getCertificateProducts (serviceName) {
-        return this.OvhApiOrder.Cart().ServiceOption().v6().get({
-            productName: "ipLoadbalancing",
-            serviceName
-        })
-            .$promise
-            .then(options => options.filter(option => option.family === "ssl"))
-            .then(options => options.map(option => {
-                // Keep only 1 year prices
-                option.prices = option.prices.filter(price => price.interval === 12);
-                return option;
-            }))
-            .catch(this.ServiceHelper.errorHandler("iplb_ssl_order_loading_error"));
-    }
+  getCertificateProducts(serviceName) {
+    return this.OvhApiOrder.Cart().ServiceOption().v6().get({
+      productName: 'ipLoadbalancing',
+      serviceName,
+    })
+      .$promise
+      .then(options => options.filter(option => option.family === 'ssl'))
+      .then(options => options.map((option) => {
+        // Keep only 1 year prices
+        _.set(option, 'prices', option.prices.filter(price => price.interval === 12));
+        return option;
+      }))
+      .catch(this.ServiceHelper.errorHandler('iplb_ssl_order_loading_error'));
+  }
 
-    /**
+  /**
      * Order a paid certificate
      * @param  String serviceName       the service name
      * @param  Object options           order parameters
@@ -76,53 +76,54 @@ class IpLoadBalancerSslCertificateService {
      *  - pricingMode
      *  - quantity
      */
-    orderPaidCertificate (serviceName, orderOptions, configuration) {
-        let cartId;
-        return this.User.v6().get().$promise
-            .then(me => this.OvhApiOrder.Cart().v6().post({}, { ovhSubsidiary: me.ovhSubsidiary }).$promise)
-            .then(cart => {
-                cartId = cart.cartId;
-                return this.OvhApiOrder.Cart().v6().assign({ cartId }).$promise;
-            })
-            .then(() => this.OvhApiOrder.Cart().ServiceOption().v6().post({
-                productName: "ipLoadbalancing",
-                serviceName
-            }, Object.assign({}, orderOptions, {
-                cartId
-            })).$promise)
-            .then(item => {
-                // Apply item configuration
-                const promises = Object.keys(configuration)
-                    .map(label => this.configureCartItem(cartId, item.itemId, label, configuration[label]));
-                return this.$q.all(promises);
-            })
-            .then(() => this.OvhApiOrder.Cart().v6().checkout({ cartId }, {}).$promise)
-            .catch(err => {
-                if (cartId) {
-                    this.OvhApiOrder.Cart().v6().delete({ cartId });
-                }
+  orderPaidCertificate(serviceName, orderOptions, configuration) {
+    let cartId;
+    return this.User.v6().get().$promise
+      .then(me => this.OvhApiOrder.Cart().v6()
+        .post({}, { ovhSubsidiary: me.ovhSubsidiary }).$promise)
+      .then((cart) => {
+        cartId = _.get(cart, 'cartId');
+        return this.OvhApiOrder.Cart().v6().assign({ cartId }).$promise;
+      })
+      .then(() => this.OvhApiOrder.Cart().ServiceOption().v6().post({
+        productName: 'ipLoadbalancing',
+        serviceName,
+      }, Object.assign({}, orderOptions, {
+        cartId,
+      })).$promise)
+      .then((item) => {
+        // Apply item configuration
+        const promises = Object.keys(configuration)
+          .map(label => this.configureCartItem(cartId, item.itemId, label, configuration[label]));
+        return this.$q.all(promises);
+      })
+      .then(() => this.OvhApiOrder.Cart().v6().checkout({ cartId }, {}).$promise)
+      .catch((err) => {
+        if (cartId) {
+          this.OvhApiOrder.Cart().v6().delete({ cartId });
+        }
 
-                this.ServiceHelper.errorHandler("iplb_ssl_order_error")(err);
-            });
-    }
+        this.ServiceHelper.errorHandler('iplb_ssl_order_error')(err);
+      });
+  }
 
-    configureCartItem (cartId, itemId, label, value) {
-        return this.OvhApiOrder.Cart().Item().Configuration().v6()
-            .post({
-                cartId,
-                itemId
-            }, {
-                label,
-                value
-            }).$promise;
-    }
+  configureCartItem(cartId, itemId, label, value) {
+    return this.OvhApiOrder.Cart().Item().Configuration().v6()
+      .post({
+        cartId,
+        itemId,
+      }, {
+        label,
+        value,
+      }).$promise;
+  }
 
-    orderFreeCertificate (serviceName, fqdn) {
-        return this.OvhApiIpLoadBalancing.v6().freeCertificate({ serviceName }, { fqdn }).$promise
-            .then(this.ServiceHelper.successHandler("iplb_ssl_order_success"))
-            .then(() => this.Ssl.resetQueryCache())
-            .catch(this.ServiceHelper.errorHandler("iplb_ssl_order_error"));
-    }
+  orderFreeCertificate(serviceName, fqdn) {
+    return this.OvhApiIpLoadBalancing.v6().freeCertificate({ serviceName }, { fqdn }).$promise
+      .then(this.ServiceHelper.successHandler('iplb_ssl_order_success'))
+      .then(() => this.Ssl.resetQueryCache())
+      .catch(this.ServiceHelper.errorHandler('iplb_ssl_order_error'));
+  }
 }
 
-angular.module("managerApp").service("IpLoadBalancerSslCertificateService", IpLoadBalancerSslCertificateService);
+angular.module('managerApp').service('IpLoadBalancerSslCertificateService', IpLoadBalancerSslCertificateService);
--- client/app/iplb/sslCertificate/update/iplb-ssl-certificate-update.controller.js
+++ client/app/iplb/sslCertificate/update/iplb-ssl-certificate-update.controller.js
@@ -1,24 +1,26 @@
 class IpLoadBalancerSslCertificateUpdateCtrl {
-    constructor ($uibModalInstance, ControllerHelper, IpLoadBalancerSslCertificateService, serviceName, ssl) {
-        this.$uibModalInstance = $uibModalInstance;
-        this.ControllerHelper = ControllerHelper;
-        this.IpLoadBalancerSslCertificateService = IpLoadBalancerSslCertificateService;
-        this.serviceName = serviceName;
-        this.ssl = ssl;
-    }
+  constructor($uibModalInstance, ControllerHelper, IpLoadBalancerSslCertificateService,
+    serviceName, ssl) {
+    this.$uibModalInstance = $uibModalInstance;
+    this.ControllerHelper = ControllerHelper;
+    this.IpLoadBalancerSslCertificateService = IpLoadBalancerSslCertificateService;
+    this.serviceName = serviceName;
+    this.ssl = ssl;
+  }
 
-    dismiss () {
-        this.$uibModalInstance.dismiss();
-    }
+  dismiss() {
+    this.$uibModalInstance.dismiss();
+  }
 
-    update () {
-        this.updateSsl = this.ControllerHelper.request.getHashLoader({
-            loaderFunction: () => this.IpLoadBalancerSslCertificateService.update(this.serviceName, this.ssl.id, { displayName: this.displayName })
-                .then(response => this.$uibModalInstance.close(response))
-                .catch(error => this.$uibModalInstance.dismiss(error))
-        });
-        return this.updateSsl.load();
-    }
+  update() {
+    this.updateSsl = this.ControllerHelper.request.getHashLoader({
+      loaderFunction: () => this.IpLoadBalancerSslCertificateService
+        .update(this.serviceName, this.ssl.id, { displayName: this.displayName })
+        .then(response => this.$uibModalInstance.close(response))
+        .catch(error => this.$uibModalInstance.dismiss(error)),
+    });
+    return this.updateSsl.load();
+  }
 }
 
-angular.module("managerApp").controller("IpLoadBalancerSslCertificateUpdateCtrl", IpLoadBalancerSslCertificateUpdateCtrl);
+angular.module('managerApp').controller('IpLoadBalancerSslCertificateUpdateCtrl', IpLoadBalancerSslCertificateUpdateCtrl);
