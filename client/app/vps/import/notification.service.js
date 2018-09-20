978f59ade15b209f33be66e40caa1f9b3a368b7b
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-20 16:05:08 +0200

    fix(vps.import): apply eslint rules

--- client/app/vps/import/notification.service.js
+++ client/app/vps/import/notification.service.js
@@ -1,31 +1,28 @@
-"use strict";
+angular.module('managerApp').service('VpsNotificationIpv6', ['$q', 'ovhUserPref', function VpsNotificationIpv6($q, ovhUserPref) {
+  const self = this;
 
-angular.module("managerApp").service("VpsNotificationIpv6", ["$q", "ovhUserPref", function ($q, ovhUserPref) {
-    "use strict";
+  function createNotificationUserPref(userPrefName, subject) {
+    return ovhUserPref.create(userPrefName, [subject]);
+  }
 
-    var self = this;
+  self.stopNotification = function stopNotification(userPrefName, subject) {
+    let notificationArray = [];
+    return ovhUserPref.getValue(userPrefName).then((data) => {
+      notificationArray = data;
 
-    self.stopNotification = function (userPrefName, subject) {
-        var notificationArray = [];
-        return ovhUserPref.getValue(userPrefName).then(function (data) {
-            notificationArray = data;
+      notificationArray.push(subject);
+      return ovhUserPref.assign(userPrefName, notificationArray);
+    }).catch(error => (error.status === 404
+      ? createNotificationUserPref(userPrefName, subject)
+      : $q.reject(error)));
+  };
 
-            notificationArray.push(subject);
-            return ovhUserPref.assign(userPrefName, notificationArray);
-        })["catch"](function (error) {
-            return error.status === 404 ? createNotificationUserPref(userPrefName, subject) : $q.reject(error);
-        });
-    };
-
-    self.checkIfStopNotification = function (userPrefName, isArray, subject) {
-        return ovhUserPref.getValue(userPrefName).then(function (notification) {
-            return isArray ? _.indexOf(notification, subject) !== -1 : notification;
-        })["catch"](function () {
-            return false;
-        });
-    };
-
-    function createNotificationUserPref(userPrefName, subject) {
-        return ovhUserPref.create(userPrefName, [subject]);
-    }
+  self.checkIfStopNotification = function checkIfStopNotification(userPrefName, isArray, subject) {
+    return ovhUserPref
+      .getValue(userPrefName)
+      .then(notification => (isArray
+        ? _.indexOf(notification, subject) !== -1
+        : notification))
+      .catch(() => false);
+  };
 }]);
