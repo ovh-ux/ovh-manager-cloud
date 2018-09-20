f0578530eedab150a2860314b42295dce5268a9f
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-20 17:43:47 +0200

    fix(vrack): apply eslint rules

--- client/app/vrack/add/vrack-add.controller.js
+++ client/app/vrack/add/vrack-add.controller.js
@@ -1,69 +1,68 @@
-"use strict";
 
-angular.module("managerApp").controller("VrackAddCtrl",
-    function ($q, $translate, $state, $rootScope, CloudMessage, VrackService, OvhApiOrder, TARGET) {
 
-        var self = this;
+angular.module('managerApp').controller('VrackAddCtrl',
+  function ($q, $translate, $state, $rootScope, CloudMessage, VrackService, OvhApiOrder, TARGET) {
+    const self = this;
 
-        self.TARGET = TARGET;
+    self.TARGET = TARGET;
 
-        this.loaders = {
-            loading: false,
-            validationPending: false
-        };
+    this.loaders = {
+      loading: false,
+      validationPending: false,
+    };
 
-        this.model = {
-            agreements: [],
-            contractsAccepted: false,
-            purchaseOrderUrl: ""
-        };
+    this.model = {
+      agreements: [],
+      contractsAccepted: false,
+      purchaseOrderUrl: '',
+    };
 
-        this.getVrackContract = function () {
-            return OvhApiOrder.Vrack().New().v6().get({
-                quantity: 1
-            }).$promise.then(function (data) {
-                self.model.agreements = data.contracts;
-            }).catch(function (error) {
-                CloudMessage.error($translate.instant("vrack_error_reason", { message: error.data.message }));
-            });
-        };
+    this.getVrackContract = function () {
+      return OvhApiOrder.Vrack().New().v6().get({
+        quantity: 1,
+      }).$promise.then((data) => {
+        self.model.agreements = data.contracts;
+      }).catch((error) => {
+        CloudMessage.error($translate.instant('vrack_error_reason', { message: error.data.message }));
+      });
+    };
 
-        this.addVrack = function () {
-            self.loaders.loading = true;
-            return OvhApiOrder.Vrack().New().v6().create({
-                quantity: this.model.quantityToOrder
-            }, {}).$promise.then(function (data) {
-                CloudMessage.success($translate.instant("vrack_adding_success", { data: _.pick(data, ["url", "orderId"]) }));
-                self.model.purchaseOrderUrl = data.url;
-                self.loaders.validationPending = true;
-            }).catch(function (error) {
-                CloudMessage.error($translate.instant("vrack_error_reason", { message: error.data.message }));
-            }).finally(function () {
-                self.loaders.loading = false;
-            });
-        };
+    this.addVrack = function () {
+      self.loaders.loading = true;
+      return OvhApiOrder.Vrack().New().v6().create({
+        quantity: this.model.quantityToOrder,
+      }, {}).$promise.then((data) => {
+        CloudMessage.success($translate.instant('vrack_adding_success', { data: _.pick(data, ['url', 'orderId']) }));
+        self.model.purchaseOrderUrl = data.url;
+        self.loaders.validationPending = true;
+      }).catch((error) => {
+        CloudMessage.error($translate.instant('vrack_error_reason', { message: error.data.message }));
+      }).finally(() => {
+        self.loaders.loading = false;
+      });
+    };
 
-        function init () {
-            self.loaders.loading = true;
-            self.vrackOrderUrl = null;
+    function init() {
+      self.loaders.loading = true;
+      self.vrackOrderUrl = null;
 
-            var promise = {
-                vrackOrderUrl: VrackService.getOrderUrl()
-            };
+      const promise = {
+        vrackOrderUrl: VrackService.getOrderUrl(),
+      };
 
-            if (self.TARGET !== "US") {
-                promise.vrackContract = self.getVrackContract();
-            }
+      if (self.TARGET !== 'US') {
+        promise.vrackContract = self.getVrackContract();
+      }
 
-            return $q
-                .all(promise)
-                .then(function (results) {
-                    self.vrackOrderUrl = results.vrackOrderUrl;
-                })
-                .finally(function () {
-                    self.loaders.loading = false;
-                });
-        }
+      return $q
+        .all(promise)
+        .then((results) => {
+          self.vrackOrderUrl = results.vrackOrderUrl;
+        })
+        .finally(() => {
+          self.loaders.loading = false;
+        });
+    }
 
-        init();
-    });
+    init();
+  });
