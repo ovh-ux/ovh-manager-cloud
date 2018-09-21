5ce4e02c0daf20afaf0c2722717885a82a42f2da
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-21 14:44:26 +0200

    fix(components.cloud.user-pref): apply eslint rules

--- client/components/cloud/cloud-user-pref/cloud-user-pref.js
+++ client/components/cloud/cloud-user-pref/cloud-user-pref.js
@@ -1,28 +1,19 @@
-"use strict";
-angular.module("managerApp").service("CloudUserPref",
-    function ($q, ovhUserPref) {
+angular.module('managerApp').service('CloudUserPref',
+  function ($q, ovhUserPref) {
+    this.get = function (key) {
+      if (_.isString(key)) {
+        return ovhUserPref
+          .getValue(key.toUpperCase())
+          .then(data => $q.when(data || {}), () => ({}));
+      }
+      return $q.reject('UserPref key must be of type String');
+    };
 
-        this.get = function (key) {
-            if (_.isString(key)) {
-                return ovhUserPref.getValue(key.toUpperCase()).then(function (data) {
-                    return $q.when(data || {});
-                }, function () {
-                    return {};
-                });
-            } else {
-                return $q.reject("UserPref key must be of type String");
-            }
-        };
-
-        this.set = function (key, value) {
-            // We do it asynchronously and assume everything is ok.
-            if (_.isString(key)) {
-                return ovhUserPref.assign(key.toUpperCase(), value || {}).then(function () {
-                    return $q.when(value);
-                });
-            } else {
-                return $q.reject("UserPref key must be of type String");
-            }
-        };
-    }
-);
+    this.set = function (key, value) {
+      // We do it asynchronously and assume everything is ok.
+      if (_.isString(key)) {
+        return ovhUserPref.assign(key.toUpperCase(), value || {}).then(() => $q.when(value));
+      }
+      return $q.reject('UserPref key must be of type String');
+    };
+  });
