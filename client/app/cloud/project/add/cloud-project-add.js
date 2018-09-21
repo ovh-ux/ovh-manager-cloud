ab1c20d33541b823a60ea7f1fb73e7726e8518bc
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-21 10:22:28 +0200

    fix(cloud.project.add): apply eslint rules

--- client/app/cloud/project/add/cloud-project-add.js
+++ client/app/cloud/project/add/cloud-project-add.js
@@ -1,21 +1,21 @@
-"use strict";
 
-angular.module("managerApp")
-    .config(function ($stateProvider) {
-        $stateProvider
-        /**
+
+angular.module('managerApp')
+  .config(($stateProvider) => {
+    $stateProvider
+    /**
          * NEW PROJECT
          * #/cloud/project/new (see "add" folder)
          */
 
-            .state("iaas.pci-project-new", {
-                url: "/pci/project/new",
-                templateUrl: "app/cloud/project/add/cloud-project-add.html",
-                controller: "CloudProjectAddCtrl",
-                controllerAs: "CloudProjectAddCtrl",
-                translations: [
-                  "common",
-                  "cloud/project/add",
-                ],
-            });
-    });
+      .state('iaas.pci-project-new', {
+        url: '/pci/project/new',
+        templateUrl: 'app/cloud/project/add/cloud-project-add.html',
+        controller: 'CloudProjectAddCtrl',
+        controllerAs: 'CloudProjectAddCtrl',
+        translations: [
+          'common',
+          'cloud/project/add',
+        ],
+      });
+  });
