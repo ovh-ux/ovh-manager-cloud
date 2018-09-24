e98e14016ebdcef7c79b3e027515bdb3101ab6d2
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-24 17:47:31 +0200

    fix(components.manager-preload): apply eslint rules

--- client/components/manager-preload/manager-preload.config.js
+++ client/components/manager-preload/manager-preload.config.js
@@ -1,15 +1,13 @@
-angular.module("managerApp").run(($q, $rootScope, $transitions, $translate, SessionService, ProductsService) => {
-
-    $transitions.onSuccess({}, () => {
-        $q.all({
-            user: SessionService.getUser(),
-            products: ProductsService.getProducts(),
-            translate: $translate.refresh()
-        }).then(() => {
-            $rootScope.managerPreloadHide += " manager-preload-hide";
-        });
-    }, {
-        priority: -1 // Last to load so we hide as much as possible
+angular.module('managerApp').run(($q, $rootScope, $transitions, $translate, SessionService, ProductsService) => {
+  $transitions.onSuccess({}, () => {
+    $q.all({
+      user: SessionService.getUser(),
+      products: ProductsService.getProducts(),
+      translate: $translate.refresh(),
+    }).then(() => {
+      $rootScope.managerPreloadHide += ' manager-preload-hide'; // eslint-disable-line
     });
-
+  }, {
+    priority: -1, // Last to load so we hide as much as possible
+  });
 });
