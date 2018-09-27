c0bc80bd42e609fa3b679fe3f7fc2471c3116251
Jérôme Martin <jerome.martin@corp.ovh.com>
2018-09-27 15:29:15 +0200

    feat(clouddb): add datagrid with fake data

--- client/app/vps/cloud-database/vps-cloud-database.controller.js
+++ client/app/vps/cloud-database/vps-cloud-database.controller.js
@@ -1,6 +1,27 @@
 class VpsCloudDatabaseCtrl {
-    constructor () {
+    constructor ($q /* , CloudDatabase */) {
+        this.$q = $q;
+        // this.CloudDatabase = CloudDatabase;
+    }
 
+    refresh () {
+        // this.CloudDatabase.getAll().then(databases => {
+        //     this.cloudDatabases = databases;
+        // });
+        return this.$q.when()
+            .then(() => [1, 2, 3])
+            .then(ids => ({
+                data: ids ? _.map(ids, id => ({ id })) : [],
+                meta: { totalCount: ids ? ids.length : 0 }
+            }));
+    }
+
+    transformItem (row) {
+        return this.$q.when().then(() => ({
+            name: `Database ${row.id}`,
+            type: "MariaDb",
+            status: "created"
+        }));
     }
 }
 
