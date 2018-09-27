efc148946e98f2fed9a757277c11781ad2a2b6d6
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-27 08:30:17 +0200

    fix(components.ovh-angular-otrs): apply eslint rules

--- client/components/ovh-angular-otrs/ovh-angular-otrs.js
+++ client/components/ovh-angular-otrs/ovh-angular-otrs.js
@@ -2,4 +2,3 @@ angular.module('managerApp').run(($translate, asyncLoader) => {
   asyncLoader.addTranslations(import(`ovh-angular-otrs/src/translations/Messages_${$translate.use()}.xml`).then(x => x.default));
   $translate.refresh();
 });
-
