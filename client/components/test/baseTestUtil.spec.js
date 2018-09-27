214b57e8c062ed833ac4c2809ecc59dcedcba4c6
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-27 10:29:58 +0200

    fix: remove unexplored karma mocha and protactor deps

--- client/components/test/baseTestUtil.spec.js
+++
@@ -1,9 +0,0 @@
-angular.module("managerAppMock").factory("baseTestUtil", function ($rootScope) {
-    "use strict";
-    function TestUtil () {
-        this.$scope = $rootScope;
-    }
-
-    return new TestUtil();
-
-});
--- client/components/translate/translate.mock.js
+++
@@ -1,7 +0,0 @@
-
-
-angular.module('translateMock', []);
-
-angular.module('translateMock').run(($httpBackend) => {
-  $httpBackend.whenGET(/translations\/Messages\w+\.json$/).respond(200, {});
-});
