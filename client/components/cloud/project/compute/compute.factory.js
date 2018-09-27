cf840591f74708a10704258abbe7ddeac92ec33f
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-27 14:35:49 +0200

    fix(components.cloud.project.compute): apply eslint rules

--- client/components/cloud/project/compute/compute.factory.js
+++ client/components/cloud/project/compute/compute.factory.js
@@ -1,45 +1,47 @@
-angular.module("managerApp").factory('CloudProjectComputeFactory',
-    function (CloudProjectComputeInfrastructureFactory, CloudProjectComputeVolumesFactory) {
+angular.module('managerApp').factory('CloudProjectComputeFactory',
+  (CloudProjectComputeInfrastructureFactory, CloudProjectComputeVolumesFactory) => {
+    /**
+     *  Defines a cloud project compute
+     *
+     *  @param    {Object}  options
+     *                      Options for creating a new CloudProjectCompute
+     *  @param    {Object}  options.infrastructure
+     *                      Options of the CloudProjectCompute infrastructure
+     *  @param    {Object}  options.volumes
+     *                      Options of the CloudProjectCompute volumes
+     */
+    const ComputeFactory = (function () {
+      return function CloudProjectComputeFactory(optionsParam) {
+        let options = optionsParam;
+        if (!options) {
+          options = {};
+        }
 
-        'use strict';
+        this.serviceName = options.serviceName || null;
 
-        /**
-         *  Defines a cloud project compute
-         *
-         *  @param    {Object}  options                 - Options for creating a new CloudProjectCompute
-         *  @param    {Object}  options.infrastructure  - Options of the CloudProjectCompute infrastructure
-         *  @param    {Object}  options.volumes         - Options of the CloudProjectCompute volumes
-         */
-        var ComputeFactory = (function () {
+        this.infrastructure = new CloudProjectComputeInfrastructureFactory(angular.extend(
+          (options.infrastructure || {}),
+          { serviceName: this.serviceName },
+        ));
+        this.volumes = new CloudProjectComputeVolumesFactory(angular.extend(
+          (options.volumes || {}),
+          { serviceName: this.serviceName },
+        ));
+      };
+    }());
 
-            return function CloudProjectComputeFactory (options) {
+    // /////////////////////////////
+    // /         METHODS          //
+    // /////////////////////////////
 
-                if (!options) {
-                    options = {};
-                }
+    /**
+     *  Prepare object to json encode function to avoid function being encoded
+     */
+    ComputeFactory.prototype.prepareToJson = function () {
+      return {
+        infrastructure: this.infrastructure.prepareToJson(),
+      };
+    };
 
-                this.serviceName = options.serviceName || null;
-
-                this.infrastructure = new CloudProjectComputeInfrastructureFactory( angular.extend((options.infrastructure || {}), { serviceName: this.serviceName }) );
-                this.volumes = new CloudProjectComputeVolumesFactory( angular.extend((options.volumes || {}), { serviceName: this.serviceName }) );
-            };
-
-        })();
-
-        ///////////////////////////////
-        ///         METHODS          //
-        ///////////////////////////////
-
-        /**
-         *  Prepare object to json encode function to avoid function being encoded
-         */
-        ComputeFactory.prototype.prepareToJson = function () {
-            return {
-                infrastructure: this.infrastructure.prepareToJson()
-            };
-        };
-
-        return ComputeFactory;
-
-    }
-);
+    return ComputeFactory;
+  });
