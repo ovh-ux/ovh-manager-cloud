5e111fbe0678ee965b6506f7d61633d1785555d2
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-09-27 10:13:22 +0200

    fix(components.ui-router): apply eslint rules

--- client/components/ui-router/ui-router.mock.js
+++ client/components/ui-router/ui-router.mock.js
@@ -1,34 +1,32 @@
-"use strict";
+angular.module('stateMock', []);
+angular.module('stateMock').service('$state', function ($q) {
+  this.expectedTransitions = [];
 
-angular.module("stateMock", []);
-angular.module("stateMock").service("$state", function ($q) {
-    this.expectedTransitions = [];
+  this.transitionTo = function (stateName) {
+    if (this.expectedTransitions.length > 0) {
+      const expectedState = this.expectedTransitions.shift();
+      if (expectedState !== stateName) {
+        throw Error(`Expected transition to state: ${expectedState} but transitioned to ${stateName}`);
+      }
+    } else {
+      throw Error(`No more transitions were expected! Tried to transition to ${stateName}`);
+    }
+    console.log(`Mock transition to: ${stateName}`);
+    const deferred = $q.defer();
+    const { promise } = deferred;
+    deferred.resolve();
+    return promise;
+  };
 
-    this.transitionTo = function (stateName) {
-        if (this.expectedTransitions.length > 0) {
-            var expectedState = this.expectedTransitions.shift();
-            if (expectedState !== stateName) {
-                throw Error("Expected transition to state: " + expectedState + " but transitioned to " + stateName);
-            }
-        } else {
-            throw Error("No more transitions were expected! Tried to transition to " + stateName);
-        }
-        console.log("Mock transition to: " + stateName);
-        var deferred = $q.defer();
-        var promise = deferred.promise;
-        deferred.resolve();
-        return promise;
-    };
+  this.go = this.transitionTo;
 
-    this.go = this.transitionTo;
+  this.expectTransitionTo = function (stateName) {
+    this.expectedTransitions.push(stateName);
+  };
 
-    this.expectTransitionTo = function (stateName) {
-        this.expectedTransitions.push(stateName);
-    };
-
-    this.ensureAllTransitionsHappened = function () {
-        if (this.expectedTransitions.length > 0) {
-            throw Error("Not all transitions happened!");
-        }
-    };
+  this.ensureAllTransitionsHappened = function () {
+    if (this.expectedTransitions.length > 0) {
+      throw Error('Not all transitions happened!');
+    }
+  };
 });
