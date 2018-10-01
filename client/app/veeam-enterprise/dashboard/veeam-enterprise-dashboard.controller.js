84416cd9af4296ebedb7933a6d16c84f44139161
Axel Peter <axel.peter@corp.ovh.com>
2018-10-01 17:46:57 +0200

    fix: apply eslint

--- client/app/veeam-enterprise/dashboard/veeam-enterprise-dashboard.controller.js
+++ client/app/veeam-enterprise/dashboard/veeam-enterprise-dashboard.controller.js
@@ -1,90 +1,93 @@
 (() => {
-    class VeeamEnterpriseDashboardCtrl {
-        constructor ($stateParams, $translate, ControllerHelper, FeatureAvailabilityService, VeeamEnterpriseService) {
-            this.$stateParams = $stateParams;
-            this.$translate = $translate;
-            this.ControllerHelper = ControllerHelper;
-            this.FeatureAvailabilityService = FeatureAvailabilityService;
-            this.VeeamEnterpriseService = VeeamEnterpriseService;
+  class VeeamEnterpriseDashboardCtrl {
+    constructor($stateParams, $translate, ControllerHelper, FeatureAvailabilityService,
+      VeeamEnterpriseService) {
+      this.$stateParams = $stateParams;
+      this.$translate = $translate;
+      this.ControllerHelper = ControllerHelper;
+      this.FeatureAvailabilityService = FeatureAvailabilityService;
+      this.VeeamEnterpriseService = VeeamEnterpriseService;
 
-            this.serviceName = this.$stateParams.serviceName;
+      this.serviceName = this.$stateParams.serviceName;
 
-            this.initLoaders();
-            this.initActions();
-        }
-
-        initLoaders () {
-            const errorHandler = response => this.VeeamEnterpriseService.unitOfWork.messages.push({
-                text: response.message,
-                type: "error"
-            });
-
-            this.configurationInfos = this.ControllerHelper.request.getHashLoader({
-                loaderFunction: () => this.VeeamEnterpriseService.getConfigurationInfos(this.serviceName),
-                errorHandler
-            });
-
-            this.subscriptionInfos = this.ControllerHelper.request.getHashLoader({
-                loaderFunction: () => this.VeeamEnterpriseService.getSubscriptionInfos(this.serviceName),
-                errorHandler
-            });
-        }
-
-        initActions () {
-            this.uiActions = {
-                manageAutorenew: {
-                    text: this.$translate.instant("common_manage"),
-                    href: this.ControllerHelper.navigation.getUrl(
-                        "renew", {
-                            serviceName: this.serviceName,
-                            serviceType: "VEEAM_ENTERPRISE"
-                        }),
-                    isAvailable: () => true
-                },
-                manageContact: {
-                    text: this.$translate.instant("common_manage"),
-                    href: this.ControllerHelper.navigation.getUrl(
-                        "contacts", {
-                            serviceName: this.serviceName
-                        }),
-                    isAvailable: () => this.FeatureAvailabilityService.hasFeature("CONTACTS", "manage")
-                }
-            };
-        }
-
-        $onInit () {
-            this.configurationInfos.load();
-            this.subscriptionInfos.load();
-        }
-
-        activateLicense () {
-            this.ControllerHelper.modal.showModal({
-                modalConfig: {
-                    templateUrl: "app/veeam-enterprise/dashboard/license/veeam-enterprise-license.html",
-                    controller: "VeeamEnterpriseLicenseCtrl",
-                    controllerAs: "$ctrl",
-                    resolve: {
-                        action: () => "register",
-                        serviceName: () => this.serviceName
-                    }
-                }
-            });
-        }
-
-        updateLicense () {
-            this.ControllerHelper.modal.showModal({
-                modalConfig: {
-                    templateUrl: "app/veeam-enterprise/dashboard/license/veeam-enterprise-license.html",
-                    controller: "VeeamEnterpriseLicenseCtrl",
-                    controllerAs: "$ctrl",
-                    resolve: {
-                        action: () => "update",
-                        serviceName: () => this.serviceName
-                    }
-                }
-            });
-        }
+      this.initLoaders();
+      this.initActions();
     }
 
-    angular.module("managerApp").controller("VeeamEnterpriseDashboardCtrl", VeeamEnterpriseDashboardCtrl);
+    initLoaders() {
+      const errorHandler = response => this.VeeamEnterpriseService.unitOfWork.messages.push({
+        text: response.message,
+        type: 'error',
+      });
+
+      this.configurationInfos = this.ControllerHelper.request.getHashLoader({
+        loaderFunction: () => this.VeeamEnterpriseService.getConfigurationInfos(this.serviceName),
+        errorHandler,
+      });
+
+      this.subscriptionInfos = this.ControllerHelper.request.getHashLoader({
+        loaderFunction: () => this.VeeamEnterpriseService.getSubscriptionInfos(this.serviceName),
+        errorHandler,
+      });
+    }
+
+    initActions() {
+      this.uiActions = {
+        manageAutorenew: {
+          text: this.$translate.instant('common_manage'),
+          href: this.ControllerHelper.navigation.getUrl(
+            'renew', {
+              serviceName: this.serviceName,
+              serviceType: 'VEEAM_ENTERPRISE',
+            },
+          ),
+          isAvailable: () => true,
+        },
+        manageContact: {
+          text: this.$translate.instant('common_manage'),
+          href: this.ControllerHelper.navigation.getUrl(
+            'contacts', {
+              serviceName: this.serviceName,
+            },
+          ),
+          isAvailable: () => this.FeatureAvailabilityService.hasFeature('CONTACTS', 'manage'),
+        },
+      };
+    }
+
+    $onInit() {
+      this.configurationInfos.load();
+      this.subscriptionInfos.load();
+    }
+
+    activateLicense() {
+      this.ControllerHelper.modal.showModal({
+        modalConfig: {
+          templateUrl: 'app/veeam-enterprise/dashboard/license/veeam-enterprise-license.html',
+          controller: 'VeeamEnterpriseLicenseCtrl',
+          controllerAs: '$ctrl',
+          resolve: {
+            action: () => 'register',
+            serviceName: () => this.serviceName,
+          },
+        },
+      });
+    }
+
+    updateLicense() {
+      this.ControllerHelper.modal.showModal({
+        modalConfig: {
+          templateUrl: 'app/veeam-enterprise/dashboard/license/veeam-enterprise-license.html',
+          controller: 'VeeamEnterpriseLicenseCtrl',
+          controllerAs: '$ctrl',
+          resolve: {
+            action: () => 'update',
+            serviceName: () => this.serviceName,
+          },
+        },
+      });
+    }
+  }
+
+  angular.module('managerApp').controller('VeeamEnterpriseDashboardCtrl', VeeamEnterpriseDashboardCtrl);
 })();
--- client/app/veeam-enterprise/veeam-enterprise.js
+++ client/app/veeam-enterprise/veeam-enterprise.js
@@ -1,22 +1,22 @@
-angular.module("managerApp").config($stateProvider => {
-    $stateProvider
-        .state("paas.veeam-enterprise", {
-            "abstract": true,
-            url: "/veeam-enterprise/{serviceName}",
-            templateUrl: "app/veeam-enterprise/veeam-enterprise.html",
-            controller: "VeeamEnterpriseCtrl",
-            controllerAs: "VeeamEnterpriseCtrl",
-            translations: ["common", "veeam-enterprise"]
-        })
-        .state("paas.veeam-enterprise.dashboard", {
-            url: "/dashboard",
-            views: {
-                veeamEnterpriseContent: {
-                    templateUrl: "app/veeam-enterprise/dashboard/veeam-enterprise-dashboard.html",
-                    controller: "VeeamEnterpriseDashboardCtrl",
-                    controllerAs: "$ctrl"
-                }
-            },
-            translations: ["common", "veeam-enterprise", "veeam-enterprise/dashboard"]
-        });
+angular.module('managerApp').config(($stateProvider) => {
+  $stateProvider
+    .state('paas.veeam-enterprise', {
+      abstract: true,
+      url: '/veeam-enterprise/{serviceName}',
+      templateUrl: 'app/veeam-enterprise/veeam-enterprise.html',
+      controller: 'VeeamEnterpriseCtrl',
+      controllerAs: 'VeeamEnterpriseCtrl',
+      translations: ['../common', '.'],
+    })
+    .state('paas.veeam-enterprise.dashboard', {
+      url: '/dashboard',
+      views: {
+        veeamEnterpriseContent: {
+          templateUrl: 'app/veeam-enterprise/dashboard/veeam-enterprise-dashboard.html',
+          controller: 'VeeamEnterpriseDashboardCtrl',
+          controllerAs: '$ctrl',
+        },
+      },
+      translations: ['../common', '.', './dashboard'],
+    });
 });
