9fa640f68444b518159bce735a8fe74fb16dab2d
Jérôme Martin <jerome.martin@corp.ovh.com>
2018-10-08 16:27:18 +0200

    feat(clouddb): display prices

--- client/app/vps/cloud-database/order/vps-cloud-database-order.controller.js
+++ client/app/vps/cloud-database/order/vps-cloud-database-order.controller.js
@@ -1,10 +1,18 @@
 class VpsCloudDatabaseOrderCtrl {
-    constructor ($q, $translate, CloudMessage, OvhApiHostingPrivateDatabase, OvhApiOrderPrivateDatabase) {
+    constructor (
+        $q,
+        $translate,
+        CloudMessage,
+        OvhApiHostingPrivateDatabase,
+        OvhApiOrderPrivateDatabase,
+        OvhApiMe
+    ) {
         this.$q = $q;
         this.$translate = $translate;
         this.CloudMessage = CloudMessage;
         this.ApiPrivateDb = OvhApiHostingPrivateDatabase.v6();
         this.ApiOrderDb = OvhApiOrderPrivateDatabase.v6();
+        this.ApiMe = OvhApiMe.v6();
     }
 
     $onInit () {
@@ -28,6 +36,9 @@ class VpsCloudDatabaseOrderCtrl {
         };
 
         this.loadSelectValues();
+        this.ApiMe.get().$promise.then(user => {
+            this.user = user;
+        });
     }
 
     loadSelectValues () {
@@ -57,24 +68,40 @@ class VpsCloudDatabaseOrderCtrl {
             return this.$q.when();
         }
 
+        const version = this.currentOrder.version.value;
+        const ram = this.currentOrder.ram.value;
+        const datacenter = this.currentOrder.datacenter.value;
+
         this.loading.durations = true;
-        return this.ApiOrderDb.getNew({
-            version: this.currentOrder.version.value,
-            ram: this.currentOrder.ram.value,
-            datacenter: this.currentOrder.datacenter.value
-        }).$promise
+        return this.ApiOrderDb.getNew({ version, ram, datacenter }).$promise
             .then(durations => {
-                this.durations = _.map(durations, duration => ({
-                    value: duration,
-                    text: `${duration} months`
-                }));
+                this.durations = _.map(durations, duration => ({ value: duration, prices: null }));
+                // we run this in parallel, so no return
+                this.getPricesForEachDuration(this.durations, version, ram, datacenter);
             })
             .catch(error => this.CloudMessage.error([
                 this.$translate.instant("vps_tab_cloud_database_order_fetch_duration_failed"),
-                _(error).get("data.message", "")
+                _(error).get("data.message", error)
             ].join(" ")))
             .finally(() => {
                 this.loading.durations = false;
+            });
+    }
+
+    getPricesForEachDuration (durations, version, ram, datacenter) {
+        return this.$q.all(_.map(
+            durations,
+            duration => this.getPrices(duration, version, ram, datacenter)
+        )).catch(error => this.CloudMessage.error([
+            this.$translate.instant("vps_tab_cloud_database_order_fetch_prices_failed"),
+            _(error).get("data.message", error)
+        ].join(" ")));
+    }
+
+    getPrices (duration, version, ram, datacenter) {
+        return this.ApiOrderDb.getNewDetails({ duration: duration.value, version, ram, datacenter }).$promise
+            .then(({ prices }) => {
+                duration.prices = prices;
             });
     }
 }
---
+++ client/components/price/price-filter.js
@@ -0,0 +1,76 @@
+angular.module("managerApp").filter("price", [
+    "$translate",
+    function ($translate) {
+        "use strict";
+
+        const frenchTouch = {
+            TTCOnly: false,
+            HTOnly: false,
+            withTTC: true
+        };
+
+        const usTouch = {
+            TTCOnly: false,
+            HTOnly: true,
+            withTTC: false
+        };
+
+        const deutchTouch = {
+            TTCOnly: true,
+            HTOnly: false,
+            withTTC: true
+        };
+
+        const showTaxes = {
+            CA: usTouch,
+            WE: usTouch,
+            WS: usTouch,
+            QC: usTouch,
+            DE: deutchTouch,
+            FI: deutchTouch,
+            SN: deutchTouch,
+            CZ: frenchTouch,
+            ES: frenchTouch,
+            FR: frenchTouch,
+            GB: frenchTouch,
+            IE: frenchTouch,
+            IT: frenchTouch,
+            LT: frenchTouch,
+            MA: frenchTouch,
+            NL: frenchTouch,
+            PL: frenchTouch,
+            PT: frenchTouch,
+            TN: frenchTouch
+        };
+
+        function format (price, country, frequency) {
+            country = country || "FR";
+
+            const taxes = showTaxes[country];
+            if (price.withTax.value !== 0) {
+                if (taxes.TTCOnly) {
+                    return `<span class="bold">${price.withTax.text}</span>`;
+                }
+                if (taxes.HTOnly) {
+                    return `<span class="bold">${price.withoutTax.text}</span>`;
+                }
+                if (frequency === "yearly") {
+                    return `<span class="bold">${$translate.instant("common_price_ht_label", { price: price.withoutTax.text })}` +
+                           `<small>${$translate.instant("common_price_label_yearly")}</small></span> ` +
+                           `<span class="italic small"> (${$translate.instant("common_price_ttc_label", { price: price.withTax.text })}` +
+                           `<small>${$translate.instant("common_price_label_yearly")}</small>)</span>`;
+                }
+                return `<span class="bold">${$translate.instant("common_price_ht_label", { price: price.withoutTax.text })}` +
+                       `</span> <span class="italic small"> (${$translate.instant("common_price_ttc_label", { price: price.withTax.text })})</span>`;
+            }
+            return `<span class="bold">${$translate.instant("common_price_free")}</span>`;
+        }
+
+        return function (price, ovhSubsidiary, frequency) {
+            if (price) {
+                return format(price, ovhSubsidiary, frequency);
+            }
+
+            return "<span/>";
+        };
+    }]);
