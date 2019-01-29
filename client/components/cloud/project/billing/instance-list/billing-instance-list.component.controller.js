c918339602bb96eed9c6a413b25f306914de2c18
Marie JONES <marie.jones@corp.ovh.com>
2019-01-29 16:23:43 +0100

    refactor(components.billing.instance-list): switch to a class

--- client/components/cloud/project/billing/instance-list/billing-instance-list.component.controller.js
+++ client/components/cloud/project/billing/instance-list/billing-instance-list.component.controller.js
@@ -1,153 +1,175 @@
 angular.module('managerApp')
-  .controller('BillingInstanceListComponentCtrl', function BillingInstanceListComponentCtrl($stateParams, $q, $translate, OvhApiCloudProjectImage, DetailsPopoverService, OvhApiCloudProjectInstance, Toast, OvhApiMe, OvhCloudPriceHelper) {
-    const self = this;
-    self.windowsStringPattern = '/^win-/';
-    self.instanceConsumptionDetails = [];
-
-    self.data = {
-      instances: [],
-      images: [],
-      instanceToMonthlyPrice: null,
-    };
-
-    self.loaders = {
-      monthlyBilling: false,
-      instanceList: false,
-    };
-
-    self.DetailsPopoverService = DetailsPopoverService;
-    self.currencySymbol = '';
-
-    self.instanceToMonthly = null;
-
-    function initInstances() {
-      return OvhApiCloudProjectInstance.v6().query({
-        serviceName: $stateParams.projectId,
-      }).$promise.then((instances) => {
-        self.data.instances = instances;
-      });
-    }
-
-    function initImages() {
-      return OvhApiCloudProjectImage.v6().query({
-        serviceName: $stateParams.projectId,
-      }).$promise.then((result) => {
-        self.data.images = result;
-      });
-    }
-
-    function initUserCurrency() {
-      return OvhApiMe.v6().get().$promise.then((me) => {
-        self.currencySymbol = me.currency.symbol;
-      });
-    }
-
-    function getImageTypeFromReference(reference) {
-      if (reference) {
-        return /^win/.test(reference) ? 'windows' : 'linux';
-      }
-      return '';
-    }
-
-    function getInstanceConsumptionDetails(billingDetail) {
-      const instanceConsumptionDetail = {};
-      instanceConsumptionDetail.instanceId = billingDetail.instanceId;
-      instanceConsumptionDetail.instanceName = billingDetail.instanceId;
-      instanceConsumptionDetail.total = `${billingDetail.totalPrice.toFixed(2)} ${self.currencySymbol}`;
-      instanceConsumptionDetail.region = billingDetail.region;
-      instanceConsumptionDetail.reference = billingDetail.reference;
-      instanceConsumptionDetail.imageType = getImageTypeFromReference(billingDetail.reference);
-      instanceConsumptionDetail.vmType = billingDetail.reference ? billingDetail.reference.replace(self.windowsStringPattern, '').toUpperCase() : '';
-
-      const instance = _.find(self.data.instances, { id: billingDetail.instanceId });
-      if (instance) {
-        instanceConsumptionDetail.isDeleted = false;
-        instanceConsumptionDetail.instanceName = instance.name;
-        instanceConsumptionDetail.monthlyBilling = instance.monthlyBilling;
-        instanceConsumptionDetail.planCode = instance.planCode;
-        const imageData = _.find(self.data.images, { id: instance.imageId });
-        if (imageData) {
-          instanceConsumptionDetail.imageType = imageData.type;
-        }
-      } else {
-        instanceConsumptionDetail.isDeleted = true;
+  .controller('BillingInstanceListComponentCtrl',
+    class {
+      /* @ngInject */
+      constructor(
+        $stateParams,
+        $q, $translate,
+        OvhApiCloudProjectImage,
+        DetailsPopoverService,
+        OvhApiCloudProjectInstance,
+        Toast,
+        OvhApiMe,
+        OvhCloudPriceHelper,
+      ) {
+        this.$stateParams = $stateParams;
+        this.$q = $q;
+        this.$translate = $translate;
+        this.OvhApiCloudProjectImage = OvhApiCloudProjectImage;
+        this.DetailsPopoverService = DetailsPopoverService;
+        this.OvhApiCloudProjectInstance = OvhApiCloudProjectInstance;
+        this.Toast = Toast;
+        this.OvhApiMe = OvhApiMe;
+        this.OvhCloudPriceHelper = OvhCloudPriceHelper;
       }
 
-      return instanceConsumptionDetail;
-    }
+      $onInit() {
+        this.windowsStringPattern = /^win-/;
+        this.instanceConsumptionDetails = [];
 
-    function loadConsumptionDetails() {
-      self.instanceConsumptionDetailsInit = _.map(
-        self.instances,
-        billingDetail => getInstanceConsumptionDetails(billingDetail),
-      );
+        this.data = {
+          instances: [],
+          images: [],
+          instanceToMonthlyPrice: null,
+        };
 
-      $q.allSettled(self.instanceConsumptionDetailsInit).then((instances) => {
-        self.instanceConsumptionDetails = instances;
-      });
-    }
+        this.loaders = {
+          monthlyBilling: false,
+          instanceList: true,
+        };
 
-    self.$onInit = () => {
-      self.loaders.instanceList = true;
+        this.DetailsPopoverService = this.DetailsPopoverService;
+        this.currencySymbol = '';
 
-      $q
-        .all([
-          initInstances(),
-          initImages(),
-          initUserCurrency(),
-        ])
-        .then(() => {
-          loadConsumptionDetails();
-        })
-        .catch((err) => {
-          Toast.error([$translate.instant('cpb_error_message'), (err.data && err.data.message) || ''].join(' '));
-          return $q.reject(err);
-        })
-        .finally(() => {
-          self.loaders.instanceList = false;
+        this.instanceToMonthly = null;
+
+        return this.$q
+          .all([
+            this.initInstances(),
+            this.initImages(),
+            this.initUserCurrency(),
+          ])
+          .then(() => {
+            this.loadConsumptionDetails();
+          })
+          .catch((err) => {
+            this.Toast.error([this.$translate.instant('cpb_error_message'), (err.data && err.data.message) || ''].join(' '));
+            return this.$q.reject(err);
+          })
+          .finally(() => {
+            this.loaders.instanceList = false;
+          });
+      }
+
+
+      initInstances() {
+        return this.OvhApiCloudProjectInstance.v6().query({
+          serviceName: this.$stateParams.projectId,
+        }).$promise.then((instances) => {
+          this.data.instances = instances;
         });
-    };
+      }
 
-    self.prepareMonthlyPaymentActivation = function (instance) {
-      self.instanceToMonthly = instance.instanceId;
-      self.data.instanceToMonthlyPrice = null;
-      self.loaders.monthlyBilling = true;
+      initImages() {
+        return this.OvhApiCloudProjectImage.v6().query({
+          serviceName: this.$stateParams.projectId,
+        }).$promise.then((result) => {
+          this.data.images = result;
+        });
+      }
 
-      OvhCloudPriceHelper.getPrices($stateParams.projectId).then((prices) => {
-        const monthlyPrice = prices[instance.planCode && instance.planCode.replace('consumption', 'monthly')];
-        if (!monthlyPrice) {
-          self.endInstanceToMonthlyConversion();
-          return $q.reject({ data: { message: 'No monthly price for this instance' } });
+      initUserCurrency() {
+        return this.OvhApiMe.v6().get().$promise.then((me) => {
+          this.currencySymbol = me.currency.symbol;
+        });
+      }
+
+      getImageTypeFromReference(reference) {
+        if (reference) {
+          return this.windowsStringPattern.test(reference) ? 'windows' : 'linux';
         }
-        self.data.instanceToMonthlyPrice = monthlyPrice;
-        return $.when();
-      }).catch((err) => {
-        self.instanceToMonthly = null;
-        Toast.error([$translate.instant('cpbc_hourly_instance_pass_to_monthly_price_error'), (err.data && err.data.message) || ''].join(' '));
-        return $q.reject(err);
-      }).finally(() => {
-        self.loaders.monthlyBilling = false;
-      });
-    };
+        return '';
+      }
 
-    self.confirmMonthlyPaymentActivation = function () {
-      self.loaders.monthlyBilling = true;
+      getInstanceConsumptionDetails(billingDetail) {
+        const instanceConsumptionDetail = {};
+        instanceConsumptionDetail.instanceId = billingDetail.instanceId;
+        instanceConsumptionDetail.instanceName = billingDetail.instanceId;
+        instanceConsumptionDetail.total = `${billingDetail.totalPrice.toFixed(2)} ${this.currencySymbol}`;
+        instanceConsumptionDetail.region = billingDetail.region;
+        instanceConsumptionDetail.reference = billingDetail.reference;
+        instanceConsumptionDetail
+          .imageType = this.getImageTypeFromReference(billingDetail.reference);
+        instanceConsumptionDetail.vmType = billingDetail.reference ? billingDetail.reference.replace(this.windowsStringPattern, '').toUpperCase() : '';
 
-      OvhApiCloudProjectInstance.v6().activeMonthlyBilling({
-        serviceName: $stateParams.projectId,
-        instanceId: self.instanceToMonthly,
-      }, {}).$promise.then(() => {
-        // reset loaders and instance to activate
-        self.endInstanceToMonthlyConversion();
-      }).catch((err) => {
-        Toast.error([$translate.instant('cpbc_hourly_instance_pass_to_monthly_error'), (err.data && err.data.message) || ''].join(' '));
-        return $q.reject(err);
-      }).finally(() => {
-        self.loaders.monthlyBilling = false;
-      });
-    };
+        const instance = _.find(this.data.instances, { id: billingDetail.instanceId });
+        if (instance) {
+          instanceConsumptionDetail.isDeleted = false;
+          instanceConsumptionDetail.instanceName = instance.name;
+          instanceConsumptionDetail.monthlyBilling = instance.monthlyBilling;
+          instanceConsumptionDetail.planCode = instance.planCode;
+          const imageData = _.find(this.data.images, { id: instance.imageId });
+          if (imageData) {
+            instanceConsumptionDetail.imageType = imageData.type;
+          }
+        } else {
+          instanceConsumptionDetail.isDeleted = true;
+        }
 
-    self.endInstanceToMonthlyConversion = function () {
-      self.instanceToMonthly = null;
-    };
-  });
+        return instanceConsumptionDetail;
+      }
+
+      loadConsumptionDetails() {
+        this.instanceConsumptionDetailsInit = _.map(
+          this.instances,
+          billingDetail => this.getInstanceConsumptionDetails(billingDetail),
+        );
+
+        this.$q.allSettled(this.instanceConsumptionDetailsInit).then((instances) => {
+          this.instanceConsumptionDetails = instances;
+        });
+      }
+
+      prepareMonthlyPaymentActivation(instance) {
+        this.instanceToMonthly = instance.instanceId;
+        this.data.instanceToMonthlyPrice = null;
+        this.loaders.monthlyBilling = true;
+
+        this.OvhCloudPriceHelper.getPrices(this.$stateParams.projectId).then((prices) => {
+          const monthlyPrice = prices[instance.planCode && instance.planCode.replace('consumption', 'monthly')];
+          if (!monthlyPrice) {
+            this.endInstanceToMonthlyConversion();
+            return this.$q.reject({ data: { message: 'No monthly price for this instance' } });
+          }
+          this.data.instanceToMonthlyPrice = monthlyPrice;
+          return $.when();
+        }).catch((err) => {
+          this.instanceToMonthly = null;
+          this.Toast.error([this.$translate.instant('cpbc_hourly_instance_pass_to_monthly_price_error'), (err.data && err.data.message) || ''].join(' '));
+          return this.$q.reject(err);
+        }).finally(() => {
+          this.loaders.monthlyBilling = false;
+        });
+      }
+
+      confirmMonthlyPaymentActivation() {
+        this.loaders.monthlyBilling = true;
+
+        this.OvhApiCloudProjectInstance.v6().activeMonthlyBilling({
+          serviceName: this.$stateParams.projectId,
+          instanceId: this.instanceToMonthly,
+        }, {}).$promise.then(() => {
+          // reset loaders and instance to activate
+          this.endInstanceToMonthlyConversion();
+        }).catch((err) => {
+          this.Toast.error([this.$translate.instant('cpbc_hourly_instance_pass_to_monthly_error'), (err.data && err.data.message) || ''].join(' '));
+          return this.$q.reject(err);
+        }).finally(() => {
+          this.loaders.monthlyBilling = false;
+        });
+      }
+
+      endInstanceToMonthlyConversion() {
+        this.instanceToMonthly = null;
+      }
+    });
