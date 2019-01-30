939071421a376186bc6b77bcef4a9dc4444c5b2b
Ravindra Adireddy <ravindra.adireddy@corp.ovh.com>
2019-01-30 15:17:43 +0530

    fix(location): openstreetmap location is missing
    openstreetmap location is not shown for SYD and SGP locations
    
    Closes #MANAGER-2147

--- client/components/openStreetMap/openStreetMap.filter.js
+++ client/components/openStreetMap/openStreetMap.filter.js
@@ -6,6 +6,8 @@ angular.module('managerApp').filter('openStreetMap', (RegionService) => {
     WAW: 'https://www.openstreetmap.org/?mlat=52.23000&mlon=21.01100#map=4/50.00/20.00',
     DE: 'https://www.openstreetmap.org/?mlat=50.11365&mlon=8.67870#map=5/50.00/8.00',
     UK: 'https://www.openstreetmap.org/?mlat=51.50073&mlon=-0.12611#map=5/50.00/0',
+    SYD: 'https://www.openstreetmap.org/?mlat=-33.865143&mlon=151.209900#map=5/-33.847/151.239',
+    SGP: 'https://www.openstreetmap.org/?mlat=1.290270&mlon=103.851959#map=8/1.290/103.852',
   };
 
   return function (region) {
