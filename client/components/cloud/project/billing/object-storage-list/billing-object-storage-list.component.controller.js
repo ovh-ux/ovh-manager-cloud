02f14cadc7fd2f9c77420f4490afbea7e35fc1eb
Marie JONES <marie.jones@corp.ovh.com>
2019-01-31 14:46:49 +0100

    refactor(components.billing.object-storage): switch controller to class

--- client/components/cloud/project/billing/object-storage-list/billing-object-storage-list.component.controller.js
+++ client/components/cloud/project/billing/object-storage-list/billing-object-storage-list.component.controller.js
@@ -1,42 +1,47 @@
 angular.module('managerApp')
-  .controller('BillingObjectStorageListComponentCtrl', function ($q, $filter, $translate, OvhApiMe, Toast, RegionService) {
-    const self = this;
-    self.RegionService = RegionService;
+  .controller('BillingObjectStorageListComponentCtrl',
+    class {
+      /* @ngInject */
+      constructor($q, $filter, $translate, OvhApiMe, Toast, RegionService) {
+        this.$q = $q;
+        this.$filter = $filter;
+        this.$translate = $translate;
+        this.OvhApiMe = OvhApiMe;
+        this.Toast = Toast;
+        this.RegionService = RegionService;
+      }
 
-    self.currencySymbol = '';
+      $onInit() {
+        this.loading = true;
+        this.currencySymbol = '';
+        return this.$q.all([this.initUserCurrency()])
+          .catch((err) => {
+            this.Toast.error([this.$translate.instant('cpb_error_message'), (err.data && err.data.message) || ''].join(' '));
+            return this.$q.reject(err);
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+      }
 
-    self.loading = false;
-
-    function initUserCurrency() {
-      return OvhApiMe.v6().get().$promise.then((me) => {
-        self.currencySymbol = me.currency.symbol;
-      });
-    }
-
-    self.$onInit = () => {
-      self.loading = true;
-
-      $q.all([initUserCurrency()])
-        .catch((err) => {
-          Toast.error([$translate.instant('cpb_error_message'), (err.data && err.data.message) || ''].join(' '));
-          return $q.reject(err);
-        })
-        .finally(() => {
-          self.loading = false;
+      initUserCurrency() {
+        return this.OvhApiMe.v6().get().$promise.then((me) => {
+          this.currencySymbol = me.currency.symbol;
         });
-    };
+      }
 
-    self.getStorageVolumeInfoTooltip = function (storage) {
-      return $translate.instant('cpbc_object_storage_consumption_info_part1')
-        .concat($translate.instant('cpbc_object_storage_consumption_info_part2', {
-          amount: (storage.stored ? storage.stored.quantity.value : 0),
-        }));
-    };
 
-    self.getStorageBandwidthInfoTooltip = function (storage) {
-      return $translate.instant('cpbc_object_storage_output_traffic_info_part1')
-        .concat($translate.instant('cpbc_object_storage_output_traffic_info_part2', {
-          amount: storage.outgoingBandwidth.quantity.value,
-        }));
-    };
-  });
+      getStorageVolumeInfoTooltip(storage) {
+        return this.$translate.instant('cpbc_object_storage_consumption_info_part1')
+          .concat(this.$translate.instant('cpbc_object_storage_consumption_info_part2', {
+            amount: (storage.stored ? storage.stored.quantity.value : 0),
+          }));
+      }
+
+      getStorageBandwidthInfoTooltip(storage) {
+        return this.$translate.instant('cpbc_object_storage_output_traffic_info_part1')
+          .concat(this.$translate.instant('cpbc_object_storage_output_traffic_info_part2', {
+            amount: storage.outgoingBandwidth.quantity.value,
+          }));
+      }
+    });
