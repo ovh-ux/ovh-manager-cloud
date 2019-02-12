8d892943f29fe897a445ee035b62e44111bf08de
Marie JONES <marie.jones@corp.ovh.com>
2019-02-12 17:04:47 +0100

    refactor(cloud.project.billing.estimate): switch controller to a class

--- client/app/cloud/project/billing/consumption/estimate/cloud-project-billing-consumption-estimate.controller.js
+++ client/app/cloud/project/billing/consumption/estimate/cloud-project-billing-consumption-estimate.controller.js
@@ -1,186 +1,196 @@
 angular.module('managerApp').controller('CloudProjectBillingConsumptionEstimateCtrl',
-  function CloudProjectBillingConsumptionEstimateCtrl($q, $uibModal, $stateParams, $translate,
-    OvhApiCloudProjectAlerting, CucCloudMessage, OvhApiCloudProjectUsageForecast,
-    OvhApiCloudProjectUsageCurrent, CloudProjectBillingService) {
-    const self = this;
-    self.loading = false;
-    self.data = {
-      currencySymbol: null,
-      alert: null,
-      estimateTotals: null,
-      currentTotals: null,
-    };
+  class CloudProjectBillingConsumptionEstimateCtrl {
+    /* @ngInject */
+    constructor($q, $stateParams, $translate, $uibModal,
+      CucCloudMessage, CloudProjectBillingService, OvhApiCloudProjectAlerting,
+      OvhApiCloudProjectUsageCurrent, OvhApiCloudProjectUsageForecast) {
+      this.$q = $q;
+      this.$stateParams = $stateParams;
+      this.$translate = $translate;
+      this.$uibModal = $uibModal;
+      this.CucCloudMessage = CucCloudMessage;
+      this.CloudProjectBillingService = CloudProjectBillingService;
+      this.OvhApiCloudProjectAlerting = OvhApiCloudProjectAlerting;
+      this.OvhApiCloudProjectUsageCurrent = OvhApiCloudProjectUsageCurrent;
+      this.OvhApiCloudProjectUsageForecast = OvhApiCloudProjectUsageForecast;
+    }
 
-    self.loaders = {
-      alert: false,
-      forecast: false,
-      current: false,
-      deleteAlert: false,
-    };
+    $onInit() {
+      this.loading = false; s;
+      this.data = {
+        currencySymbol: null,
+        alert: null,
+        estimateTotals: null,
+        currentTotals: null,
+      };
+      this.loaders = {
+        alert: false,
+        forecast: false,
+        current: false,
+        deleteAlert: false,
+      };
 
-    self.getCurrentMonth = function () {
+      return this.initForecast()
+        .then(() => this.initCurrent())
+        .then(() => this.initAlert())
+        .catch((err) => {
+          this.CucCloudMessage.error([this.$translate.instant('cpbe_estimate_price_error_message'), (err.data && err.data.message) || ''].join(' '));
+        });
+    }
+
+    static getCurrentMonth() {
       return moment();
-    };
+    }
 
-    self.getNextMonth = function () {
+    static getNextMonth() {
       return moment().add(1, 'month');
-    };
+    }
 
-    function initForecast() {
-      self.loaders.forecast = true;
-      return OvhApiCloudProjectUsageForecast.v6()
+    initForecast() {
+      this.loaders.forecast = true;
+      return this.OvhApiCloudProjectUsageForecast.v6()
         .get({
-          serviceName: $stateParams.projectId,
+          serviceName: this.$stateParams.projectId,
         }).$promise
-        .then(billingInfo => CloudProjectBillingService
+        .then(billingInfo => this.CloudProjectBillingService
           .getConsumptionDetails(billingInfo, billingInfo)
           .then((data) => {
-            self.data.estimateTotals = data.totals;
-            self.data.currencySymbol = self.data.estimateTotals.currencySymbol;
+            this.data.estimateTotals = data.totals;
+            this.data.currencySymbol = this.data.estimateTotals.currencySymbol;
           })
           .finally(() => {
-            self.loaders.forecast = false;
+            this.loaders.forecast = false;
           }));
     }
 
-    function initCurrent() {
-      self.loaders.current = true;
-      return OvhApiCloudProjectUsageCurrent.v6()
+    initCurrent() {
+      this.loaders.current = true;
+      return this.OvhApiCloudProjectUsageCurrent.v6()
         .get({
-          serviceName: $stateParams.projectId,
+          serviceName: this.$stateParams.projectId,
         }).$promise
-        .then(billingInfo => CloudProjectBillingService.getConsumptionDetails(
+        .then(billingInfo => this.CloudProjectBillingService.getConsumptionDetails(
           billingInfo,
           billingInfo,
         ))
         .then((data) => {
-          self.data.currentTotals = data.totals;
+          this.data.currentTotals = data.totals;
         })
         .finally(() => {
-          self.loaders.current = false;
+          this.loaders.current = false;
         });
     }
 
-    function getAlertIds() {
-      OvhApiCloudProjectAlerting.v6().resetCache();
-      return OvhApiCloudProjectAlerting.v6().getIds({
-        serviceName: $stateParams.projectId,
+    getAlertIds() {
+      this.OvhApiCloudProjectAlerting.v6().resetCache();
+      return this.OvhApiCloudProjectAlerting.v6().getIds({
+        serviceName: this.$stateParams.projectId,
       }).$promise;
     }
 
-    function getAlert(id) {
-      return OvhApiCloudProjectAlerting.v6().get({
-        serviceName: $stateParams.projectId,
+    getAlert(id) {
+      return this.OvhApiCloudProjectAlerting.v6().get({
+        serviceName: this.$stateParams.projectId,
         alertId: id,
       }).$promise
         .catch(() => {
           // We dont rethrow or show a message to hide an API glitch.
-          self.data.alert = null;
+          this.data.alert = null;
           return null;
         });
     }
 
-    function initConsumptionChart() {
-      const labelNow = $translate.instant('cpbe_estimate_alert_chart_label_now');
-      const labelFuture = $translate.instant('cpbe_estimate_alert_chart_label_future');
-      const labelLimit = $translate.instant('cpbe_estimate_alert_chart_label_limit');
+    initConsumptionChart() {
+      const labelNow = this.$translate.instant('cpbe_estimate_alert_chart_label_now');
+      const labelFuture = this.$translate.instant('cpbe_estimate_alert_chart_label_future');
+      const labelLimit = this.$translate.instant('cpbe_estimate_alert_chart_label_limit');
 
-      self.consumptionChartData = {
+      this.consumptionChartData = {
         estimate: {
           now: {
-            value: self.data.currentTotals.hourly.total,
-            currencyCode: self.data.estimateTotals.currencySymbol,
+            value: this.data.currentTotals.hourly.total,
+            currencyCode: this.data.estimateTotals.currencySymbol,
             label: labelNow,
           },
           endOfMonth: {
-            value: self.data.estimateTotals.hourly.total,
-            currencyCode: self.data.estimateTotals.currencySymbol,
+            value: this.data.estimateTotals.hourly.total,
+            currencyCode: this.data.estimateTotals.currencySymbol,
             label: labelFuture,
           },
         },
         threshold: {
           now: {
-            value: self.data.alert.monthlyThreshold,
-            currencyCode: self.data.estimateTotals.currencySymbol,
+            value: this.data.alert.monthlyThreshold,
+            currencyCode: this.data.estimateTotals.currencySymbol,
             label: labelLimit,
           },
           endOfMonth: {
-            value: self.data.alert.monthlyThreshold,
-            currencyCode: self.data.estimateTotals.currencySymbol,
+            value: this.data.alert.monthlyThreshold,
+            currencyCode: this.data.estimateTotals.currencySymbol,
             label: labelLimit,
           },
         },
       };
     }
 
-    function initAlert() {
-      self.loaders.alert = true;
+    initAlert() {
+      this.loaders.alert = true;
       // list alerts ids
-      return getAlertIds()
+      return this.getAlertIds()
         .then((alertIds) => {
           if (_.isEmpty(alertIds)) {
             return null;
           }
-          return getAlert(_.first(alertIds));
+          return this.getAlert(_.first(alertIds));
         })
         .then((alertObject) => {
-          self.data.alert = alertObject;
+          this.data.alert = alertObject;
           if (!_.isNull(alertObject)) {
-            initConsumptionChart();
+            this.initConsumptionChart();
           }
         }).finally(() => {
-          self.loaders.alert = false;
+          this.loaders.alert = false;
         });
     }
 
-    function init() {
-      initForecast().then(() => initCurrent()).then(() => initAlert()).catch((err) => {
-        CucCloudMessage.error([$translate.instant('cpbe_estimate_price_error_message'), (err.data && err.data.message) || ''].join(' '));
-      });
-    }
-
-    self.openAlertAddModal = function () {
-      const modal = $uibModal.open({
+    openAlertAddModal() {
+      const modal = this.$uibModal.open({
         windowTopClass: 'cui-modal',
         templateUrl: 'app/cloud/project/billing/consumption/estimate/alert/add/cloud-project-billing-consumption-estimate-alert-add.html',
         controller: 'CloudProjectBillingConsumptionEstimateAlertAddCtrl',
         controllerAs: 'CloudProjectBillingConsumptionEstimateAlertAddCtrl',
         resolve: {
-          dataContext() {
-            return self.data;
-          },
+          dataContext: () => this.data,
         },
       });
 
       modal.result.then(() => {
-        initAlert();
+        this.initAlert();
       });
-    };
+    }
 
-    self.deleteAlert = function () {
-      self.loaders.deleteAlert = true;
+    deleteAlert() {
+      this.loaders.deleteAlert = true;
       // we query alerts to check if an alert already exists, in this case we delete it
-      OvhApiCloudProjectAlerting.v6().getIds({
-        serviceName: $stateParams.projectId,
+      this.OvhApiCloudProjectAlerting.v6().getIds({
+        serviceName: this.$stateParams.projectId,
       }).$promise.then((alertIds) => {
         if (!_.isEmpty(alertIds)) {
-          return OvhApiCloudProjectAlerting.v6().delete({
-            serviceName: $stateParams.projectId,
+          return this.OvhApiCloudProjectAlerting.v6().delete({
+            serviceName: this.$stateParams.projectId,
             alertId: _.first(alertIds),
           }).$promise.then(() => {
-            CucCloudMessage.success($translate.instant('cpbe_estimate_alert_delete_success'));
+            this.CucCloudMessage.success(this.$translate.instant('cpbe_estimate_alert_delete_success'));
           });
         }
-        return $q.reject({ data: { message: 'Alert not found' } });
+        return this.$q.reject({ data: { message: 'Alert not found' } });
       }).catch((err) => {
-        CucCloudMessage.error([$translate.instant('cpbe_estimate_alert_delete_error'), (err.data && err.data.message) || ''].join(' '));
-        return $q.reject(err);
+        this.CucCloudMessage.error([this.$translate.instant('cpbe_estimate_alert_delete_error'), (err.data && err.data.message) || ''].join(' '));
+        return this.$q.reject(err);
       }).finally(() => {
-        self.loaders.deleteAlert = false;
+        this.loaders.deleteAlert = false;
       });
 
-      initAlert();
-    };
-
-    init();
+      return this.initAlert();
+    }
   });
