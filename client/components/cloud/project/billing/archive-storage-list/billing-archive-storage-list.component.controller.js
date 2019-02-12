38f77d0adae2b1c70a6d1d6988460630c6166e31
Marie JONES <marie.jones@corp.ovh.com>
2019-02-12 08:46:30 +0100

    refactor(components.cloud.billing): switch function to a class

--- client/components/cloud/project/billing/archive-storage-list/billing-archive-storage-list.component.controller.js
+++ client/components/cloud/project/billing/archive-storage-list/billing-archive-storage-list.component.controller.js
@@ -1,51 +1,53 @@
 angular.module('managerApp')
-  .controller('BillingArchiveStorageListComponentCtrl', function BillingArchiveStorageListComponentCtrl($q, $filter, $translate, OvhApiMe, Toast, RegionService) {
-    const self = this;
+  .controller('BillingArchiveStorageListComponentCtrl', class BillingArchiveStorageListComponentCtrl {
+    /* @ngInject */
+    constructor($q, $filter, $translate, OvhApiMe, Toast, RegionService) {
+      this.$q = $q;
+      this.$filter = $filter;
+      this.$translate = $translate;
+      this.OvhApiMe = OvhApiMe;
+      this.Toast = Toast;
+      this.RegionService = RegionService;
+    }
 
-    self.RegionService = RegionService;
+    $onInit() {
+      this.loading = true;
+      this.currencySymbol = '';
 
-    self.currencySymbol = '';
+      this.$q.all([this.initUserCurrency()])
+        .catch((err) => {
+          this.Toast.error([this.$translate.instant('cpb_error_message'), (err.data && err.data.message) || ''].join(' '));
+          return this.$q.reject(err);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    }
 
-    self.loading = false;
-
-    function initUserCurrency() {
-      return OvhApiMe.v6().get().$promise.then((me) => {
-        self.currencySymbol = me.currency.symbol;
+    initUserCurrency() {
+      return this.OvhApiMe.v6().get().$promise.then((me) => {
+        this.currencySymbol = me.currency.symbol;
       });
     }
 
-
-    self.$onInit = () => {
-      self.loading = true;
-
-      $q.all([initUserCurrency()])
-        .catch((err) => {
-          Toast.error([$translate.instant('cpb_error_message'), (err.data && err.data.message) || ''].join(' '));
-          return $q.reject(err);
-        })
-        .finally(() => {
-          self.loading = false;
-        });
-    };
-
-    self.getStorageVolumeInfoTooltip = function (storage) {
-      return $translate.instant('cpbc_archive_storage_consumption_info_part1')
-        .concat($translate.instant('cpbc_archive_storage_consumption_info_part2', {
+    getStorageVolumeInfoTooltip(storage) {
+      return this.$translate.instant('cpbc_archive_storage_consumption_info_part1')
+        .concat(this.$translate.instant('cpbc_archive_storage_consumption_info_part2', {
           amount: (storage.stored ? storage.stored.quantity.value : 0),
         }));
-    };
+    }
 
-    self.getStorageOutgoingBandwidthInfoTooltip = function (storage) {
-      return $translate.instant('cpbc_archive_storage_output_traffic_info_part1')
-        .concat($translate.instant('cpbc_archive_storage_output_traffic_info_part2', {
+    getStorageOutgoingBandwidthInfoTooltip(storage) {
+      return this.$translate.instant('cpbc_archive_storage_output_traffic_info_part1')
+        .concat(this.$translate.instant('cpbc_archive_storage_output_traffic_info_part2', {
           amount: (storage.outgoingBandwidth ? storage.outgoingBandwidth.quantity.value : 0),
         }));
-    };
+    }
 
-    self.getStorageIncomingBandwidthInfoTooltip = function (storage) {
-      return $translate.instant('cpbc_archive_storage_input_traffic_info_part1')
-        .concat($translate.instant('cpbc_archive_storage_input_traffic_info_part2', {
+    getStorageIncomingBandwidthInfoTooltip(storage) {
+      return this.$translate.instant('cpbc_archive_storage_input_traffic_info_part1')
+        .concat(this.$translate.instant('cpbc_archive_storage_input_traffic_info_part2', {
           amount: (storage.incomingBandwidth ? storage.incomingBandwidth.quantity.value : 0),
         }));
-    };
+    }
   });
