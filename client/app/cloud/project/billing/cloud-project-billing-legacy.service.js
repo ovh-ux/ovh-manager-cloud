366f9409919cf8abe4e972690867e34e961d5028
Marie JONES <marie.jones@corp.ovh.com>
2019-02-27 17:13:11 +0100

    fix(cloud.project.billing): make service stateless
    prevents service from mixing up datas when being called twice in the same controller

--- client/app/cloud/project/billing/cloud-project-billing-legacy.service.js
+++ client/app/cloud/project/billing/cloud-project-billing-legacy.service.js
@@ -6,33 +6,40 @@ angular.module('managerApp')
       return Number((`${Math.round(`${number}e${decimals}`)}e-${decimals}`));
     }
 
-    function initHourlyInstanceList() {
-      if (!_.get(self.data, 'hourlyBilling') || !_.get(self.data, 'hourlyBilling.hourlyUsage')) {
-        return;
+    function getEmptyConsumption(key) {
+      return ({
+        [key]: [],
+        total: 0,
+      });
+    }
+
+    function initHourlyInstanceList(consumptionDetails) {
+      if (!_.get(consumptionDetails, 'hourlyBilling') || !_.get(consumptionDetails, 'hourlyBilling.hourlyUsage')) {
+        return getEmptyConsumption('hourlyInstances');
       }
       const hourlyInstances = _.flatten(_.map(
-        _.get(self.data, 'hourlyBilling.hourlyUsage.instance'),
+        _.get(consumptionDetails, 'hourlyBilling.hourlyUsage.instance'),
         instance => _.map(instance.details, (detail) => {
           const newDetail = _.clone(detail);
           newDetail.totalPrice = roundNumber(newDetail.totalPrice, 2);
           return _.extend(newDetail, { reference: instance.reference, region: instance.region });
         }),
       ));
-      self.data.hourlyInstances = hourlyInstances;
-      self.data.totals.hourly.instance = _.reduce(
-        _.get(self.data, 'hourlyBilling.hourlyUsage.instance'),
+      const hourlyInstancesTotal = _.reduce(
+        _.get(consumptionDetails, 'hourlyBilling.hourlyUsage.instance'),
         (sum, instance) => sum + roundNumber(instance.totalPrice, 2), 0,
       );
-      self.data.totals.hourly.instance = roundNumber(_.get(self.data, 'totals.hourly.instance'), 2);
+
+      return ({ hourlyInstances, total: roundNumber(hourlyInstancesTotal, 2) });
     }
 
-    function initMonthlyInstanceList() {
-      if (!_.get(self.data, 'monthlyBilling') || !_.get(self.data, 'monthlyBilling.monthlyUsage')) {
-        return;
+    function initMonthlyInstanceList(consumptionDetails) {
+      if (!_.get(consumptionDetails, 'monthlyBilling') || !_.get(consumptionDetails, 'monthlyBilling.monthlyUsage')) {
+        return getEmptyConsumption('monthlyInstances');
       }
 
       const monthlyInstances = _.flatten(_.map(
-        _.get(self.data, 'monthlyBilling.monthlyUsage.instance'),
+        _.get(consumptionDetails, 'monthlyBilling.monthlyUsage.instance'),
         instance => _.map(instance.details, (detail) => {
           const newDetail = _.clone(detail);
           newDetail.totalPrice = roundNumber(newDetail.totalPrice, 2);
@@ -40,75 +47,93 @@ angular.module('managerApp')
         }),
       ));
 
-      self.data.monthlyInstances = monthlyInstances;
-      self.data.totals.monthly.instance = _.reduce(
-        self.data.monthlyBilling.monthlyUsage.instance,
+      const monthlyTotal = _.reduce(
+        _.get(consumptionDetails, 'monthlyBilling.monthlyUsage.instance'),
         (sum, instance) => sum + roundNumber(instance.totalPrice, 2),
         0,
       );
-      self.data.totals.monthly.instance = roundNumber(self.data.totals.monthly.instance, 2);
+
+      return ({
+        monthlyInstances,
+        total: roundNumber(monthlyTotal, 2),
+      });
     }
 
-    function initObjectStorageList() {
-      if (!self.data.hourlyBilling || !self.data.hourlyBilling.hourlyUsage) {
-        return;
+    function initObjectStorageList(consumptionDetails) {
+      if (!consumptionDetails.hourlyBilling || !consumptionDetails.hourlyBilling.hourlyUsage) {
+        return getEmptyConsumption('objectStorages');
       }
-      _.each(self.data.hourlyBilling.hourlyUsage.objectStorage, (objectStorage) => {
-        _.set(objectStorage, 'totalPrice', roundNumber(objectStorage.totalPrice, 2));
-      });
 
-      self.data.objectStorages = _.reject(self.data.hourlyBilling.hourlyUsage.storage, { type: 'pca' });
-      self.data.totals.hourly.objectStorage = _.reduce(
-        self.data.objectStorages,
+      const objectStorages = _.reject(
+        _.map(
+          consumptionDetails.hourlyBilling.hourlyUsage.storage,
+          objectStorage => _.set(objectStorage, 'totalPrice', roundNumber(objectStorage.totalPrice, 2)),
+        ), { type: 'pca' },
+      );
+
+      const objectStorageTotal = _.reduce(
+        objectStorages,
         (sum, storage) => sum + roundNumber(storage.totalPrice, 2),
         0,
       );
-      self.data.totals.hourly.objectStorage = roundNumber(self.data.totals.hourly.objectStorage, 2);
+
+      return ({
+        objectStorages,
+        total: roundNumber(objectStorageTotal, 2),
+      });
     }
 
-    function initArchiveStorageList() {
-      if (!self.data.hourlyBilling || !self.data.hourlyBilling.hourlyUsage) {
-        return;
+    function initArchiveStorageList(consumptionDetails) {
+      if (!consumptionDetails.hourlyBilling || !consumptionDetails.hourlyBilling.hourlyUsage) {
+        return getEmptyConsumption('archiveStorages');
       }
-      _.each(self.data.hourlyBilling.hourlyUsage.archiveStorage, (archiveStorage) => {
-        _.set(archiveStorage, 'totalPrice', roundNumber(archiveStorage.totalPrice, 2));
-      });
 
-      self.data.archiveStorages = _.filter(self.data.hourlyBilling.hourlyUsage.storage, { type: 'pca' });
-      self.data.totals.hourly.archiveStorage = _.reduce(
-        self.data.archiveStorages,
+      const archiveStorages = _.filter(
+        _.map(
+          consumptionDetails.hourlyBilling.hourlyUsage.storage,
+          objectStorage => _.set(objectStorage, 'totalPrice', roundNumber(objectStorage.totalPrice, 2)),
+        ), { type: 'pca' },
+      );
+      const archiveStorageTotal = _.reduce(
+        archiveStorages,
         (sum, archiveStorage) => sum + roundNumber(archiveStorage.totalPrice, 2),
         0,
       );
-      self.data.totals.hourly.archiveStorage = roundNumber(
-        self.data.totals.hourly.archiveStorage,
-        2,
-      );
+
+      return ({
+        archiveStorages,
+        total: roundNumber(archiveStorageTotal, 2),
+      });
     }
 
-    function initSnapshotList() {
-      if (!self.data.hourlyBilling || !self.data.hourlyBilling.hourlyUsage) {
-        return;
+    function initSnapshotList(consumptionDetails) {
+      if (!consumptionDetails.hourlyBilling || !consumptionDetails.hourlyBilling.hourlyUsage) {
+        return getEmptyConsumption('snapshots');
       }
-      _.each(self.data.hourlyBilling.hourlyUsage.snapshot, (snapshot) => {
-        _.set(snapshot, 'totalPrice', roundNumber(snapshot.totalPrice, 2));
-      });
 
-      self.data.snapshots = self.data.hourlyBilling.hourlyUsage.snapshot;
-      self.data.totals.hourly.snapshot = _.reduce(
-        self.data.hourlyBilling.hourlyUsage.snapshot,
+      const snapshots = _.map(
+        consumptionDetails.hourlyBilling.hourlyUsage.snapshot,
+        snapshot => _.set(snapshot, 'totalPrice', roundNumber(snapshot.totalPrice, 2)),
+      );
+
+      const snapshotTotal = _.reduce(
+        consumptionDetails.hourlyBilling.hourlyUsage.snapshot,
         (sum, snapshot) => sum + roundNumber(snapshot.totalPrice, 2),
         0,
       );
-      self.data.totals.hourly.snapshot = roundNumber(self.data.totals.hourly.snapshot, 2);
+
+      return ({
+        snapshots,
+        total: roundNumber(snapshotTotal, 2),
+      });
     }
 
-    function initVolumeList() {
-      if (!self.data.hourlyBilling || !self.data.hourlyBilling.hourlyUsage) {
-        return;
+    function initVolumeList(consumptionDetails) {
+      if (!consumptionDetails.hourlyBilling || !consumptionDetails.hourlyBilling.hourlyUsage) {
+        return getEmptyConsumption('volumes');
       }
       const volumes = _.flatten(_.map(
-        self.data.hourlyBilling.hourlyUsage.volume,
+        consumptionDetails.hourlyBilling.hourlyUsage.volume,
         volume => _.map(volume.details, (detail) => {
           const newDetail = _.clone(detail);
           newDetail.totalPrice = roundNumber(newDetail.totalPrice, 2);
@@ -116,21 +141,24 @@ angular.module('managerApp')
         }),
       ));
 
-      self.data.volumes = volumes;
-      self.data.totals.hourly.volume = _.reduce(
-        self.data.hourlyBilling.hourlyUsage.volume,
+      const volumeTotal = _.reduce(
+        consumptionDetails.hourlyBilling.hourlyUsage.volume,
         (sum, volume) => sum + roundNumber(volume.totalPrice, 2),
         0,
       );
-      self.data.totals.hourly.volume = roundNumber(self.data.totals.hourly.volume, 2);
+
+      return ({
+        volumes,
+        total: roundNumber(volumeTotal, 2),
+      });
     }
 
-    function initInstanceBandwidth() {
-      if (!self.data.hourlyBilling || !self.data.hourlyBilling.hourlyUsage) {
-        return;
+    function initInstanceBandwidth(consumptionDetails) {
+      if (!consumptionDetails.hourlyBilling || !consumptionDetails.hourlyBilling.hourlyUsage) {
+        return getEmptyConsumption('bandwidthByRegions');
       }
       const bandwidthByRegions = _.map(
-        self.data.hourlyBilling.hourlyUsage.instanceBandwidth,
+        consumptionDetails.hourlyBilling.hourlyUsage.instanceBandwidth,
         (bandwidthByRegion) => {
           const newBandwidthByRegion = _.clone(bandwidthByRegion);
           newBandwidthByRegion.outgoingBandwidth.totalPrice = roundNumber(
@@ -146,13 +174,17 @@ angular.module('managerApp')
           return newBandwidthByRegion;
         },
       );
-      self.data.bandwidthByRegions = bandwidthByRegions;
-      self.data.totals.hourly.bandwidth = _.reduce(
-        self.data.bandwidthByRegions,
+
+      const instanceBandwidthTotal = _.reduce(
+        consumptionDetails.hourlyBilling.hourlyUsage.instanceBandwidth,
         (sum, bandwidth) => sum + bandwidth.outgoingBandwidth.totalPrice,
         0,
       );
-      self.data.totals.hourly.bandwidth = roundNumber(self.data.totals.hourly.bandwidth, 2);
+
+      return ({
+        bandwidthByRegions,
+        total: roundNumber(instanceBandwidthTotal, 2),
+      });
     }
 
     self.getConsumptionDetails = function getConsumptionDetails(
@@ -160,41 +192,77 @@ angular.module('managerApp')
       monthlyBillingInfo,
     ) {
       return self.getDataInitialized()
-        .then(() => {
-          self.data.hourlyBilling = hourlyBillingInfo;
-          self.data.monthlyBilling = monthlyBillingInfo;
+        .then((consumptionDetails) => {
+          _.set(consumptionDetails, 'hourlyBilling', hourlyBillingInfo);
+          _.set(consumptionDetails, 'monthlyBilling', monthlyBillingInfo);
 
           return $q
-            .allSettled([
-              initHourlyInstanceList(),
-              initMonthlyInstanceList(),
-              initObjectStorageList(),
-              initArchiveStorageList(),
-              initSnapshotList(),
-              initVolumeList(),
-              initInstanceBandwidth(),
-            ])
-            .then(() => {
-              self.data.totals.monthly.total = roundNumber(self.data.totals.monthly.instance, 2);
-              self.data.totals.hourly.total = roundNumber(
-                self.data.totals.hourly.instance
-                + self.data.totals.hourly.snapshot
-                + self.data.totals.hourly.objectStorage
-                + self.data.totals.hourly.archiveStorage
-                + self.data.totals.hourly.volume
-                + self.data.totals.hourly.bandwidth,
-                2,
-              );
-              self.data.totals.total = roundNumber(
-                self.data.totals.monthly.total + self.data.totals.hourly.total,
-                2,
-              );
-              return self.data;
+            .all({
+              hourlyInstanceList: initHourlyInstanceList(consumptionDetails),
+              monthlyInstanceList: initMonthlyInstanceList(consumptionDetails),
+              objectStorageList: initObjectStorageList(consumptionDetails),
+              archiveStorageList: initArchiveStorageList(consumptionDetails),
+              snapshotList: initSnapshotList(consumptionDetails),
+              volumeList: initVolumeList(consumptionDetails),
+              instanceBandwidthList: initInstanceBandwidth(consumptionDetails),
+            })
+            .then(({
+              hourlyInstanceList,
+              monthlyInstanceList,
+              objectStorageList,
+              archiveStorageList,
+              snapshotList,
+              volumeList,
+              instanceBandwidthList,
+            }) => {
+              const consumption = {
+                hourlyInstances: hourlyInstanceList.hourlyInstances,
+                monthlyInstances: monthlyInstanceList.monthlyInstances,
+                objectStorages: objectStorageList.objectStorages,
+                archiveStorages: archiveStorageList.archiveStorages,
+                snapshots: snapshotList.snapshots,
+                volumes: volumeList.volumes,
+                bandwidthByRegions: instanceBandwidthList.bandwidthByRegions,
+                totals: {
+                  currencySymbol: consumptionDetails.totals.currencySymbol,
+                  hourly: {
+                    total: roundNumber(
+                      hourlyInstanceList.total
+                      + snapshotList.total
+                      + objectStorageList.total
+                      + archiveStorageList.total
+                      + volumeList.total
+                      + instanceBandwidthList.total,
+                      2,
+                    ),
+                    instance: hourlyInstanceList.total,
+                    objectStorage: objectStorageList.total,
+                    archiveStorage: archiveStorageList.total,
+                    snapshot: snapshotList.total,
+                    volume: volumeList.total,
+                    bandwidth: instanceBandwidthList.total,
+                  },
+                  monthly: {
+                    total: roundNumber(monthlyInstanceList.total, 2),
+                    instance: monthlyInstanceList.total,
+                  },
+                },
+              };
+
+              Object.assign(consumption.totals, {
+                total: roundNumber(
+                  consumption.totals.monthly.total + consumption.totals.hourly.total,
+                  2,
+                ),
+              });
+
+              return consumption;
             });
         });
     };
 
     self.getDataInitialized = function getDataInitialized() {
+      const consumptionDetails = {
         hourlyInstances: [],
         monthlyInstances: [],
         objectStorages: [],
@@ -222,8 +290,8 @@ angular.module('managerApp')
         },
       };
       return OvhApiMe.v6().get().$promise.then((me) => {
-        self.data.totals.currencySymbol = me.currency.symbol;
-        return self.data;
+        consumptionDetails.totals.currencySymbol = me.currency.symbol;
+        return consumptionDetails;
       });
     };
   });
