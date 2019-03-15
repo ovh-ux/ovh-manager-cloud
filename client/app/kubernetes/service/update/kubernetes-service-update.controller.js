4a32034d2f37c85278ff9d3aa5efc0e256ee0290
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-03-15 16:23:58 +0100

    fix(kubernetes): display modals
    * update `kubernetes` module to use `CucControllerHelper` instead of
    `ControllerHelper`

--- client/app/kubernetes/service/update/kubernetes-service-update.controller.js
+++ client/app/kubernetes/service/update/kubernetes-service-update.controller.js
@@ -1,13 +1,13 @@
 class kubernetesUpdateCtrl {
   /* @ngInject */
   constructor($rootScope, $stateParams, $translate, $uibModalInstance, CucCloudMessage,
-    ControllerHelper, Kubernetes) {
+    CucControllerHelper, Kubernetes) {
     this.$rootScope = $rootScope;
     this.serviceName = $stateParams.serviceName;
     this.$uibModalInstance = $uibModalInstance;
     this.$translate = $translate;
     this.CucCloudMessage = CucCloudMessage;
-    this.ControllerHelper = ControllerHelper;
+    this.CucControllerHelper = CucControllerHelper;
     this.Kubernetes = Kubernetes;
   }
 
@@ -22,14 +22,14 @@ class kubernetesUpdateCtrl {
   update() {
     this.loading = true;
     this.CucCloudMessage.flushChildMessage();
-    this.updating = this.ControllerHelper.request.getHashLoader({
+    this.updating = this.CucControllerHelper.request.getHashLoader({
       loaderFunction: () => this.Kubernetes
         .updateKubernetesVersion(this.serviceName)
         .then(() => this.CucCloudMessage.success(this.$translate.instant('kube_service_update_success')))
         .catch(err => this.CucCloudMessage.error(this.$translate.instant('kube_service_update_error', { message: _.get(err, 'data.message', '') })))
         .finally(() => {
           this.loading = false;
-          this.ControllerHelper.scrollPageToTop();
+          this.CucControllerHelper.scrollPageToTop();
           this.$uibModalInstance.close();
           this.$rootScope.$broadcast('kube.service.refresh');
         }),
