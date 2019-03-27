c50a36b7e8132d0b5a42c649e4dd5544223fd64f
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2019-03-27 16:58:29 +0100

    fix(us): display modal for each order in US

---
+++ client/components/disable-order-modal/controller.js
@@ -0,0 +1,6 @@
+export default class DisableOrderModalCtrl {
+  /* @ngInject */
+  constructor($uibModalInstance) {
+    this.$uibModalInstance = $uibModalInstance;
+  }
+}
--- client/components/sidebar/sidebar-order.service.js
+++ client/components/sidebar/sidebar-order.service.js
@@ -1,11 +1,17 @@
+import disableOrderModalTpl from '../disable-order-modal/template.html';
+import disableOrderModalCtrl from '../disable-order-modal/controller';
+
 class SidebarOrderService {
   constructor(atInternet, CucFeatureAvailabilityService, SidebarMenu, CloudProjectSidebar,
     VpsSidebar, DedicatedServerSidebar, DedicatedCloudSidebar, CdaSidebar, NashaSidebar,
     VeeamSidebar, VeeamEnterpriseSidebar, DBaasTsSidebar, VrackSidebar, DeskaasSidebar,
-    IpSidebar, IplbSidebar, LicenseSidebar, CloudDBSidebar, LogsSidebar, KubernetesSidebar) {
+    IpSidebar, IplbSidebar, LicenseSidebar, CloudDBSidebar, LogsSidebar, KubernetesSidebar,
+    $uibModal) {
     this.CucFeatureAvailabilityService = CucFeatureAvailabilityService;
     this.SidebarMenu = SidebarMenu;
     this.atInternet = atInternet;
+
+    this.$uibModal = $uibModal;
 
     // In order of appearance in the actions menu
     this.productsToOrder = [
@@ -38,6 +44,22 @@ class SidebarOrderService {
       if (!orderItem) {
         return;
       }
+
+      if (locale === 'US') {
+        delete orderItem.state;
+        orderItem.href = window.location.href;
+        orderItem.target = '_self';
+        orderItem.external = false;
+        orderItem.onClick = () => {
+          this.$uibModal.open({
+            template: disableOrderModalTpl,
+            controller: disableOrderModalCtrl,
+            controllerAs: '$ctrl',
+          });
+          return true;
+        };
+      }
+
       this.SidebarMenu.addActionsMenuOption(orderItem);
     });
     this.SidebarMenu.addActionsMenuItemClickHandler((id) => {
