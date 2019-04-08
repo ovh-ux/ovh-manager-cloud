be6f374e670639a0d846c78e469e70d279d669ce
Ganesh Kumar Venkata Krishnan <ganesh.kumar@corp.ovh.com>
2019-04-08 11:51:51 +0530

    feat(adp): adp version update to latest

--- client/components/sidebar/sections/analytics-section.service.js
+++ client/components/sidebar/sections/analytics-section.service.js
@@ -12,22 +12,15 @@ class AnalyticsSectionSidebarService {
     ];
   }
 
-  createSection(logsProducts) {
-    const analyticsMenuSection = this.SidebarMenu.addMenuItem({
+  createSection() {
+    this.SidebarMenu.addMenuItem({
       id: 'mainAnalyticsItem',
       title: this.$translate.instant('cloud_sidebar_section_analytics'),
-      allowSubItems: true,
+      allowSubItems: false,
       state: 'adp.list',
       loadOnState: 'adp.list',
       icon: 'ovh-font ovh-font-adp',
-      allowSearch: this.SidebarHelper.constructor
-        .countProductsInSection(logsProducts) > this.SIDEBAR_MIN_ITEM_FOR_SEARCH,
     });
-    this.SidebarHelper.constructor.fillSection(
-      analyticsMenuSection,
-      this.productTypesInSection,
-      logsProducts,
-    );
   }
 }
 
