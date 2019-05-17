c8f296b75723b048b6e2313d3fb1dc3fd4786d13
Marie JONES <marie.jones@corp.ovh.com>
2019-05-17 15:57:56 +0200

    feat(sidebar): add link to new manager

--- client/components/sidebar/sidebar.js
+++ client/components/sidebar/sidebar.js
@@ -1,32 +1,37 @@
-angular.module('managerApp').run(($translate, asyncLoader) => {
-  asyncLoader.addTranslations(
-    import(`ovh-angular-sidebar-menu/src/ovh-angular-sidebar-menu/translations/Messages_${$translate.use()}.xml`)
-      .catch(() => import(`ovh-angular-sidebar-menu/src/ovh-angular-sidebar-menu/translations/Messages_${$translate.fallbackLanguage()}.xml`))
-      .then(x => x.default),
-  );
-  asyncLoader.addTranslations(import(`./translations/Messages_${$translate.use()}.json`).then(x => x.default));
-  $translate.refresh();
-});
+import { PUBLIC_CLOUD_URL } from './sidebar.constants';
+
 angular.module('managerApp')
-  .run(($q, $translate, Toast, SidebarMenu, SidebarOrderService, SidebarContentService,
-    CucProductsService, SessionService) => {
+  .run(/* @ngTranslationsInject:json ./translations */)
+  .run(($q, $translate, atInternet, Toast, SidebarMenu, SidebarOrderService, SidebarContentService,
+    CucProductsService, SessionService, TARGET) => {
     const promise = $q.all({
       user: SessionService.getUser(),
       products: CucProductsService.getProducts(),
       translate: $translate.refresh(),
-    }).catch((err) => {
-      Toast.error(`${$translate.instant('cloud_sidebar_error')} ${err.data.message}`);
-    });
+    })
+      .catch((err) => {
+        Toast.error(`${$translate.instant('cloud_sidebar_error')} ${err.data.message}`);
+      });
 
     SidebarMenu.setInitializationPromise(promise);
 
     SidebarMenu.loadDeferred.promise
       .then((data) => {
         SidebarOrderService.buildSidebarMenuActions(data.user.ovhSubsidiary);
-        return SidebarContentService.buildSidebarContent(
+        SidebarContentService.buildSidebarContent(
           data.products.results,
           data.user.ovhSubsidiary,
         );
+        return SidebarMenu.addMenuItem({
+          category: 'cloud-sidebar-orphan-link',
+          title: $translate.instant('cloud_sidebar_public_cloud'),
+          url: PUBLIC_CLOUD_URL[TARGET],
+          target: '_self',
+          onClick: () => atInternet.trackClick({
+            id: 'cloud_sidebar_go-to-public-cloud',
+            type: 'action',
+          }),
+        });
       })
       .then(() => {
         // After sidebar elements are all loaded, check if there is an element for the current state
