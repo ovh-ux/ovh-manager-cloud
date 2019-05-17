c9d244df8b126f06f9357b67ab2c701cdf4f90ef
Marie JONES <marie.jones@corp.ovh.com>
2019-05-17 15:57:56 +0200

    feat(sidebar): add link to new manager

---
+++ client/components/sidebar/sidebar.constants.js
@@ -0,0 +1,9 @@
+export const PUBLIC_CLOUD_URL = {
+  CA: 'https://ca.ovh.com/manager/public-cloud/index.html#/',
+  EU: 'https://www.ovh.com/manager/public-cloud/index.html#/',
+  US: 'https://us.ovhcloud.com/manager/public-cloud/index.html#/',
+};
+
+export default {
+  PUBLIC_CLOUD_URL,
+};
