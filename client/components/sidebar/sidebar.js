170127ad8a57366977bd2a143d46578d9568f78c
Marie JONES <14836007+marie-j@users.noreply.github.com>
2019-05-23 16:15:29 +0200

    feat: change new manager url display (#1456)

--- client/components/sidebar/sidebar.js
+++ client/components/sidebar/sidebar.js
@@ -1,9 +1,7 @@
-import { PUBLIC_CLOUD_URL } from './sidebar.constants';
-
 angular.module('managerApp')
   .run(/* @ngTranslationsInject:json ./translations */)
   .run(($q, $translate, atInternet, Toast, SidebarMenu, SidebarOrderService, SidebarContentService,
-    CucProductsService, SessionService, TARGET) => {
+    CucProductsService, SessionService) => {
     const promise = $q.all({
       user: SessionService.getUser(),
       products: CucProductsService.getProducts(),
@@ -18,20 +16,10 @@ angular.module('managerApp')
     SidebarMenu.loadDeferred.promise
       .then((data) => {
         SidebarOrderService.buildSidebarMenuActions(data.user.ovhSubsidiary);
-        SidebarContentService.buildSidebarContent(
+        return SidebarContentService.buildSidebarContent(
           data.products.results,
           data.user.ovhSubsidiary,
         );
-        return SidebarMenu.addMenuItem({
-          category: 'cloud-sidebar-orphan-link',
-          title: $translate.instant('cloud_sidebar_public_cloud'),
-          url: PUBLIC_CLOUD_URL[TARGET],
-          target: '_self',
-          onClick: () => atInternet.trackClick({
-            id: 'cloud_sidebar_go-to-public-cloud',
-            type: 'action',
-          }),
-        });
       })
       .then(() => {
         // After sidebar elements are all loaded, check if there is an element for the current state
