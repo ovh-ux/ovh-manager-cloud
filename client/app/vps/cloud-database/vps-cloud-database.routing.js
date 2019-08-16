1aeee3a8cf48e62c80623106b0f5a6df3ef96b6a
radireddy <adireddyravindra@gmail.com>
2019-08-16 13:44:55 +0530

    fix(vps): hide VPS and cloudDatabase tabs in both CA and US region (#1549)
    ref: MBP-549

--- client/app/vps/cloud-database/vps-cloud-database.routing.js
+++ client/app/vps/cloud-database/vps-cloud-database.routing.js
@@ -3,14 +3,6 @@ import component from './vps-cloud-database.component';
 export default /* @ngInject */($stateProvider) => {
   $stateProvider
     .state('iaas.vps.detail.cloud-database', {
-      resolve: {
-        shouldNotBeCA: /* @ngInject */ (
-          $q,
-          CucFeatureAvailabilityService,
-        ) => CucFeatureAvailabilityService
-          .hasFeaturePromise('VPS', 'cloudDatabase')
-          .then(hasFeature => (hasFeature ? $q.when() : $q.reject('Not authorized'))),
-      },
       url: '/cloud-database',
       views: {
         'vpsContent@iaas.vps.detail': {
